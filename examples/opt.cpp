787361225e7c5518159c68eb3ff60cd37106a2cb
Vissarion Fisikopoulos <fisikop@gmail.com>
2012-11-14 17:38:50 +0000

    n^5 volume algorithm implemented

--- examples/opt.cpp
+++ examples/opt.cpp
@@ -1,3 +1,23 @@
+// Copyright 2012-2013 National and Kapodistrian University of Athens, Greece.
+//
+// This file is part of RandGeom.
+//
+// RandGeom is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at
+// your option) any later version.
+//
+// RandGeom is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// See the file COPYING.LESSER for the text of the GNU Lesser General
+// Public License.  If you did not receive this file along with HeaDDaCHe,
+// see <http://www.gnu.org/licenses/>.
+// 
+// Developer: Vissarion Fisikopoulos
+
 #include <CGAL/point_generators_d.h>
 //#include <CGAL/Filtered_kernel_d.h>
 //#include <CGAL/Triangulation.h>
@@ -34,11 +54,17 @@ typedef Kernel::Vector_d							Vector;
 typedef Kernel::Line_d								Line;
 typedef Kernel::Hyperplane_d					Hyperplane;
 typedef Kernel::Direction_d						Direction;
-typedef Kernel::Sphere_d							Ball;
-typedef std::vector<Hyperplane>       H_polytope;
-typedef H_polytope 								    Polytope;
-typedef std::vector<Point>						V_polytope;
+//typedef Kernel::Sphere_d							Ball;
+struct Ball;
+
+// define different kind of polytopes
+typedef std::vector<Hyperplane>           H_polytope;
+typedef H_polytope 								        Polytope;
+typedef std::vector<Point>						    V_polytope;
 typedef std::pair<V_polytope,V_polytope> 	MinkSumPolytope;
+typedef std::pair<H_polytope,Ball> 	      BallIntersectPolytope;
+
+
 
 typedef CGAL::Extreme_points_traits_d<Point>   EP_Traits_d;
 
@@ -54,7 +80,7 @@ void round_print(T p) {
 }
 
 // Naive algorithm for Mink sum
-typedef std::vector<V_polytope>  	Vpolys;
+typedef std::vector<V_polytope>              Vpolys;
 
 int Minkowski_sum_naive(V_polytope &P1, V_polytope &P2, V_polytope &Msum){
 	std::cout<<(!P1.empty() && !P2.empty())<<std::endl;
@@ -78,6 +104,29 @@ int Minkowski_sum_naive(V_polytope &P1, V_polytope &P2, V_polytope &Msum){
   }
   return -1;
 }
+
+// ball type
+struct Ball{
+	public:
+	  Ball(Point c, NT R) : _c(c),	 _R(R) {}
+	   
+	  Point center(){
+			return _c;
+		}
+		NT squared_radius(){
+			return _R;
+		}
+		NT radius(){
+			return std::sqrt(_R);
+		}
+		bool is_in(Point p){
+			return ((p-CGAL::Origin()) - (_c-CGAL::Origin())).squared_length() <= _R;
+		}
+	
+	private:
+	  Point  _c; //center
+	  NT     _R; //squared radius
+};
 
 // separation oracle return type 
 struct sep{
@@ -128,6 +177,7 @@ Polytope cube(const int n, const NT lw, const NT up){
 }
 
 // contruct a n-ball of radius r centered in the origin  
+/*
 Ball ball(const int n, const NT r){
   
   std::vector<Point> P_ball;
@@ -146,12 +196,15 @@ Ball ball(const int n, const NT r){
   Ball B(n,P_ball.begin(),P_ball.end());
 	return B;
 }
+*/
 
 //template <typename T> struct Oracle{
 //  sep Sep_Oracle(T &P, Point v);
 //};
 
+// GENERIC ORACLE DESCRIPTION
 template <typename T> sep Sep_Oracle(T&, Point);
+// template parameter specialization
 
 //function that implements the separation oracle 
 template<> sep Sep_Oracle<Polytope>(Polytope &P, Point v)
@@ -163,6 +216,21 @@ template<> sep Sep_Oracle<Polytope>(Polytope &P, Point v)
 		++Hit;
 	}
 	return sep(true);	
+}
+
+// BallIntersectPolytope separation oracle
+template<> sep Sep_Oracle<BallIntersectPolytope>(BallIntersectPolytope &P, Point v)
+{
+	H_polytope P1 = P.first; 
+	Ball B = P.second;
+	
+	if (B.is_in(v)){
+		return Sep_Oracle(P1,v);
+	}
+	// the problem here is that it is out but without separating hyperplane
+	// so this is a membership oracle! not separation
+	// TODO: fix it!
+	return sep(false);
 }
 
 // Minkowski sum Separation 
@@ -221,7 +289,7 @@ Vector line_intersect(Point pin, Vector l, T &P, double err){
   Point pout=pin;
   //std::cout<<"Starting inside point: ";
   //round_print(pin);
-  
+
   Vector aug(l);
   while(Sep_Oracle(P,pout).get_is_in() == true){
     aug*=2;
@@ -230,7 +298,7 @@ Vector line_intersect(Point pin, Vector l, T &P, double err){
     //round_print(pout);
   }
   Vector vout=pout-CGAL::Origin();
-  
+
   //intersect using bisection
   //std::cout<<"pout"<<vout<<std::endl;
   Vector vmid;
@@ -247,7 +315,8 @@ Vector line_intersect(Point pin, Vector l, T &P, double err){
   
   //std::cout<<"Intersection point: ";
   //round_print(vmid);
-  return vmid; 
+  //return vmid; 
+	return vin; //ensure that the point is always in P 
 }
 
 /* Hit and run Random Walk */
@@ -267,6 +336,7 @@ int hit_and_run(Point &p,
 	Vector l(n,v.begin(),v.end());
 	Vector b1 = line_intersect(p,l,P,err);
 	Vector b2 = line_intersect(p,-l,P,err);
+	//std::cout<<"b1="<<b1<<"b2="<<b2<<std::endl;
 	double lambda = urdist(rng);
 	p = CGAL::Origin() + (NT(lambda)*b1 + (NT(1-lambda)*b2));
 	return 1;
@@ -650,6 +720,203 @@ int opt_interior(T &K,
 	return 0;
 }
 
+
+/////////////////////////////////////////////////////////
+// VOLUME
+// randomized approximate volume computation 
+/*************************************************
+/**************** VOLUME with hit and run        */
+// it stuck in the corners
+template <class T>
+NT volume1(T &P,
+					const int n,
+					int rnum,
+					int walk_len,
+					double err,
+					RNGType &rng,
+					boost::random::uniform_real_distribution<> &urdist,
+					boost::random::uniform_real_distribution<> &urdist1){
+						
+	// The sandwitching 
+	// r is the radius of the smallest ball
+	// d is the radius of the largest
+  std::vector<NT> coords_apex(n,1);
+	Vector p_apex(n,coords_apex.begin(),coords_apex.end());
+  const NT r=1, d=std::sqrt(p_apex.squared_length());
+  const int nb = std::ceil(n * (std::log(d)/std::log(2.0)));
+  //std::cout<<"nb="<<nb<<", d="<<d<<std::endl;
+  //std::pow(std::log(n),2)
+  
+  // Construct the sequence of balls
+  std::vector<NT> coords(n,0);
+	Point p0(n,coords.begin(),coords.end());
+  std::vector<Ball> balls;
+  for(int i=0; i<=nb; ++i){
+		balls.push_back(Ball(p0,std::pow(std::pow(2.0,NT(i)/NT(n)),2))); 
+		std::cout<<"ball"<<i<<"="<<balls[i].center()<<" "<<balls[i].radius()<<std::endl;
+	}
+  assert(!balls.empty());
+  std::cout<<"---------"<<std::endl;
+  
+    
+  std::vector<int> telescopic_prod(nb,0);
+  for(int i=0; i<rnum; ++i){ //generate rnum rand points 
+		//start with a u.d.r point in the smallest ball 
+		//radius=1, center=Origin()
+		std::vector<NT> coords(n,0);
+		Point p(n,coords.begin(),coords.end());
+		BallIntersectPolytope PBold(P,balls[0]);
+		hit_and_run(p,PBold,n,err,rng,urdist,urdist1);
+		//std::cout<<p<<std::endl;
+		//std::cout<<Sep_Oracle(PBold,p).get_is_in()<<std::endl;
+		//std::cout<<balls[0].is_in(p)<<std::endl;
+		
+		//exit(0);
+		std::vector<Ball>::iterator bit=balls.begin();
+		std::vector<int>::iterator prod_it=telescopic_prod.begin();
+		++bit; 
+		std::cout<<"generate random point..."<<i<<"/"<<rnum<<" ";
+		const NT pi = boost::math::constants::pi<NT>();
+		NT vol = std::pow(pi,n/2.0)/std::tgamma(1+n/2.0); 
+		for(std::vector<int>::iterator prod_it=telescopic_prod.begin(); 
+		    prod_it!=telescopic_prod.end(); ++prod_it){
+			vol *= NT(i+1)/NT(*prod_it);
+		}
+		std::cout<<"current vol estimation= "<<vol<<std::endl;
+			
+		for(; bit!=balls.end(); ++bit, ++prod_it){
+			// generate a random point in bit intersection with P 
+			BallIntersectPolytope PB(P,*bit);
+			
+			for(int j=0; j<walk_len; ++j){
+			  hit_and_run(p,PB,n,err,rng,urdist,urdist1);
+				//std::cout<<"h-n-r:"<<p<<std::endl;
+			}
+			if (Sep_Oracle(PBold,p).get_is_in()){
+				//std::cout<<p<<" IN ball: "<<PBold.second.center()<<PBold.second.radius()<<std::endl;
+			  ++(*prod_it);
+			}else{
+			  ;//std::cout<<p<<":"<<(p-CGAL::Origin()).squared_length()
+			  //<<" OUT ball: "<<PBold.second.center()<<PBold.second.radius()<<std::endl;
+			}
+			PBold=PB;
+		}
+		//for(prod_it=telescopic_prod.begin(); prod_it!=telescopic_prod.end(); ++prod_it)
+		//	std::cout<<(*prod_it)<<" ";
+		//std::cout<<std::endl;
+	}	
+	const NT pi = boost::math::constants::pi<NT>();
+	NT vol = std::pow(pi,n/2.0)/std::tgamma(1+n/2.0); 
+	//NT vol=1;
+	std::cout<<"vol(K_0)="<<vol<<" ";
+	for(std::vector<int>::iterator prod_it=telescopic_prod.begin(); 
+	    prod_it!=telescopic_prod.end(); ++prod_it){
+		vol *= NT(rnum)/NT(*prod_it);
+		std::cout<<NT(rnum)<<"/" << NT(*prod_it)<<"="<<NT(rnum)/NT(*prod_it)<<"\n";
+	}
+	std::cout<<std::endl;
+	std::cout<<"volume = "<<vol<<std::endl;
+	std::cout<<"exact volume = "<<std::pow(2,n)<<std::endl;
+	std::cout<<"# walk steps = "<<walk_len<<std::endl;
+	std::cout<<"# rand point = "<<rnum<<std::endl;
+	return 0;
+}
+
+
+// VOLUME with multipoint random walk
+template <class T>
+NT volume2(T &P,
+					const int n,
+					int rnum,
+					int walk_len,
+					double err,
+					RNGType &rng,
+					boost::variate_generator< RNGType, boost::normal_distribution<> >
+					&get_snd_rand,
+					boost::random::uniform_real_distribution<> &urdist,
+					boost::random::uniform_real_distribution<> &urdist1){
+						
+	// The sandwitching 
+	// r is the radius of the smallest ball
+	// d is the radius of the largest
+  std::vector<NT> coords_apex(n,1);
+	Vector p_apex(n,coords_apex.begin(),coords_apex.end());
+  const NT r=1, d=std::sqrt(p_apex.squared_length());
+  const int nb = std::ceil(n * (std::log(d)/std::log(2.0)));
+  std::cout<<"nb="<<nb<<", d="<<d<<std::endl;
+  //std::pow(std::log(n),2)
+  
+  // Construct the sequence of balls
+  std::vector<NT> coords(n,0);
+	Point p0(n,coords.begin(),coords.end());
+  std::vector<Ball> balls;
+  for(int i=0; i<=nb; ++i){
+		balls.push_back(Ball(p0,std::pow(std::pow(2.0,NT(i)/NT(n)),2))); 
+		std::cout<<"ball"<<i<<"="<<balls[i].center()<<" "<<balls[i].radius()<<std::endl;
+	}
+  assert(!balls.empty());
+  std::cout<<"---------"<<std::endl;
+  
+  
+  
+  std::vector<int> telescopic_prod(nb,0);
+  //vector to store the random points
+  std::vector<Point> V;
+  BallIntersectPolytope PBold(P,balls[0]);
+  for(int i=0; i<rnum; ++i){ 
+		// generate rnum rand points  
+		// in the smallest ball i.e. radius=1, center=Origin()
+		std::vector<NT> coords(n,0);
+		Point p(n,coords.begin(),coords.end());
+		hit_and_run(p,PBold,n,err,rng,urdist,urdist1);
+		V.push_back(p);
+		//std::cout<<p<<std::endl;
+	}	
+	//std::cout<<p<<std::endl;
+	//std::cout<<Sep_Oracle(PBold,p).get_is_in()<<std::endl;
+	//std::cout<<balls[0].is_in(p)<<std::endl;
+	//exit(0);
+	std::vector<Ball>::iterator bit=balls.begin();
+	std::vector<int>::iterator prod_it=telescopic_prod.begin();
+	++bit; 
+	for(; bit!=balls.end(); ++bit, ++prod_it){
+		// generate a random point in bit (intersection) P 
+		BallIntersectPolytope PB(P,*bit);
+	  std::cout<<"walking..."<<walk_len<<"steps"<<std::endl;
+		multipoint_random_walk(PB,V,V.size(),n,walk_len,err,rng,get_snd_rand,urdist,urdist1);
+		
+		for(int j=0; j<V.size(); ++j){
+		  
+			if (Sep_Oracle(PBold,V[j]).get_is_in()){
+				//std::cout<<p<<" IN ball: "<<PBold.second.center()<<PBold.second.radius()<<std::endl;
+				++(*prod_it);
+			}//else{
+				//std::cout<<p<<":"<<(p-CGAL::Origin()).squared_length()
+				//<<" OUT ball: "<<PBold.second.center()<<PBold.second.radius()<<std::endl;
+		}
+		PBold=PB;	
+		//for(prod_it=telescopic_prod.begin(); prod_it!=telescopic_prod.end(); ++prod_it)
+		//	std::cout<<(*prod_it)<<" ";
+		//std::cout<<std::endl;
+	}	
+	const NT pi = boost::math::constants::pi<NT>();
+	NT vol = std::pow(pi,n/2.0)/std::tgamma(1+n/2.0); 
+	//NT vol=1;
+	std::cout<<"vol(K_0)="<<vol<<" ";
+	for(std::vector<int>::iterator prod_it=telescopic_prod.begin(); 
+	    prod_it!=telescopic_prod.end(); ++prod_it){
+		vol *= NT(rnum)/NT(*prod_it);
+		std::cout<<NT(rnum)<<"/" << NT(*prod_it)<<"="<<NT(rnum)/NT(*prod_it)<<"\n";
+	}
+	std::cout<<std::endl;
+	std::cout<<"volume = "<<vol<<std::endl;
+	std::cout<<"exact volume = "<<std::pow(2,n)<<std::endl;
+	return 0;
+}
+
+
+
+
 //////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////
@@ -664,7 +931,7 @@ int main(const int argc, const char** argv)
 
   /* CONSTANTS */
   //dimension
-  const size_t n=2; 
+  const size_t n=4; 
   //number of random points
   //const int m = 2*n*std::pow(std::log(n),2);
   const int m = 20*n;
@@ -800,21 +1067,18 @@ int main(const int argc, const char** argv)
 	std::cout<<std::endl;
 	
 	
-	//VOLUME 
-  cube(n,-0.5,0.5);
+ 
+  // The input volume polytope 
+	Polytope P = cube(n,-1,1);
   
-  //construct the sequence of balls
-  const NT r=0.5, d=0.71;
-  std::vector<Ball> balls;
-  const int nb = n * (std::log(d)/std::log(2.0));
+  // Random walks in K_i := the intersection of the ball i with P
+  // the number of random points to be generated in each K_i
+  const int rnum = 30 * n * std::log(n);
+  const int walk_len =  1 * std::pow(n,4);
   
-  std::cout<<nb<<","<<(std::log(d)/std::log(2.0))<<std::endl;
-  //std::pow(std::log(n),2)
+  volume1(P,n,rnum,walk_len,err,rng,urdist,urdist1);
+  volume2(P,n,rnum,walk_len,err,rng,get_snd_rand,urdist,urdist1);
   
-  for(int i=0; i<nb; ++i){
-		balls.push_back(ball(n,std::pow(2,i/n)));
-	}
-  std::cout<<balls.size()<<std::endl;
   //std::vector<NT> testp(n,NT(0.2));
   //std::cout<<B.has_on_positive_side(Point(n,testp.begin(),testp.end()))<<std::endl;
   
@@ -886,3 +1150,5 @@ int main(const int argc, const char** argv)
 	
   return 0;
 }
+
+
