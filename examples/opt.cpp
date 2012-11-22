b44a79f16124aaa371ec5611c6296a8b8d64a553
Vissarion Fisikopoulos <fisikop@gmail.com>
2012-11-22 18:18:49 +0000

    working on opt

--- examples/opt.cpp
+++ examples/opt.cpp
@@ -31,7 +31,6 @@ int main(const int argc, const char** argv)
 	double wl_c, e;
 	//parse command line input
 	if(argc==4){
-		std::cout << argv[3]<<std::endl;
 	  //dimension
 		n = atoi(argv[1]);
 		//constants
@@ -52,7 +51,7 @@ int main(const int argc, const char** argv)
   const double err=0.001; 
   const double err_opt=0.001; 
   //bounds for the cube	
-  const int lw=0, up=10000, R=up-lw;
+  const int lw=0, up=100000, R=up-lw;
   
 		
 		
@@ -80,40 +79,53 @@ int main(const int argc, const char** argv)
   Vector w(n,ww.begin(),ww.end());
   w=w/w.squared_length();           //normalize
   
-  //the feasible point that approximates opt (at the end fp should be opt)
-  Point fp;
+  
+  //the feasible point that approximates opt (at the end fp should be opt)	
+  Point fp, fp2;
+  
   //the number of generated random points 
   int rnum =  e * n * std::pow(std::log(n),2);;
   //the number of steps in every random walk
   int walk_len =  wl_c * std::pow(n,4);
   
-  //do optimization 
-  tstart = (double)clock()/(double)CLOCKS_PER_SEC;
+  double t1=0, t2=0, max_err1=0, max_err2=0;
+  int num_of_exp = 50;  
   vars var(rnum,n,walk_len,err,err_opt,lw,up,L,rng,get_snd_rand,urdist,urdist1);
-  optimization(P,var,fp,w);
-  tstop = (double)clock()/(double)CLOCKS_PER_SEC;
-  
-  std::cout<<"Interior point"<<std::endl;
-  Point fp2;
-  //do interior point optimization	
-	tstart2 = (double)clock()/(double)CLOCKS_PER_SEC;
   vars var2(rnum,n,walk_len,err,err_opt,lw,up,L,rng,get_snd_rand,urdist,urdist1);
-  opt_interior(P,var2,fp2,w);
-  tstop2 = (double)clock()/(double)CLOCKS_PER_SEC;
-  
-  //print the results
-  std::cout<<"------------------"<<std::endl;
   std::cout<<"# walk steps = "<<walk_len<<std::endl;
 	std::cout<<"# rand points = "<<rnum<<std::endl<<std::endl;
+  for(int i=0; i<num_of_exp; ++i){
+	  std::cout<<"i="<<i<<" "<<std::flush;
+	  //do optimization 
+	  tstart = (double)clock()/(double)CLOCKS_PER_SEC;
+	  optimization(P,var,fp,w);
+	  tstop = (double)clock()/(double)CLOCKS_PER_SEC;
+	  t1 += tstop-tstart;
+	  if (max_err1 < std::abs((fp-CGAL::Origin())*w - Vector(n,ww.begin(),ww.end())*w))
+	    max_err1=std::abs((fp-CGAL::Origin())*w - Vector(n,ww.begin(),ww.end())*w);
+	    
+	  //std::cout<<"Interior point"<<std::endl;
+	  //do interior point optimization	
+		tstart2 = (double)clock()/(double)CLOCKS_PER_SEC;
+	  opt_interior(P,var2,fp2,w);
+	  tstop2 = (double)clock()/(double)CLOCKS_PER_SEC;
+	  t2 += tstop2-tstart2;
+	  if (max_err2 < std::abs((fp2-CGAL::Origin())*w - Vector(n,ww.begin(),ww.end())*w))
+	    max_err2=std::abs((fp2-CGAL::Origin())*w - Vector(n,ww.begin(),ww.end())*w);
+	  //std::cout<<"t1="<<tstop-tstart<<" "<<t1<<" t2="<<tstop2-tstart2<<" "<<t2<<std::endl;
+  }
+  //print the results
+  std::cout<<"------------------"<<std::endl;
 
   std::cout<<"OPT = "<<fp<<std::endl;
-  std::cout<<"err = "<<std::abs((fp-CGAL::Origin())*w - Vector(n,ww.begin(),ww.end())*w)<<std::endl;
-  std::cout<<"time = "<<tstop-tstart<<std::endl<<std::endl;
+  std::cout<<"max err = "<<max_err1<<std::endl;
+  std::cout<<"time = "<<t1/num_of_exp<<std::endl<<std::endl;
 	//std::cout<<"max num of iterations = "<<2*n*L<<std::endl;
   //
   std::cout<<"OPT I="<<fp2<<std::endl;
-  std::cout<<"err = "<<std::abs((fp2-CGAL::Origin())*w - Vector(n,ww.begin(),ww.end())*w)<<std::endl;
-	std::cout<<"time = "<<tstop2-tstart2<<std::endl;
+  std::cout<<"max err = "<<max_err2<<std::endl;
+	std::cout<<"time = "<<t2/num_of_exp<<std::endl;
+  
   /**/
   
   //std::vector<NT> testp(n,NT(0.2));
