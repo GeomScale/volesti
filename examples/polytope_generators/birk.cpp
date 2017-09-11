37e0fc70c1250dd0e982cd6a45fa8f8218da1daa
Vissarion Fisikopoulos <fisikop@gmail.com>
2017-09-11 14:31:17 +0300

    Fixing copyright and added to project files

--- examples/polytope_generators/birk.cpp
+++ examples/polytope_generators/birk.cpp
@@ -1,18 +1,20 @@
-// RandGeom is free software: you can redistribute it and/or modify it
+// VolEsti
+
+// Copyright (c) 2012-2017 Vissarion Fisikopoulos
+
+// VolEsti is free software: you can redistribute it and/or modify it
 // under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or (at
 // your option) any later version.
 //
-// RandGeom is distributed in the hope that it will be useful, but WITHOUT
+// VolEsti is distributed in the hope that it will be useful, but WITHOUT
 // ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 // FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 // for more details.
 //
 // See the file COPYING.LESSER for the text of the GNU Lesser General
-// Public License.  If you did not receive this file along with RandGeom,
+// Public License.  If you did not receive this file along with HeaDDaCHe,
 // see <http://www.gnu.org/licenses/>.
-// 
-// Developer: Vissarion Fisikopoulos
 
 #include <iostream>
 #include <math.h>
--- examples/sample_simplex_main.cpp
+++ examples/sample_simplex_main.cpp
@@ -1,3 +1,21 @@
+// VolEsti
+
+// Copyright (c) 2017 Tolis Chalkis
+
+// VolEsti is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at
+// your option) any later version.
+//
+// VolEsti is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// See the file COPYING.LESSER for the text of the GNU Lesser General
+// Public License.  If you did not receive this file along with HeaDDaCHe,
+// see <http://www.gnu.org/licenses/>.
+
 /*
 
 Sampling Uniformly from Unit or an Arbitrary Simplex given in V-representation.
