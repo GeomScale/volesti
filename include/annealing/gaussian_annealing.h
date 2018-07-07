1b032bf0ddfc75bfa7e84e3f308dc573a44a6b2d
Tolis <tolis.chal@gmail.com>
2018-07-07 15:30:28 +0300

    merge cgal_remove into schedule_annealing

--- include/annealing/gaussian_annealing.h
+++ include/annealing/gaussian_annealing.h
@@ -8,13 +8,13 @@ int get_first_gaussian(T1 &K, NT &a0, NT frac, vars var){
 
     int m=K.num_of_hyperplanes(), dim=var.n;
     NT sum, lower=0.0, upper=1.0;
-    std::vector dists(m,0);
+    std::vector<NT> dists(m,0);
     for(int i=0; i<m; i++){
         sum=0.0;
         for(int j=1; j<dim+1; j++){
-            sum+=P.get_coeff(i,j)*P.get_coeff(i,j);
+            sum+=K.get_coeff(i,j)*K.get_coeff(i,j);
         }
-        dists[i]=P.get_coeff(i,0)/std::sqrt(sum);
+        dists[i]=K.get_coeff(i,0)/std::sqrt(sum);
     }
     return 1;
 }
