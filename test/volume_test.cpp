1b13775b5c21c2e2c0b80b0df1e864fc20d36472
Vissarion Fisikopoulos <fisikop@gmail.com>
2018-07-24 10:55:58 +0300

    Fixing licence details

--- test/volume_test.cpp
+++ test/volume_test.cpp
@@ -3,19 +3,7 @@
 // Copyright (c) 20012-2018 Vissarion Fisikopoulos
 // Copyright (c) 2018 Apostolos Chalkis
 
-// VolEsti is free software: you can redistribute it and/or modify it
-// under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or (at
-// your option) any later version.
-//
-// VolEsti is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-// for more details.
-//
-// See the file COPYING.LESSER for the text of the GNU Lesser General
-// Public License.  If you did not receive this file along with HeaDDaCHe,
-// see <http://www.gnu.org/licenses/>.
+// Licensed under GNU LGPL.3, see LICENCE file
 
 #include "doctest.h"
 #include <unistd.h>
