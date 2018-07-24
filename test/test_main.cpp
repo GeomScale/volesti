e54677d16a78250c80c661a1cdbb366e204f014f
Vissarion Fysikopoulos <vissarion.fysikopoulos@oracle.com>
2018-07-24 10:55:58 +0300

    Fixing licence details

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,3 +1,10 @@
+// VolEsti (volume computation and sampling library)
+
+// Copyright (c) 20012-2018 Vissarion Fisikopoulos
+// Copyright (c) 2018 Apostolos Chalkis
+
+// Licensed under GNU LGPL.3, see LICENCE file
+
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest.h"
 
