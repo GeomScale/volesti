705efe680b833cdb13932d80536712660653ae87
Tolis <tolis.chal@gmail.com>
2018-07-25 22:57:03 +0300

    create a function in solve_lp.h header file that solves the LPs for the hit and run. In Vpolytope class we define function intersect_line() and intersect_line_coord(). For the chebychev ball of the Vpoly we add a check: We check if the selected simplex is full dimensional or not (we compute the rank of the matrix).

--- external/LPsolve_src/run_headers/solve_lp.h
+++ external/LPsolve_src/run_headers/solve_lp.h
@@ -116,11 +116,9 @@ std::pair<Point,double> solveLP(std::vector<std::vector<K> > A, int d){//std::ve
 	
 	Point xc( d , temp_p.begin() , temp_p.end() );
 	double r=double(get_objective(lp));
-	
+	delete_lp(lp);
 			
 	return std::pair<Point,double> (xc,r);
-
-	
 }
 
 
@@ -240,11 +238,150 @@ bool memLP_Vpoly(std::vector<std::vector<K> > &A, Point q){
 	}
 
 	double r=double(get_objective(lp));
+	delete_lp(lp);
 	if(r>0.0){
 		return false;
 	}
 	return true;
 
+
+}
+
+
+template <typename K>
+double intersect_line_Vpoly(std::vector<std::vector<K> > &A,Point &p, Point &v, bool maxi){
+
+	int d=v.dimension();
+	//std::vector<std::vector<K> > A = P.get_matrix();
+	lprec *lp;
+	int m=A.size();
+	m++;
+	int Ncol=m, *colno = NULL, j, ret = 0;
+	REAL *row = NULL;
+	K res;
+
+	lp = make_lp(d+1, Ncol);
+
+	if(lp == NULL)
+		ret = 1; /* couldn't construct a new model... */
+
+	REAL infinite = get_infinite(lp); /* will return 1.0e30 */
+
+	//std::cout<<"ret1 = "<<ret<<std::endl;
+	if(ret == 0) {
+		/* let us name our variables. Not required, but can be useful for debugging */
+
+		//for (int i=0; i<d; i++){
+		//set_col_name(lp, i+1, "xi");
+		//set_col_name(lp, 2, "y");
+		//}
+
+		/* create space large enough for one row */
+		colno = (int *) malloc(Ncol * sizeof(*colno));
+		row = (REAL *) malloc(Ncol * sizeof(*row));
+		if((colno == NULL) || (row == NULL))
+			ret = 2;
+	}
+	//std::cout<<"ret2 = "<<ret<<std::endl;
+
+	if(ret == 0) {
+		set_add_rowmode(lp, TRUE);  /* makes building the model faster if it is done rows by row */
+	}
+	int i=0;
+	//std::cout<<"ret3 = "<<ret<<std::endl;
+
+	while(ret==0 & i<d){
+		/* construct all rows (120 x + 210 y <= 15000) */
+		//sum=K(0);
+		for(j=0; j<m-1; j++){
+			colno[j] = j+1; /* j_th column */
+			row[j] = A[j][i+1];
+			//sum += A[i][j+1]*A[i][j+1];
+		}
+		colno[m-1] = m; /* last column */
+		row[m-1] = v[i];
+		//set_bounds(lp, d, 0.0, infinite);
+
+
+		/* add the row to lpsolve */
+		if(!add_constraintex(lp, m, row, colno, EQ, p[i])){
+			ret = 3;
+		}
+		i++;
+	}
+	//std::cout<<"ret4 = "<<ret<<std::endl;
+	if(ret==0){
+		for(j=0; j<m-1; j++){
+			colno[j] = j+1; /* j_th column */
+			row[j] = 1.0;
+			//sum += A[i][j+1]*A[i][j+1];
+		}
+		colno[m-1] = m; /* last column */
+		row[m-1] = 0.0;
+		//set_bounds(lp, d, 0.0, infinite);
+
+
+		/* add the row to lpsolve */
+		if(!add_constraintex(lp, m, row, colno, EQ, 1.0)){
+			ret = 3;
+		}
+	}
+
+	//set the bounds
+	//std::cout<<"ret5 = "<<ret<<std::endl;
+	if(ret == 0) {
+		set_add_rowmode(lp, FALSE); /* rowmode should be turned off again when done building the model */
+
+		// set the objective function
+		for(j=0; j<m-1; j++){
+			colno[j] = j+1; /* j_th column */
+			row[j] = 0;
+			//set_bounds(lp, j+1, 0.0, infinite);
+		}
+		colno[m - 1] =m; /* last column */
+		row[m-1] = 1.0;
+		set_bounds(lp, m, -infinite, infinite);
+		if(!set_obj_fnex(lp, m, row, colno)){
+			ret = 4;
+		}
+
+	}
+
+	//std::cout<<"ret6 = "<<ret<<std::endl;
+	if(ret == 0) {
+		/* set the object direction to maximize */
+		if(maxi) {
+			set_maxim(lp);
+		}else{
+			set_minim(lp);
+		}
+		//set_minim(lp);
+
+		/* just out of curioucity, now show the model in lp format on screen */
+		/* this only works if this is a console application. If not, use write_lp and a filename */
+		//write_LP(lp, stdout);
+		/* write_lp(lp, "model.lp"); */
+
+		/* I only want to see important messages on screen while solving */
+		set_verbose(lp, NEUTRAL);
+
+		/* Now let lpsolve calculate a solution */
+		ret = solve(lp);
+		if(ret == OPTIMAL)
+			ret = 0;
+		else
+			ret = 5;
+	}
+	//std::cout<<"ret7 = "<<ret<<std::endl;
+	if(ret==0) {
+		res = K(-get_objective(lp));
+		delete_lp(lp);
+	    return res;
+
+	}
+	std::cout<<K(-get_objective(lp))<<std::endl;
+	exit(-1);
+	return 0;
 
 }
 
