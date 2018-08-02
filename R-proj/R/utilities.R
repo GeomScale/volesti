b097a693dd0a3f462521b986bce7f054708d79fc
tolischal <tolis.chal@gmail.com>
2018-08-02 19:54:45 +0300

    update R package documentation in rder to explain the new parameters for the CV algo. The new pdf is in doc folder.

--- R-proj/R/utilities.R
+++ R-proj/R/utilities.R
@@ -152,13 +152,21 @@ CheBall <- function(A,b){
 #' @param path The path to the ine file that describes the H-polytope. If path is given then "matrix" and "vector" inputs are not needed
 #' @param matrix The matrix A of the polytope. If it is in ine format then the input "vector" is not needed
 #' @param vector The vector b that containes the constants of the hyperplanes
-#' @param Chebychev Optional. A d+1 vector that containes the chebychev center in the first d coordinates and the radius of the chebychev ball in the last coordinate
-#' @param verbose Optional. A boolean parameter for printing. Default is False
-#' @param coordinate Optional. A boolean parameter for the hit-and-run. True for Coordinate Directions HnR, false for Random Directions HnR. Default is True
-#' @param rounding Optional. A boolean parameter to activate the rounding option. Default is False
 #' @param Walk_length Optional. Declare the number of the steps for the random walk, default is 10+d/10
-#' @param error Optional. Declare the goal for the approximation error. Default is 1
-#' @param test Optional. A boolean parameter. Declare if the current excecution is a test or not. Default is False
+#' @param error Optional. Declare the goal for the approximation error. Default is 1 for volesti and 0.2 for CV.
+#' @param Chebychev Optional. A d+1 vector that containes the chebychev center in the first d coordinates and the radius of the chebychev ball in the last coordinate
+#' @param annealing Optional. A boolean parameter to use CV algorithm. Default value is false.
+#' @param win_len Optional. The size of the window for the ratios' approximation in CV algorithm. Default value is win_len=4*(dimension^2)+500
+#' @param C Optional. a constant for the upper boud of variance/mean^2 in schedule annealing
+#' @param N optional. The number of points we sample in each step of schedule annealing in CV algorithm. Default value is N=500*C+(dimension^2)/2
+#' @param ratio Optional. parameter of schedule annealing, larger ratio means larger steps in schedule annealing. Default value is ratio=1-1/dimension
+#' @param frac Optional. the fraction of the total error to spend in the first gaussian. Default value is frac=0.1
+#' @param ball_walk Optional. Boolean parameter to use ball walk, only for CV algorithm .Default value is False
+#' @param delta Optional. The radius for the ball walk
+#' @param verbose Optional. A boolean parameter for printing. Default is False
+#' @param coordinate Optional. A boolean parameter for the hit-and-run. True for Coordinate Directions HnR, false for Random Directions HnR. Default value is True
+#' @param rounding Optional. A boolean parameter to activate the rounding option. Default value is False
+#' @param test Optional. A boolean parameter. Declare if the current excecution is a test or not. Default value is False
 #' @return The approximation of the volume of an H-polytope
 #' @examples
 #' VolEsti(list("path"=/path/to/ine/file, "verbose"=TRUE))
