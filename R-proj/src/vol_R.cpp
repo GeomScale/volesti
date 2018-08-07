5cc4ccca9de75e17a10bb8e8807f1c32d9aa6f0f
Tolis <tolis.chal@gmail.com>
2018-08-08 00:45:47 +0300

    add function sample_R in vol_R.cpp, in order to add the option of sampling to the R package. The user choose between uniform and spherical gaussian sampling from a convex H or V polytope. If gaussian is selected the variance has to be given as an input.

--- R-proj/src/vol_R.cpp
+++ R-proj/src/vol_R.cpp
@@ -13,7 +13,7 @@
 
 // [[Rcpp::plugins(cpp11)]]
 // [[Rcpp::export]]
-double vol_R (Rcpp::NumericMatrix A, int walk_len ,double e, Rcpp::NumericVector Chebychev, bool annealing, int win_len,
+double vol_R (Rcpp::NumericMatrix A, int walk_len, double e, Rcpp::NumericVector Chebychev, bool annealing, int win_len,
              int N, double C, double ratio, double frac, bool ball_walk, double delta, bool Vpoly, bool coord, bool rounding, bool verbose) {
 
     int n, nexp=1, n_threads=1,i,j;
@@ -68,11 +68,11 @@ double vol_R (Rcpp::NumericMatrix A, int walk_len ,double e, Rcpp::NumericVector
         }
     }else{ // if it is given as an input
         std::vector<NT> temp_p;
-        for (int j=0; j<P.dimension(); j++){
+        for (int j=0; j<n; j++){
           temp_p.push_back(Chebychev[j]);
         }
-        Point xc( P.dimension() , temp_p.begin() , temp_p.end() );
-        NT radius = Chebychev[P.dimension()];
+        Point xc( n , temp_p.begin() , temp_p.end() );
+        NT radius = Chebychev[n];
         CheBall.first = xc; CheBall.second = radius;
     }
     // print chebychev ball in verbose mode
@@ -109,3 +109,95 @@ double vol_R (Rcpp::NumericMatrix A, int walk_len ,double e, Rcpp::NumericVector
     
     return ((double)vol);
 }
+
+
+Rcpp::NumericMatrix sample_R(Rcpp::NumericMatrix A, int walk_len, int rnum,  Rcpp::NumericVector internal_pont, bool gaussian,
+                             double variance, bool ball_walk, double delta, bool Vpoly, bool coord, bool verbose){
+
+    bool rand_only=false, NN=false, birk=false;
+
+    HPolytope<NT> P;
+    VPolytope<NT> VP;
+
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    // the random engine with this seed
+    RNGType rng(seed);
+    //boost::normal_distribution<> rdist(0,1);
+    boost::random::uniform_real_distribution<>(urdist);
+    boost::random::uniform_real_distribution<> urdist1(-1,1);
+
+    int n=A.cols()-1, m = A.rows()-1;
+    NT a;
+    for (i=0; i<m+1; i++){
+        for(j=0; j<n+1; j++){
+            Pin[i][j]=A(i,j);
+        }
+    }
+    // construct polytope
+    if (!Vpoly) {
+        P.init(Pin);
+    } else {
+        VP.init(Pin);
+    }
+    std::pair CheBall;
+    if (!Vpoly) {
+        CheBall = P.chebyshev_center();
+    } else {
+        CheBall = VP.chebyshev_center();
+    }
+    if (internal_pont.size() != d){// if it is given as an input
+        std::vector<NT> temp_p;
+        for (int j=0; j<n; j++){
+            temp_p.push_back(internal_pont[j]);
+        }
+        //Point q( n , temp_p.begin() , temp_p.end() );
+        CheBall.first = Point( n , temp_p.begin() , temp_p.end() );
+    }
+    Point p = CheBall.first;
+
+    std::list<Point> randPoints;
+    vars var(rnum,n,walk_len,1,0.0,0.0,0,0.0,0,CheBall.second,rng,urdist,urdist1,
+             delta,verbose,rand_only,false,NN,birk,ball_walk,coord);
+    if (ball_walk) {
+        if (delta<0.0){
+            if(!gaussian) {
+                var.delta = var.delta = 4.0 * radius / std::sqrt(NT(n));
+            } else {
+                a = 1.0 / (2.0 * variance);
+                var.delta = var.delta = 4.0 * radius / std::sqrt(std::max(FT(1.0), a) * FT(n));
+            }
+        }
+    }
+    if (!Vpoly) {
+        rand_point_generator(P, p, 1, 50 * n, randPoints, var);
+    } else {
+        rand_point_generator(VP, p, 1, 50 * n, randPoints, var);
+    }
+    randPoints.clear();
+    if (!gaussian){
+        if (!Vpoly) {
+            rand_point_generator(P, p, rnum, walk_len, randPoints, var);
+        } else {
+            rand_point_generator(VP, p, rnum, walk_len, randPoints, var);
+        }
+    } else {
+        vars_g var2(n, walk_len, 0, 0, 1, 0, CheBall.second, rng, 0, 0, 0, delta, false, verbose,
+                    rand_only, false, NN, birk, ball_walk, coord);
+        rand_gaussian_point_generator(P, p, rnum, walk_len, randPoints, a, var2);
+    }
+
+    Rcpp::NumericMatrix PointSet(d,rnum);
+
+    typename std::list<Point>::iterator rpit=randPoints.begin();
+    typename std::vector<FT>::iterator qit;
+    for ( ; rpit!=randPoints.end(); rpit++, j++) {
+        qit = (*rpit).iter_begin(); i=0;
+        for ( ; qit!=(*rpit).iter_end(); qit++, i++){
+            PointSet(i,j)=*qit;
+        }
+    }
+
+    return PointSet;
+
+}
+
