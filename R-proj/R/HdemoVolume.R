5eda9f85c65b135e9e1da0829f318f3ebef39daa
Tolis <tolis.chal@gmail.com>
2018-09-06 03:11:00 +0300

    declare lpSolveAPI as suggested package. Upgrade the documentation. Fix bug in HdemoVolume function.

--- R-proj/R/HdemoVolume.R
+++ R-proj/R/HdemoVolume.R
@@ -32,15 +32,15 @@ HdemoVolume <- function(algo){
   cat('\n')
   print('H-cube10')
   PolyList = GenCube(10, 'H')
-  runtest(PolyList$matrix, PolyList$vector, FALSE, FALSE, 'H-cube10', 1024, 0.1, num_of_exps, algo)
+  runtest(PolyList$matrix, PolyList$vector, 'H-cube10', 1024, 0.1, num_of_exps, algo)
   
   print('H-cube20')
   PolyList = GenCube(20, 'H')
-  runtest(PolyList$matrix, PolyList$vector, FALSE, FALSE, 'H-cube20', 1048576, 0.1, num_of_exps, algo)
+  runtest(PolyList$matrix, PolyList$vector, 'H-cube20', 1048576, 0.1, num_of_exps, algo)
   
   print('H-cube30')
   PolyList = GenCube(30, 'H')
-  runtest(PolyList$matrix, PolyList$vector, FALSE, FALSE, 'H-cube30', 1073742000, 0.2, num_of_exps, algo)
+  runtest(PolyList$matrix, PolyList$vector, 'H-cube30', 1073742000, 0.2, num_of_exps, algo)
   
   print('----------------------------------------')
   print('------2nd test [cross_polytopes]--------')
@@ -49,7 +49,7 @@ HdemoVolume <- function(algo){
   
   print('H-cross10')
   PolyList = GenCross(10, 'H')
-  runtest(PolyList$matrix, PolyList$vector, FALSE, FALSE, 'H-cross10', 0.0002821869, 0.1, num_of_exps, algo)
+  runtest(PolyList$matrix, PolyList$vector, 'H-cross10', 0.0002821869, 0.1, num_of_exps, algo)
   
   print('----------------------------------------')
   print('----------3rd test [birkhoff]-----------')
@@ -58,23 +58,23 @@ HdemoVolume <- function(algo){
   print('H-birk3')
   A = ineToMatrix(read.csv(paste0(path,'birk3.ine')))
   x = modifyMat(A)
-  runtest(x$matrix, x$vector, FALSE, FALSE, 'H-birk3', 0.125, 0.1, num_of_exps, algo)
+  runtest(x$matrix, x$vector, 'H-birk3', 0.125, 0.1, num_of_exps, algo)
   
   print('H-birk4')
   A = ineToMatrix(read.csv(paste0(path,'birk4.ine')))
   x = modifyMat(A)
-  runtest(x$matrix, x$vector, FALSE, FALSE, 'H-birk4', 0.000970018, 0.2, num_of_exps, algo)
+  runtest(x$matrix, x$vector, 'H-birk4', 0.000970018, 0.2, num_of_exps, algo)
   
   print('H-birk5')
   A = ineToMatrix(read.csv(paste0(path,'birk5.ine')))
   x = modifyMat(A)
-  runtest(x$matrix, x$vector, FALSE, FALSE, 'H-birk5', 0.000000225, 0.2, num_of_exps, algo)
+  runtest(x$matrix, x$vector, 'H-birk5', 0.000000225, 0.2, num_of_exps, algo)
   
   print('H-birk6')
   A = ineToMatrix(read.csv(paste0(path,'birk6.ine')))
   x = modifyMat(A)
   
-  runtest(x$matrix, x$vector, FALSE, FALSE, 'H-birk6', 0.0000000000009455459196, 0.5, num_of_exps, algo)
+  runtest(x$matrix, x$vector, 'H-birk6', 0.0000000000009455459196, 0.5, num_of_exps, algo)
   
   print('----------------------------------------')
   print('--------4th test [prod_simplex]---------')
@@ -82,19 +82,19 @@ HdemoVolume <- function(algo){
   cat('\n')
   print('H-prod_simplex_5_5')
   PolyList = GenProdSimplex(5)
-  runtest(PolyList$matrix, PolyList$vector, FALSE, FALSE, 'H-prod_simplex_5_5', (1/prod(1:5))^2, 0.1, num_of_exps, algo)
+  runtest(PolyList$matrix, PolyList$vector, 'H-prod_simplex_5_5', (1/prod(1:5))^2, 0.1, num_of_exps, algo)
   
   print('H-prod_simplex_10_10')
   PolyList = GenProdSimplex(10)
-  runtest(PolyList$matrix, PolyList$vector, FALSE, FALSE, 'H-prod_simplex_10_10', (1/prod(1:10))^2, 0.1, num_of_exps, algo)
+  runtest(PolyList$matrix, PolyList$vector, 'H-prod_simplex_10_10', (1/prod(1:10))^2, 0.1, num_of_exps, algo)
   
   print('H-prod_simplex_15_15')
   PolyList = GenProdSimplex(15)
-  runtest(PolyList$matrix, PolyList$vector, FALSE, FALSE, 'H-prod_simplex_15_15', (1/prod(1:15))^2, 0.1, num_of_exps, algo)
+  runtest(PolyList$matrix, PolyList$vector, 'H-prod_simplex_15_15', (1/prod(1:15))^2, 0.1, num_of_exps, algo)
   
   print('H-prod_simplex_20_20')
   PolyList = GenProdSimplex(20)
-  runtest(PolyList$matrix, PolyList$vector, FALSE, FALSE, 'H-prod_simplex_20_20', (1/prod(1:20))^2, 0.1, num_of_exps, algo)
+  runtest(PolyList$matrix, PolyList$vector, 'H-prod_simplex_20_20', (1/prod(1:20))^2, 0.1, num_of_exps, algo)
   
   print('----------------------------------------')
   print('--------5th test [simplex]---------')
@@ -102,23 +102,23 @@ HdemoVolume <- function(algo){
   cat('\n')
   print('H-simplex10')
   PolyList = GenSimplex(10, 'H')
-  runtest(PolyList$matrix, PolyList$vector, FALSE, FALSE, 'H-simplex10', (1/prod(1:10)), 0.1, num_of_exps, algo)
+  runtest(PolyList$matrix, PolyList$vector, 'H-simplex10', (1/prod(1:10)), 0.1, num_of_exps, algo)
   
   print('H-simplex20')
   PolyList = GenSimplex(20, 'H')
-  runtest(PolyList$matrix, PolyList$vector, FALSE, FALSE, 'H-simplex20', (1/prod(1:20)), 0.1, num_of_exps, algo)
+  runtest(PolyList$matrix, PolyList$vector, 'H-simplex20', (1/prod(1:20)), 0.1, num_of_exps, algo)
   
   print('H-simplex30')
   PolyList = GenSimplex(30, 'H')
-  runtest(PolyList$matrix, PolyList$vector, FALSE, FALSE, 'H-simplex30', (1/prod(1:30)), 0.1, num_of_exps, algo)
+  runtest(PolyList$matrix, PolyList$vector, 'H-simplex30', (1/prod(1:30)), 0.1, num_of_exps, algo)
   
   print('H-simplex40')
   PolyList = GenSimplex(40, 'H')
-  runtest(PolyList$matrix, PolyList$vector, FALSE, FALSE, 'H-simplex40', (1/prod(1:40)), 0.1, num_of_exps, algo)
+  runtest(PolyList$matrix, PolyList$vector, 'H-simplex40', (1/prod(1:40)), 0.1, num_of_exps, algo)
   
   print('H-simplex50')
   PolyList = GenSimplex(50, 'H')
-  runtest(PolyList$matrix, PolyList$vector, FALSE, FALSE, 'H-simplex50', (1/prod(1:50)), 0.1, num_of_exps, algo)
+  runtest(PolyList$matrix, PolyList$vector, 'H-simplex50', (1/prod(1:50)), 0.1, num_of_exps, algo)
   
   if(algo=="SOB"){
     print('----------------------------------------')
@@ -127,17 +127,16 @@ HdemoVolume <- function(algo){
     cat('\n')
     print('H-skinny_cube10')
     PolyList = GenSkinnyCube(10)
-    runtest(PolyList$matrix, PolyList$vector, FALSE, FALSE, 'H-skinny_cube10', 102400, 0.1, num_of_exps, algo)
+    runtest(PolyList$matrix, PolyList$vector, 'H-skinny_cube10', 102400, 0.1, num_of_exps, algo)
     
     print('H-skinny_cube20')
     PolyList = GenSkinnyCube(20)
-    runtest(PolyList$matrix, PolyList$vector, FALSE, FALSE, 'H-skinny_cube20', 104857600, 0.1, num_of_exps, algo)
+    runtest(PolyList$matrix, PolyList$vector, 'H-skinny_cube20', 104857600, 0.1, num_of_exps, algo)
   }
   
 }
 
-runtest <- function(Mat, vector, Vpoly, Zono, name_string,
-                    exactvol, tol, num_of_exps, algo){
+runtest <- function(Mat, vector, name_string, exactvol, tol, num_of_exps, algo){
   
   vol = 0
   for (j in 1:num_of_exps) {
