0071df85fb4b5837687081c8f6738ceb26f9201d
tolischal <tolis.chal@gmail.com>
2018-11-12 18:38:57 +0200

    add test-scripts. add birk.ine files.

--- R-proj/script.R
+++ R-proj/script.R
@@ -6,30 +6,32 @@ time1=c()
 time2=c()
 steps1=c()
 steps2=c()
-for (i in 2:40) {
-  HP = GenCube(i,'H')
+for (i in 2:20) {
+  HP = GenCross(i,'H')
   tim=proc.time()
-  ban_vec=ban_volume(HP)
+  st1=ban_volume(HP, steps_only = TRUE)
   tim=proc.time()-tim
   tim = as.numeric(as.character(tim[3]))
   time1=c(time1, tim)
-  steps1=c(steps1, ban_vec[3])
+  steps1=c(steps1, st1)
   
   tim=proc.time()
-  cg_vec=cg_volume(HP)
+  st2=cg_volume(HP, steps_only = TRUE)
   tim=proc.time()-tim
   tim = as.numeric(as.character(tim[3]))
   time2=c(time2, tim)
-  steps2=c(steps2, cg_vec[3])
+  steps2=c(steps2, st2)
 }
 
 test.data <- data.frame(
-  dimension= 2:40,
-  ban_steps = steps1, 
-  cg_steps = steps2
+  dimension= 2:20,
+  BallAnnealing = steps1, 
+  CoolingGaussian = steps2
 )
 
+save(test.data, file = "script_data.RData")
+
 test.data %>%
-  gather(methods,steps, ban_steps, cg_steps) %>%
+  gather(methods,steps, BallAnnealing, CoolingGaussian) %>%
   ggplot(aes(x=dimension, y=steps, colour=methods)) +
   geom_line() + geom_point()
