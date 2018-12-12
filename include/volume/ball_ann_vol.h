81fc5e0e1de43b5751594a13925b63d7616efeeb
tolischal <tolis.chal@gmail.com>
2018-12-12 13:32:10 +0200

    fix round bug in volume_alg and add /lp_solve subdir to cmaketlists.txt

--- include/volume/ball_ann_vol.h
+++ include/volume/ball_ann_vol.h
@@ -69,7 +69,7 @@ NT volesti_ball_ann(Polytope &P, std::pair<Point,NT> &InnerBall, NT &lb, NT &up,
     }
 
     get_sequence_of_zonoballs<ZonoBall, RNGType>(P, BallSet, B0, ratio0, PointSets,
-                                                 ratios, Ns, nu, lb, up, InnerBall.second, var,
+                                                 ratios, Ns, nu, lb, up, radius, var,
                                                  ballsteps, steps, B0_radius, rmax);
     //var.coordinate = true;
     if(steps_only) {
