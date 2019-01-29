af2c9b99f24b307d74fdf762a51b2a59b58b34b3
tolischal <tolis.chal@gmail.com>
2019-01-29 18:01:41 +0200

    create rcpp funtions for exact volume computations, copulas, sliceofsimplex. Modify R functions for rounding, and random rotate.

--- R-proj/src/Rcopula_hyps.cpp
+++ R-proj/src/Rcopula_hyps.cpp
@@ -8,8 +8,7 @@
 //Contributed and/or modified by Apostolos Chalkis, as part of Google Summer of Code 2018 program.
 
 #include <Rcpp.h>
-#include <RcppEigen.h>
-#include "ellipsoids.h"
+//#include <RcppEigen.h>
 #include <chrono>
 #include "cartesian_geom/cartesian_kernel.h"
 #include <boost/random.hpp>
@@ -19,30 +18,31 @@
 
 // [[Rcpp::plugins(cpp11)]]
 // [[Rcpp::export]]
-Rcpp::NumericMatrix copula_hypEll (Rcpp::NumericVector hyplane, Rcpp::NumericMatrix E,
-                                   unsigned int num_slices, unsigned int numpoints){
+Rcpp::NumericMatrix copula1 (Rcpp::NumericVector h1, Rcpp::NumericVector h2,
+                             Rcpp::Nullable<unsigned int> numSlices, Rcpp::Nullable<unsigned int> N){
 
     typedef double NT;
     typedef Cartesian<NT>    Kernel;
     typedef typename Kernel::Point    Point;
     typedef boost::mt19937    RNGType;
-    typedef copula_ellipsoid<Point> CopEll;
+    unsigned int num_slices = 100, numpoints = 4000000;
+
+    if (numSlices.isNotNull()) {
+        num_slices = Rcpp::as<unsigned int>(numSlices);
+    }
+
+    if (N.isNotNull()) {
+        numpoints = Rcpp::as<unsigned int>(N);
+    }
 
     Rcpp::NumericMatrix copula(num_slices, num_slices);
     std::vector<std::vector<NT> > StdCopula;
-    unsigned int dim = hyplane.size(), i, j;
+    unsigned int dim = h1.size(), i, j;
 
-    std::vector<NT> hyp1(dim, 0.0);
-    std::vector<std::vector<NT> > Gin(dim, std::vector<NT>(dim));
+    std::vector<NT> hyp1 = Rcpp::as<std::vector<NT> >(h1);
+    std::vector<NT> hyp2 = Rcpp::as<std::vector<NT> >(h2);
 
-    for (i=0; i<dim; i++){
-        hyp1[i] = hyplane[i];
-        for(j=0; j<dim; j++){
-            Gin[i][j]=E(i,j);
-        }
-    }
-    CopEll Ell(Gin);
-    StdCopula = hypfam_ellfam<Point, RNGType >(dim, numpoints, num_slices, hyp1, Ell);
+    StdCopula = twoParHypFam<Point, RNGType >(dim, numpoints, num_slices, hyp1, hyp2);
 
     for(i=0; i<num_slices; i++) {
         for(j=0; j<num_slices; j++){
