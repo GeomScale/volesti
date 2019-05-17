5acdff2286746f6b72bb12e2931a24cab6ea5567
tolischal <tolis.chal@gmail.com>
2019-05-17 16:04:44 +0300

    implement R function compute_indicators, which takes a table of returns and compute the indicators to predict financial crisis.

---
+++ R-proj/R/compute_indicator.R
@@ -0,0 +1,49 @@
+compute_indicators <- function(MatReturns, win_len = NULL, numSlices = NULL, N = NULL) {
+  
+  if (is.null(win_len)) win_len = 60
+  if (is.null(numSlices)) numSlices = 100
+  if (is.null(N)) N = 500000
+  
+  nrows = dim(MatReturns)[1]
+  nassets = dim(MatReturns)[2]
+  wl = win_len-1
+  
+  indicators = c()
+  for (i in 1:(nrows-wl)) {
+    
+    W=i:wl
+    nR = MatReturns[W,]
+    E = cov(nR)
+    
+    compRet = rep(1,nassets)
+    for (j in 1:nassets) {
+      for (k in W) {
+        compRet[j] = compRet[j] * (1 + MatReturns[k, j])
+      }
+      compRet[j] = compRet[j] - 1
+    }
+    
+    cop = copula2(h = compRet, E = E, numSlices = numSlices, N = N)
+    blue_mass = 0
+    red_mass = 0
+    
+    for (row in 1:100) {
+      for (col in 1:100) {
+        if (row-col<=0.2*numSlices && row-col>=-0.2*numSlices) {
+          if (row+col<0.8*numSlices || row+col>1.2*numSlices) {
+            red_mass = red_mass + cop[row,col]
+          }
+        } else {
+          if (row+col>=0.8*numSlices && row+col<=1.2*numSlices) {
+            blue_mass = blue_mass + cop[row,col]
+          }
+        }
+      }
+    }
+    indicators = c(indicators, blue_mass / red_mass)
+    #print(length(indicators))
+  }
+  
+  return(indicators)
+  
+}
