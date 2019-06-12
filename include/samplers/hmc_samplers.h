f946f529d66de5aa4f9713c486eba150f24fd221
tolischal <tolis.chal@gmail.com>
2019-06-12 20:22:37 +0300

    construct the system of non linear equations and the jacobian matrix of the system. Add the stopping criterion for two consecutive vectors of an arithmetic iterative method for the solution od the system of equations.

--- include/samplers/hmc_samplers.h
+++ include/samplers/hmc_samplers.h
@@ -52,10 +52,11 @@ void hmc_logbarrier(Polytope &P, Point &p, PointList randPoints, NT &a, int n, i
     AAinv = A.inverse();
     T = T*AAinv;
     S = S*AAinv;
+    MT pinvA = A.completeOrthogonalDecomposition().pseudoInverse()
 
     for (int i = 0; i < N; ++i) {
 
-        get_next_hmc_logbarrier(x0, T, S, M, s0, sv0);
+        get_next_hmc_logbarrier<RNGType>(x0, T, S, M, s0, sv0, cj, pinvA, b, a);
 
         for (int i = 0; i < d; ++i) {
             v0(i) = rdist(rng);
@@ -69,6 +70,62 @@ void hmc_logbarrier(Polytope &P, Point &p, PointList randPoints, NT &a, int n, i
 }
 
 
+template <class RNGType, class VT, class MT, typename NT>
+void get_next_hmc_logbarrier(VT &x0, MT &T, VT &S, MT &M, VT &s0, VT &sv0, VT &cj, MT pinvA, VT &b, NT &a) {
+
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    RNGType rng(seed);
+    boost::normal_distribution<> rdist(0,1);
+
+    unsigned int m = s0.size();
+    unsigned int d = x0.size();
+    unsigned int n = cj.size()-1;
+
+    MT J = MT::Zero(m*(n+1), m*(n+1));
+    VT F = VT::Zero(M*(n+1));
+    VT s1(m*(n+1));
+    for (int j = 0; j < m*(n+1); ++j) {
+        s1 = rdist(rng);
+    }
+    VT s2 = 10*s1;
+
+    for (int i = 0; i < m; ++i) {
+        J(m * (n - 1) + i, i * (n + 1)) = 1.0;
+        for (int j = 0; j < n + 1; ++j) {
+            J(m * n + i, (i - 1) * (n + 1) + j) = S(1, j);
+        }
+
+        for (int j = 1; j < n - 1; ++j) {
+            for (int k = 0; k < n + 1; ++k) {
+                J((i - 1) * (n - 1) + j - 1, (i - 1) * (n + 1) + k) = T(j, k);
+            }
+        }
+    }
+
+    while ((s1-s2).abs().MaxCoeff()>=0.000001) {
+
+        for (int i = 0; i < m; ++i) {
+
+            F(m * (n - 1) + i) = s1((i - 1) * (n + 1)) - s0(i);
+            F(m * n + i) = S.dot(s1.segment(((i - 1) * (n + 1)), ((i - 1) * (n + 1) + n))) - sv0(i);
+
+            for (int j = 1; j < n - 1; ++j) {
+                J((i - 1) * (n - 1) + j - 1, (i - 1) * (n + 1) + j) = T(j, j) -
+                        M(i, i) * a * (1 / ((b(i) - s1((i - 1) * (n + 1) + j)) * (b(i) - s1((i - 1) * (n + 1) + j))));
+                for (int k = 0; k < m; ++k) {
+                    vec(k) = a / (b(k) - s1((k - 1) * (n + 1) + j));
+                }
+                F((i - 1) * (n - 1) + j - 1) =
+                        T.row(j).dot(s1.segment(((i - 1) * (n + 1)), ((i - 1) * (n + 1) + n))) - M.row(i).dot(vec);
+            }
+        }
+
+        update_svals(s1, s2, J, F);
+
+    }
+    
+
+}
 
 
 #endif
