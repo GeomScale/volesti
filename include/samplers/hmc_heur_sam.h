68091de3ed867eb103d24a0ed2bce094fd1ace65
Tolis <tolis.chal@gmail.com>
2019-06-17 16:14:36 +0300

    optimize the number of operations in HMC sampler

--- include/samplers/hmc_heur_sam.h
+++ include/samplers/hmc_heur_sam.h
@@ -12,7 +12,7 @@
 
 
 template <class RNGType, class VT, class MT, typename NT>
-void get_next_hmc_barrier(VT &x0, VT &s0, VT &sv0, MT &M, VT &b, MT &pinvA, NT &a) {
+void get_next_hmc_barrier(VT &x0, VT &v0, VT &s0, VT &sv0, MT &At, VT &b, MT &A, NT &a) {
 
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     RNGType rng(seed);
@@ -25,14 +25,20 @@ void get_next_hmc_barrier(VT &x0, VT &s0, VT &sv0, MT &M, VT &b, MT &pinvA, NT &
     unsigned int d = x0.size();
 
     MT abcs(m, 3);
-    abcs.col(1) = sv0;
     abcs.col(2) = s0;
+    abcs.col(1) = sv0;
 
     VT bt = b - abcs.col(2);
     for (int j = 0; j < m; ++j) bt(j) = a / bt(j);
 
-    abcs.col(0) = (0.5) * (M * bt);
-    MT pinvAabcs = pinvA*abcs;
+    abcs.col(0) = (-0.5) * (At * bt);
+
+    MT pinvAabcs(d,3);
+    pinvAabcs.col(2) = x0;
+    pinvAabcs.col(1) = v0;
+    pinvAabcs.col(0) = abcs.col(0);
+
+    abcs.col(0) = A * abcs.col(0);
     abcs.col(2) = abcs.col(2) - b;
 
     VT deltas = abcs.col(1).array()*abcs.col(1).array() - 4.0 * abcs.col(0).array() * abcs.col(2).array();
@@ -83,12 +89,11 @@ void hmc_logbarrier(Polytope &P, Point &p, PointList &randPoints, NT &a, int N) 
 
     VT s0 = A*x0;
     VT sv0 = A*v0;
-    MT M = - A*A.transpose();
-    MT pinvA = A.completeOrthogonalDecomposition().pseudoInverse();
+    MT At = A.transpose();
 
     for (int i = 0; i < N; ++i) {
 
-        get_next_hmc_barrier<RNGType>(x0, s0, sv0, M, b, pinvA, a);
+        get_next_hmc_barrier<RNGType>(x0, v0, s0, sv0, At, b, A, a);
 
         for (int i = 0; i < d; ++i) {
             v0(i) = rdist(rng);
