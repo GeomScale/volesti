fc17489469b05cad9589b4b072f0830159506827
tolischal <tolis.chal@gmail.com>
2020-02-13 15:41:04 +0200

    fix R tests for copulas and set to lowercase the inputvariables in the new functions

--- R-proj/R/compute_indicators.R
+++ R-proj/R/compute_indicators.R
@@ -4,10 +4,10 @@
 #' For each copula it computes an indicator: large value corresponds to a crisis period and a small value to a normal period. 
 #' The periods over which the indicator is greater than 1 for more than 60 consecutives sliding windows are warnings and for more than 100 are crisis. The sliding window is shifted by one day.
 #'
-#' @param MatReturns A \eqn{d}-dimensional vector that describes the direction of the first family of parallel hyperplanes.
-#' @param W Optional. The length of the sliding window. The default value is 60.
-#' @param M Optional. The number of slices for the copula. The default value is 100.
-#' @param N Optional. The number of points to sample. The default value is \eqn{5\cdot 10^5}.
+#' @param returns A \eqn{d}-dimensional vector that describes the direction of the first family of parallel hyperplanes.
+#' @param win_length Optional. The length of the sliding window. The default value is 60.
+#' @param m Optional. The number of slices for the copula. The default value is 100.
+#' @param n Optional. The number of points to sample. The default value is \eqn{5\cdot 10^5}.
 #'
 #' @references \cite{L. Cales, A. Chalkis, I.Z. Emiris, V. Fisikopoulos,
 #' \dQuote{Practical volume computation of structured convex bodies, and an application to modeling portfolio dependencies and financial crises,} \emph{Proc. of Symposium on Computational Geometry, Budapest, Hungary,} 2018.}
@@ -15,15 +15,15 @@
 #' @return A list that contains the indicators and the corresponding vector that label each time period with respect to the market state: a) normal, b) crisis, c) warning.
 #'
 #' @export
-compute_indicators <- function(MatReturns, W = NULL, M = NULL, N = NULL) {
+compute_indicators <- function(returns, win_length = NULL, m = NULL, n = NULL) {
   
-  if (is.null(W)) W = 60
-  if (is.null(M)) M = 100
-  if (is.null(N)) N = 500000
+  if (is.null(win_length)) win_length = 60
+  if (is.null(m)) m = 100
+  if (is.null(n)) n = 500000
   
-  nrows = dim(MatReturns)[1]
-  nassets = dim(MatReturns)[2]
-  wl = W-1
+  nrows = dim(returns)[1]
+  nassets = dim(returns)[2]
+  wl = win_length-1
   
   indicators = c()
   #print(nrows-wl)
@@ -31,47 +31,47 @@ compute_indicators <- function(MatReturns, W = NULL, M = NULL, N = NULL) {
     
     Win=i:(i+wl)
     #if("tawny" %in% rownames(installed.packages()) == FALSE && FALSE) {
-    E = cov(MatReturns[Win,])
+    #E = cov(returns[Win,])
     #} else {
-      #E = tawny::cov.shrink(MatReturns[Win,])
+    E = tawny::cov.shrink(returns[Win,])
     #}
     
     compRet = rep(1,nassets)
     for (j in 1:nassets) {
       for (k in Win) {
-        compRet[j] = compRet[j] * (1 + MatReturns[k, j])
+        compRet[j] = compRet[j] * (1 + returns[k, j])
       }
       compRet[j] = compRet[j] - 1
     }
     
-    cop = copula(R1 = compRet, Sigma = E, M = M, N = N)
+    cop = copula(r1 = compRet, sigma = E, m = m, n = n)
     blue_mass = 0
     red_mass = 0
     
-    for (row in 1:M) {
-      for (col in 1:M) {
-        if (row-col<=0.2*M && row-col>=-0.2*M) {
-          if (row+col<0.8*M || row+col>1.2*M) {
+    for (row in 1:m) {
+      for (col in 1:m) {
+        if (row-col<=0.2*m && row-col>=-0.2*m) {
+          if (row+col<0.8*m || row+col>1.2*m) {
             red_mass = red_mass + cop[row,col]
           }
         } else {
-          if (row+col>=0.8*M && row+col<=1.2*M) {
+          if (row+col>=0.8*m && row+col<=1.2*m) {
             blue_mass = blue_mass + cop[row,col]
           }
         }
       }
     }
-    #indicators = c(indicators, blue_mass / red_mass)
-    print(length(indicators))
+    indicators = c(indicators, blue_mass / red_mass)
+    #print(length(indicators))
   }
 
-  n = length(indicators)
+  N = length(indicators)
 
   index = 0
   set_index = FALSE
-  col = rep("normal",n)
+  col = rep("normal", N)
 
-  for (i in 1:n) {
+  for (i in 1:N) {
   
     if(indicators[i]>1 && !set_index){
       index = i
--- R-proj/src/copula.cpp
+++ R-proj/src/copula.cpp
@@ -21,11 +21,11 @@
 //'
 //' Given two families of parallel hyperplanes or a family of parallel hyperplanes and a family of concentric ellispoids centered at the origin intersecting the canonical simplex, this function uniformly samples from the canonical simplex and construct an approximation of the bivariate probability distribution, called copula.
 //'
-//' @param h1 A \eqn{d}-dimensional vector that describes the direction of the first family of parallel hyperplanes.
-//' @param h2 Optional. A \eqn{d}-dimensional vector that describes the direction of the second family of parallel hyperplanes.
-//' @param E Optional. The \eqn{d\times d} symmetric positive semidefine matrix that describes the family of concentric ellipsoids centered at the origin.
-//' @param M The number of the slices for the copula. The default value is 100.
-//' @param N The number of points to sample. The default value is \eqn{5\cdot 10^5}.
+//' @param r1 A \eqn{d}-dimensional vector that describes the direction of the first family of parallel hyperplanes.
+//' @param r2 Optional. A \eqn{d}-dimensional vector that describes the direction of the second family of parallel hyperplanes.
+//' @param sigma Optional. The \eqn{d\times d} symmetric positive semidefine matrix that describes the family of concentric ellipsoids centered at the origin.
+//' @param m The number of the slices for the copula. The default value is 100.
+//' @param n The number of points to sample. The default value is \eqn{5\cdot 10^5}.
 //'
 //' @references \cite{L. Cales, A. Chalkis, I.Z. Emiris, V. Fisikopoulos,
 //' \dQuote{Practical volume computation of structured convex bodies, and an application to modeling portfolio dependencies and financial crises,} \emph{Proc. of Symposium on Computational Geometry, Budapest, Hungary,} 2018.}
@@ -37,22 +37,22 @@
 //' h1 = h1 / 1000
 //' h2=runif(n = 10, min = 1, max = 1000)
 //' h2 = h2 / 1000
-//' cop = copula(R1 = h1, R2 = h2, M = 10, N = 100000)
+//' cop = copula(r1 = h1, r2 = h2, m = 10, n = 100000)
 //'
 //' # compute a copula for a family of parallel hyperplanes and a family of conentric ellipsoids
 //' h = runif(n = 10, min = 1, max = 1000)
 //' h = h / 1000
 //' E = replicate(10, rnorm(20))
 //' E = cov(E)
-//' cop = copula(R1 = h, Sigma = E, M = 10, N = 100000)
+//' cop = copula(r1 = h, sigma = E, m = 10, n = 100000)
 //'
 //' @export
 // [[Rcpp::export]]
-Rcpp::NumericMatrix copula (Rcpp::Nullable<Rcpp::NumericVector> R1 = R_NilValue,
-                            Rcpp::Nullable<Rcpp::NumericVector> R2 = R_NilValue,
-                            Rcpp::Nullable<Rcpp::NumericMatrix> Sigma = R_NilValue,
-                            Rcpp::Nullable<unsigned int> M = R_NilValue,
-                            Rcpp::Nullable<unsigned int> N = R_NilValue){
+Rcpp::NumericMatrix copula (Rcpp::Nullable<Rcpp::NumericVector> r1 = R_NilValue,
+                            Rcpp::Nullable<Rcpp::NumericVector> r2 = R_NilValue,
+                            Rcpp::Nullable<Rcpp::NumericMatrix> sigma = R_NilValue,
+                            Rcpp::Nullable<unsigned int> m = R_NilValue,
+                            Rcpp::Nullable<unsigned int> n = R_NilValue){
 
     typedef double NT;
     typedef Cartesian<NT>    Kernel;
@@ -63,34 +63,34 @@ Rcpp::NumericMatrix copula (Rcpp::Nullable<Rcpp::NumericVector> R1 = R_NilValue,
     typedef copula_ellipsoid<Point, MT, VT> CopEll;
     unsigned int num_slices = 100, numpoints = 500000;
 
-    if (M.isNotNull()) {
-        num_slices = Rcpp::as<unsigned int>(M);
+    if (m.isNotNull()) {
+        num_slices = Rcpp::as<unsigned int>(m);
     }
 
-    if (N.isNotNull()) {
-        numpoints = Rcpp::as<unsigned int>(N);
+    if (n.isNotNull()) {
+        numpoints = Rcpp::as<unsigned int>(n);
     }
 
     Rcpp::NumericMatrix copula(num_slices, num_slices);
     std::vector<std::vector<NT> > StdCopula;
-    unsigned int dim = Rcpp::as<std::vector<NT> >(R1).size(), i, j;
+    unsigned int dim = Rcpp::as<std::vector<NT> >(r1).size(), i, j;
 
-    if(!R1.isNotNull()) {
+    if(!r1.isNotNull()) {
 
         throw Rcpp::exception("You have to give at least one normal of a hyperplane!");
 
     }
 
-    std::vector<NT> hyp1 = Rcpp::as<std::vector<NT> >(R1);
-    if (R2.isNotNull()) {
+    std::vector<NT> hyp1 = Rcpp::as<std::vector<NT> >(r1);
+    if (r2.isNotNull()) {
 
-        std::vector <NT> hyp2 = Rcpp::as < std::vector < NT > > (R2);
+        std::vector <NT> hyp2 = Rcpp::as < std::vector < NT > > (r2);
         StdCopula = twoParHypFam<Point, RNGType>(dim, numpoints, num_slices, hyp1, hyp2);
 
-    } else if (Sigma.isNotNull()) {
+    } else if (sigma.isNotNull()) {
 
         std::vector<std::vector<NT> > Gin(dim, std::vector<NT>(dim));
-        MT EE = Rcpp::as<MT>(Sigma);
+        MT EE = Rcpp::as<MT>(sigma);
         for (int i=0; i<dim; i++) {
             for (int j = 0; j < dim; j++) {
                 Gin[i][j] = EE(i, j);
