6d3be5f148c0971285511285aeb67d65ca9a08e1
Dani Pozo <danipozo@correo.ugr.es>
2020-04-07 20:39:37 +0200

    Update get_direction not to initialize a new PRNG on every call
    get_direction used a new PRNG in every call, badly impacting performance.

--- include/samplers/samplers.h
+++ include/samplers/samplers.h
@@ -13,14 +13,11 @@
 
 // Pick a random direction as a normilized vector
 template <typename RNGType, typename Point, typename NT>
-Point get_direction(const unsigned int dim) {
+Point get_direction(const unsigned int dim, RNGType& rng) {
 
     boost::normal_distribution<> rdist(0,1);
     std::vector<NT> Xs(dim,0);
     NT normal = NT(0);
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    RNGType rng(seed);
-    //RNGType rng2 = var.rng;
     for (unsigned int i=0; i<dim; i++) {
         Xs[i] = rdist(rng);
         normal += Xs[i] * Xs[i];
@@ -37,8 +34,8 @@ Point get_direction(const unsigned int dim) {
 
 // Pick a random point from a d-sphere
 template <typename RNGType, typename Point, typename NT>
-Point get_point_on_Dsphere(const unsigned int dim, const NT &radius){
-    Point p = get_direction<RNGType, Point, NT>(dim);
+Point get_point_on_Dsphere(const unsigned int dim, const NT &radius, RNGType& rng){
+  Point p = get_direction<RNGType, Point, NT>(dim, rng);
     p = (radius == 0) ? p : radius * p;
     return p;
 }
@@ -46,14 +43,12 @@ Point get_point_on_Dsphere(const unsigned int dim, const NT &radius){
 
 // Pick a random point from a d-ball
 template <typename RNGType, typename Point, typename NT>
-Point get_point_in_Dsphere(const unsigned int dim, const NT &radius){
+Point get_point_in_Dsphere(const unsigned int dim, const NT &radius, RNGType& rng){
 
     boost::random::uniform_real_distribution<> urdist(0,1);
     NT U;
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    RNGType rng2(seed);
-    Point p = get_direction<RNGType, Point, NT>(dim);
-    U = urdist(rng2);
+    Point p = get_direction<RNGType, Point, NT>(dim, rng);
+    U = urdist(rng);
     U = std::pow(U, 1.0/(NT(dim)));
     p = (radius*U)*p;
     return p;
