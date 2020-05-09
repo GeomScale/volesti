cca8900d90389e790785f56eb27833b6ec4642c5
Marios Papachristou <papachristoumarios@gmail.com>
2020-05-09 16:36:29 +0300

    Add boundary reflections to Euler

--- include/ode_solvers/ode_solvers.h
+++ include/ode_solvers/ode_solvers.h
@@ -33,39 +33,103 @@ see <http://www.gnu.org/licenses/>.
 #ifndef ODE_SOLVERS_H
 #define ODE_SOLVERS_H
 
-
 template <typename Point, typename NT>
+class HamiltonianGrads {
+public:
+  typedef std::vector<Point> pts;
+  typedef std::function<Point(pts, NT)> func;
+  typedef std::vector<func> funcs;
+
+  funcs Fs;
+
+  HamiltonianGrads(func neg_grad_U) {
+    // Oracle for position update dx / dt = v
+    func grad_K = [](pts x, NT t) { return x[1]; };
+    Fs.push_back(grad_K);
+    // Oracle for velocity update dv / dt = - grad f
+    Fs.push_back(neg_grad_U);
+  }
+
+  HamiltonianGrads() {
+    // Oracle for position update dx / dt = v
+    func grad_K = [](pts x, NT t) { return x[1]; };
+    Fs.push_back(grad_K);
+    // Oracle for velocity update dv / dt = - x
+    // Uses
+    func neg_grad_U = [](pts x, NT t) { return (-1.0) * x[0]; };
+    Fs.push_back(neg_grad_U);
+  }
+
+  func get_grad_K() {
+    return Fs[0];
+  }
+
+  func get_neg_grad_U() {
+    return Fs[1];
+  }
+
+};
+
+
+template <typename Point, typename NT, class Polytope>
 class EulerODESolver {
 public:
-  typedef std::function <Point(Point, NT)> funcs;
-  // Polytope *K;
+  typedef std::vector<Point> pts;
+  typedef std::function <Point(pts, NT)> func;
+  typedef std::vector<func> funcs;
+  typedef std::vector<Polytope*> bounds;
+
   NT h;
   NT t;
-  unsigned int T;
   unsigned int counter;
 
-  funcs F;
+  funcs Fs;
+  bounds Ks;
+
 
   // Contains the sub-states
-  Point x;
+  pts xs;
 
-  EulerODESolver(NT initial_time, unsigned int total_steps, NT step, Point initial_state, funcs oracles) :
-    t(initial_time), T(total_steps), x(initial_state), F(oracles), counter(0), h(step) {};
+  EulerODESolver(NT initial_time, NT step, pts initial_state, funcs oracles, bounds boundaries) :
+    t(initial_time), xs(initial_state), Fs(oracles), counter(0), h(step), Ks(boundaries) {};
+
+  EulerODESolver(NT initial_time, NT step, pts initial_state, funcs oracles) :
+    t(initial_time), xs(initial_state), Fs(oracles), counter(0), h(step) {
+      Ks = bounds(xs.size(), NULL);
+    };
+
 
   void step() {
     t += h;
-    Point y = F(x, t);
-    y = h * y;
-    x = x + y;
 
-    std::cout << x[0] << std::endl;
+    for (unsigned int i = 0; i < xs.size(); i++) {
+      Point y = Fs[i](xs, t);
+      y = h * y;
 
+      if (Ks[i] == NULL) {
+        xs[i] = xs[i] + y;
+      }
+      else {
+        Point v = xs[i] + y;
+        std::pair<NT, int> pbpair = Ks[i]->line_positive_intersect(xs[i], v);
+        std::cout << xs[i][0] << " " <<  pbpair.first << " " << pbpair.second << std::endl;
+        if (pbpair.first < 0) {
+          std::cout << "OUTSIDE " << std::endl;
+          Point z = pbpair.first * xs[i] + (1 - pbpair.first) * v;
+          Ks[i]->compute_reflection(v, z, 0);
+          xs[i] = v;
+        } else {
+          xs[i] = v;
+        }
+      }
+
+    }
     // TODO add boundary reflections
     counter++;
   }
 
-  void steps() {
-    while (counter < T) step();
+  void steps(int num_steps) {
+    for (int i = 0; i < num_steps; i++) step();
   }
 
 };
