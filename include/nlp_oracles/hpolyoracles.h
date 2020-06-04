b73cff787ba8ec7cfd430ba0e08cdadb76b10b39
Marios Papachristou <papachristoumarios@gmail.com>
2020-06-04 17:57:37 +0300

    Initialize NLP oracles (general curve w/ polytope)

---
+++ include/nlp_oracles/hpolyoracles.h
@@ -0,0 +1,17 @@
+#ifndef NLP_HPOLYORACLES_H
+#define NLP_HPOLYORACLES_H
+
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ifopt/variable_set.h>
+#include <ifopt/constraint_set.h>
+#include <ifopt/cost_term.h>
+#include <ifopt/problem.h>
+#include <ifopt/ipopt_solver.h>
+
+
+
+
+
+#endif
