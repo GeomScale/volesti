4febd78bf73ed0c86bda3375b69651105d770da9
Marios Papachristou <papachristoumarios@gmail.com>
2020-06-04 21:05:47 +0300

    Finish boundary oracle tests for H-polytope

--- test/boundary_oracles_test.cpp
+++ test/boundary_oracles_test.cpp
@@ -43,18 +43,29 @@ see <http://www.gnu.org/licenses/>.
 #include "doctest.h"
 
 template <typename NT, class Point, class bfunc>
-void test_h_poly_oracles(std::vector<Point> coeffs, bfunc phi, bfunc grad_phi) {
+void test_h_poly_oracles(std::vector<Point> coeffs, bfunc phi, bfunc grad_phi, NT t_des, int facet_des) {
   typedef boost::mt19937    RNGType;
   typedef HPolytope<Point> Hpolytope;
-  typedef std::vector<std::pair<NT, int>> results;
+  typedef std::tuple<NT, Point, int> result;
   Hpolytope P;
+  NT tol = 1e-6;
 
   P = gen_cube<Hpolytope>(2, false);
 
-  results res = P.curve_intersect(0.01, 0, coeffs, phi, grad_phi);
+  result res = P.curve_intersect_newton_raphson(0.01, 0, coeffs, phi, grad_phi);
+  NT t = std::get<0>(res);
+  int facet = std::get<2>(res);
 
-  std::cout << "t = " << res[0].first << " facet = " << res[0].second << std::endl;
+  CHECK(facet == facet_des);
+  CHECK(std::abs(std::abs(t) - t_des) / t_des < tol);
 
+  result res2 = P.curve_intersect_ipopt(0.01, 0, coeffs, phi, grad_phi);
+
+  t = std::get<0>(res2);
+  facet = std::get<2>(res2);
+
+  CHECK(facet == facet_des);
+  CHECK(std::abs(std::abs(t) - t_des) / t_des < tol);
 
 }
 
@@ -82,7 +93,10 @@ void call_test_h_poly_oracles() {
     return ((NT) j) * pow(t - t0, (NT) (j - 1));
   };
 
-  test_h_poly_oracles<NT, Point, bfunc>(line_coeffs, poly_basis, poly_basis_grad);
+  NT t_des_line = NT(1);
+  int facet_des_line = 0;
+
+  test_h_poly_oracles<NT, Point, bfunc>(line_coeffs, poly_basis, poly_basis_grad, t_des_line, facet_des_line);
 
   std::cout << "--- Testing intersection of 2D H-cube with p(t) = (t, 2 * t^2)" << std::endl;
 
@@ -92,9 +106,11 @@ void call_test_h_poly_oracles() {
   b1.set_coord(0, 1);
   b2.set_coord(1, 2);
 
+  NT t_des_parabola = NT(1 / sqrt(2));
+  int facet_des_parabola = 1;
   pts parabola_coeffs{b0, b1, b2};
 
-  test_h_poly_oracles<NT, Point, bfunc>(parabola_coeffs, poly_basis, poly_basis_grad);
+  test_h_poly_oracles<NT, Point, bfunc>(parabola_coeffs, poly_basis, poly_basis_grad, t_des_parabola, facet_des_parabola);
 
 
 }
