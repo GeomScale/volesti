67450267dbcd0bb11ff467fa903819c569d602c1
hariszaf <haris-zaf@hcmr.gr>
2020-06-16 18:15:55 +0300

    Update bindings.h

--- volestipy/volestipy/include/bindings.h
+++ volestipy/volestipy/include/bindings.h
@@ -3,13 +3,23 @@
 
 
 #include "Eigen/Eigen"
-#include "volume.h"
-#include "sampling.hpp"
+
+
+// altered by Haris at 2020_06_08
+//#include "volume.h"
+//#include "sampling.hpp"
+#include "cartesian_kernel.h"
+
 
 
 class HPolytopeCPP{
    public:
+
+// ATTENTION! Only one of the following should remain. I need to talk to Tolis about this.
+      //template <typename NT>;
       typedef double NT;
+
+
       typedef Cartesian<NT>    Kernel;
       typedef typename Kernel::Point    Point;
       typedef boost::mt19937    RNGType;
@@ -24,7 +34,8 @@ class HPolytopeCPP{
       Hpolytope HP;
       std::pair<Point,NT> CheBall;
       ~HPolytopeCPP();
-      double compute_volume(char* method, int walk_len, double epsilon, uint seed);
+      //double compute_volume(char* method, int walk_len, double epsilon, uint seed);
+      double compute_volume(char* vol_method, char* walk_method, int walk_len, double epsilon, uint seed);
       void generate_samples(int walk_len, int n_samples, double* samples, uint seed);
 
 };
