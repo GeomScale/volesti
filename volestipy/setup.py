eb0fbe60a0c185436266c33b329823fee7f3a34f
hariszaf <haris-zaf@hcmr.gr>
2020-06-17 16:53:08 +0300

    Update setup.py

--- volestipy/setup.py
+++ volestipy/setup.py
@@ -5,20 +5,14 @@ from os.path import join
 import numpy
 
 
-version = "0.2.0"
+version = "0.3.0"
 license='LGPL3',
 packages = ["volestipy"]
 description="volestipy: wrapper for the VolEsti library to sample from convex sets and compute volume."
 author = "Haris Zafeiropoulos"
 author_email="haris-zaf@hcmr.gr"
 name = 'volestipy'
-# scripts = []
-# url = ""
-# download_url = ""
-# classifiers = []
 zip_safe=False
-
-
 
 source_directory_list = ['volestipy', join('volestipy','src')]
 
@@ -36,23 +30,59 @@ extra_volesti_include_dirs = [
 	join("volestipy","include"),
 
 # the volesti code uses some external classes. these are located on the "external" directory and we need to add them as well
-	join("..","external")
-	join("..","external","Eigen")
-	join("..","external","boost")
-	join("..","external","boost","random")
-	join("..","external","")
-
+	# join("..","external"),
+	join("..","external"),
+	join("..","external","Eigen"),
+	join("..","external","boost"),
+	join("..","external","boost","random"),
+	join("..","external","LPsolve_src","include"), # ATTENTION HERE! I do not know if needed.
+	join("..","external","minimum_ellipsoid"),
+	
+# in the same way, there is a "doctest.h" file in the "tests" directory - better to discuss if this is needed
+	join("..","test"),
 
 # finally, we also move back and include and add the directories on the "include" directory (generatorors, random_walks, sampling etc)
 	join("..","include"),
-	join("..","include", "misc"),
-	join("..","include", "random_walks"),
-	join("..","include", "volume"),
-	join("..","include","generators"),	
+	join("..","include","convex"),
+	join("..","include","misc"),
+	join("..","include","random_walks"),
+	join("..","include","volume"),
+	join("..","include","generators"),
+	join("..","include","cartesian_geom"),
 ]
 
+src_files = ["volestipy/volestipy.pyx","volestipy/src/bindings.cpp"]
+extra_include_dirs = [numpy.get_include()] 
+# Return the directory that contains the NumPy *.h header files.
+# Extension modules that need to compile against NumPy should use this function to locate the appropriate include directory.
+
+# here I need to check how it is actually included this library
+library_includes = ["lpsolve55"]
+
+ext_module = Extension(
+    "volestipy",
+    language = "c++",
+    sources = src_files,
+    include_dirs = extra_include_dirs + extra_volesti_include_dirs,
+    libraries = library_includes,
+    extra_compile_args = compiler_args,
+)
+
+print("The Extension function is OK.")
 
 
+ext_modules = cythonize([ext_module], gdb_debug=False)
 
+print("The cythonize function ran fine!")
 
+setup(
+    version = version,
+    author = author,
+    author_email = author_email,
+    name = name,
+    packages = packages,
+    ext_modules = ext_modules,
+    zip_safe=zip_safe,
+)
 
+print("Finally, the setup function was performed. We ve got out interface.")
