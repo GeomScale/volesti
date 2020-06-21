9254c29d1605338127cbac14fcd8cdf3a9c079d9
AlexManochis <alex.manochis@gmail.com>
2020-06-21 17:43:58 +0300

    implement multivariate gweke diagnostics

---
+++ include/diagnostics/geweke.hpp
@@ -0,0 +1,70 @@
+// VolEsti is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at
+// your option) any later version.
+//
+// VolEsti is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// See the file COPYING.LESSER for the text of the GNU Lesser General
+// Public License.  If you did not receive this file along with HeaDDaCHe,
+// see <http://www.gnu.org/licenses/>.
+
+
+#ifndef PRSF_HPP
+#define PRSF_HPP
+
+template <typename NT, typename MT, typename VT, typename Point, typename PointList>
+bool perform_geweke(PointList &point_list, NT frac1, NT frac2)
+{
+    typename typename Point::FT NT;
+    typename std::list<Point>::iterator rpit = point_list.begin();
+
+    unsigned int d = (*rpit).dimension(), N = point_list.size();
+    unsigned int N1 = N*frac1;
+    unsigned int N2 = N*frac2;
+
+    MT sigma1 = MT::(d,d), sigma2 = MT::(d,d), samples(d,N);
+    VT mean1 = VT::Zero(d), mean2 = VT::Zero(d);
+    NT alpha = 0.05;
+
+    unsigned int jj = 0;
+    for (typename std::list<Point>::iterator rpit = randPoints.begin(); rpit!=randPoints.end(); rpit++, jj++) {
+        samples.col(jj) = (*rpit).getCoefficients();
+    }
+
+    for (int i = 0; i < N1; ++i) {
+        mean1 += samples.col(i);
+    }
+    mean1 = mean1 / NT(N1);
+
+    for (int i = 0; i < N1; ++i) {
+        sigma1 = sigma1 + (samples.col(i) - mean1) * (samples.col(i) - mean1).transpose();
+    }
+    sigma1 = sigma1 / (NT(N1) - 1.0);
+
+
+    for (int i = N-N2; i < N; ++i) {
+        mean2 += samples.col(i);
+    }
+    mean2 = mean2 / NT(N2);
+
+    for (int i = N-N2; i < N; ++i) {
+        sigma2 = sigma2 + (samples.col(i) - mean2) * (samples.col(i) - mean2).transpose();
+    }
+    sigma2 = sigma2 / (NT(N2) - 1.0);
+
+    MT S_pl = ((NT(N1) - 1.0)*sigma1 + (NT(N2) - 1.0)*sigma2) / (NT(N1) + NT(N2) - 2.0);
+
+    NT T2 = ((NT(N1)*NT(N2))/(NT(N1) + NT(N2))) * ((mean1 - mean2).transpose() * S_pl.inverse() * (mean1 - mean2));
+
+    NT U = ((NT(N1) + NT(N2) - NT(d) - 1.0) / ((NT(N1) + NT(N2) - 2.0)*NT(d))) * T;
+
+    if (U > boost::quantile(fisher_f(d, N1 + N2 - d - 1), alpha)) {
+        return false;
+    }
+    return true;
+
+}
