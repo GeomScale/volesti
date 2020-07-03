5a010eccd14a04ed6d29c22eda4300628d369b81
AlexManochis <alex.manochis@gmail.com>
2020-07-03 22:49:18 +0300

    implement thin subroutine for raftery diagnostic

---
+++ include/diagnostics/raftery_subroutines/thin.hpp
@@ -0,0 +1,30 @@
+// VolEsti (volume computation and sampling library)
+
+// Copyright (c) 2012-2020 Vissarion Fisikopoulos
+// Copyright (c) 2018-2020 Apostolos Chalkis
+
+//Contributed and/or modified by Alexandros Manochis, as part of Google Summer of Code 2020 program.
+
+// Licensed under GNU LGPL.3, see LICENCE file
+
+#ifndef THIN_HPP
+#define THIN_HPP
+
+template <typename NT, typename VT, typename MT>
+std::pair<int,VT>  thin(MT work, unsigned int n, unsigned int kthin)
+{
+    VT y((n-1) / kthin + 1);
+
+    int i = 0;
+    while (i < n)
+    {
+        y(i) = work(i,0);
+        i += kthin;
+    }
+
+    return std::pair<int,VT>((n-1) / kthin + 1, y);
+}
+
+
+
+#endif
