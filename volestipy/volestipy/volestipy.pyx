4e1dda51fbc871854d8288a18ebf22a91b111ce4
hariszaf <haris-zaf@hcmr.gr>
2020-07-08 12:04:02 +0300

    Update volestipy.pyx

--- volestipy/volestipy/volestipy.pyx
+++ volestipy/volestipy/volestipy.pyx
@@ -25,12 +25,14 @@ cdef extern from "bindings.h":
 
       double compute_volume(char* vol_method, char* walk_method, int walk_len, double epsilon, int seed);     
 
-      double generate_samples(int walk_len, int number_of_points, int number_of_points_to_burn, bool boundary, bool cdhr, bool rdhr, bool gaussian, bool set_L, bool billiard, bool ball_walk, double a, double L,  double* samples);
+      double generate_samples(int walk_len, int number_of_points, int number_of_points_to_burn, bool boundary, \
+         bool cdhr, bool rdhr, bool gaussian, bool set_L, bool billiard, bool ball_walk, double a, double L,  double* samples);
 
 
 # with respect to the "compute_volume" def
 volume_methods = ["sequence_of_balls".encode("UTF-8"), "cooling_gaussian".encode("UTF-8"), "cooling_balls".encode("UTF-8")]
-walk_methods = ["uniform_ball".encode("UTF-8"), "CDHR".encode("UTF-8"), "RDHR".encode("UTF-8"), "gaussian_ball".encode("UTF-8"), "gaussian_CDHR".encode("UTF-8"), "gaussian_RDHR".encode("UTF-8"), "uniform_ball".encode("UTF-8"), "billiard".encode("UTF-8")]
+walk_methods = ["uniform_ball".encode("UTF-8"), "CDHR".encode("UTF-8"), "RDHR".encode("UTF-8"), "gaussian_ball".encode("UTF-8"), \
+                "gaussian_CDHR".encode("UTF-8"), "gaussian_RDHR".encode("UTF-8"), "uniform_ball".encode("UTF-8"), "billiard".encode("UTF-8")]
 
 
 cdef class HPolytope:
@@ -44,7 +46,8 @@ cdef class HPolytope:
       n_hyperplanes, n_variables = A.shape[0], A.shape[1]
       self.polytope_cpp = HPolytopeCPP(&A[0,0], &b[0], n_hyperplanes, n_variables)
 
-   def compute_volume(self, walk_len=2, epsilon=0.05, vol_method="sequence_of_balls", walk_method="uniform_ball", np.npy_int32 seed=get_time_seed()):
+   def compute_volume(self, walk_len=2, epsilon=0.05, vol_method="sequence_of_balls", walk_method="uniform_ball", \
+      np.npy_int32 seed=get_time_seed()):
       vol_method = vol_method.encode("UTF-8")
       walk_method = walk_method.encode("UTF-8")
       if vol_method  in volume_methods:
@@ -57,7 +60,7 @@ cdef class HPolytope:
 
 
 # we need to build a Python function for getting a starting point depending on the polytope 
-   def generate_samples(self, walk_len=1, number_of_points=1000, number_of_points_to_burn=100, boundary=True, cdhr=True, rdhr=False, gaussian=False, set_L=False, billiard=False, ball_walk=False, a=0, L=0):
+   def generate_samples(self, walk_len=1, number_of_points=1000, number_of_points_to_burn=100, boundary=False, cdhr=True, rdhr=False, gaussian=False, set_L=False, billiard=False, ball_walk=False, a=0, L=0):
       n_variables = self._A.shape[1]
       cdef double[:,::1] samples = np.zeros((number_of_points,  n_variables), dtype=np.float64, order="C")
       
