3351d647436affe0331fe87cda755018d0b00644
hariszaf <haris-zaf@hcmr.gr>
2020-07-08 12:26:19 +0300

    Update setup.py

--- volestipy/setup.py
+++ volestipy/setup.py
@@ -1,5 +1,7 @@
-from setuptools import setup
-from setuptools import Extension
+# from setuptools import setup
+# from setuptools import Extension
+from distutils.core import setup
+from distutils.core import Extension
 from Cython.Build import cythonize
 from os.path import join
 import numpy
@@ -9,40 +11,40 @@ version = "0.3.0"
 license='LGPL3',
 packages = ["volestipy"]
 description="volestipy: wrapper for the VolEsti library to sample from convex sets and compute volume."
-author = "Haris Zafeiropoulos"
-author_email="haris-zaf@hcmr.gr"
+author = "Pedro Zuidberg Dos Martires, Haris Zafeiropoulos"
+author_email="pedro.zuidbergdosmartires@cs.kuleuven.be, haris-zaf@hcmr.gr"
 name = 'volestipy'
 zip_safe=False
 
 source_directory_list = ['volestipy', join('volestipy','src')]
 
 compiler_args = [
-	"-std=c++11",
-	"-O3",
-	"-lm",
-	"-ldl",
-	"-DBOOST_NO_AUTO_PTR",
+ "-std=c++11",
+ "-O3",
+ "-lm",
+ "-ldl",
+ "-DBOOST_NO_AUTO_PTR",
 ]
 
 
 extra_volesti_include_dirs = [
 # inside the volestipy directory, there is an extra volestipy folder containing a "include" folder; we add those as included dirs; the bindings.h file is located there
-	join("volestipy","include"),
+ join("volestipy","include"),
 
 # the volesti code uses some external classes. these are located on the "external" directory and we need to add them as well
-    join("..","external"),
-    join("..","external","minimum_ellipsoid"),
-    join("..","external","LPsolve_src","run_headers"),
-    join("..","external","boost"),
+ join("..","external"),
+ join("..","external","minimum_ellipsoid"),
+ join("..","external","LPsolve_src","run_headers"),
+ join("..","external","boost"),
 
-# finally, we also move back and include and add the directories on the "include" directory (generatorors, random_walks, sampling etc)
-	join("..","include"),
-	join("..","include","convex"),
-	join("..","include","misc"),
-	join("..","include","random_walks"),
-	join("..","include","volume"),
-	join("..","include","generators"),
-	join("..","include","cartesian_geom"),
+# we also move back and include and add the directories on the "include" directory (generatorors, random_walks, sampling etc)
+ join("..","include"),
+ join("..","include","convex"),
+ join("..","include","misc"),
+ join("..","include","random_walks"),
+ join("..","include","volume"),
+ join("..","include","generators"),
+ join("..","include","cartesian_geom"), 
 ]
 
 
@@ -55,12 +57,12 @@ extra_include_dirs = [numpy.get_include()]
 library_includes = ["lpsolve55"]
 
 ext_module = Extension(
-    "volestipy",
-    language = "c++",
-    sources = src_files,
-    include_dirs = extra_include_dirs + extra_volesti_include_dirs,
-    libraries = library_includes,
-    extra_compile_args = compiler_args,
+ "volestipy",
+ language = "c++",
+ sources = src_files,
+ include_dirs = extra_include_dirs + extra_volesti_include_dirs,
+ libraries = library_includes,
+ extra_compile_args = compiler_args,
 )
 print("The Extension function is OK.")
 
@@ -69,13 +71,13 @@ ext_modules = cythonize([ext_module], gdb_debug=False)
 print("The cythonize function ran fine!")
 
 setup(
-    version = version,
-    author = author,
-    author_email = author_email,
-    name = name,
-    packages = packages,
-    ext_modules = ext_modules,
-    zip_safe=zip_safe,
+ version = version,
+ author = author,
+ author_email = author_email,
+ name = name,
+ packages = packages,
+ ext_modules = ext_modules,
+ zip_safe=zip_safe,
 )
 
 print("Finally, the setup function was performed. We ve got out interface.")
