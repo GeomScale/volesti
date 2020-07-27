05babe16cf8146a05c1c561a3705ef979ed28e6d
AlexManochis <alex.manochis@gmail.com>
2020-07-27 14:30:19 +0300

    fix c++ tests and update rd files

--- test/new_rounding_test.cpp
+++ test/new_rounding_test.cpp
@@ -20,6 +20,8 @@
 #include "volume/volume_cooling_gaussians.hpp"
 #include "volume/volume_cooling_balls.hpp"
 
+#include "preprocess/min_ellipsoid_rounding.hpp"
+
 #include "known_polytope_generators.h"
 
 template <typename NT>
@@ -59,7 +61,7 @@ void rounding_test(Polytope &HP,
     RNGType rng(d);
 
     std::pair<Point, NT> InnerBall = HP.ComputeInnerBall();
-    std::pair< std::pair<MT, VT>, NT > res = round_polytope<CDHRWalk, MT, VT>(HP, InnerBall, 10 + 10 * d, rng);
+    std::pair< std::pair<MT, VT>, NT > res = min_ellipsoid_rounding<CDHRWalk, MT, VT>(HP, InnerBall, 10 + 10 * d, rng);
 
     // Setup the parameters
     int walk_len = 1;
@@ -99,13 +101,13 @@ void call_test_skinny_cubes() {
 
     std::cout << "\n--- Testing rounding of H-skinny_cube10" << std::endl;
     P = gen_skinny_cube<Hpolytope>(10);
-    rounding_test(P, 0, 101895, 100779.0, 105003.0, 102400.0);
+    rounding_test(P, 0, 101895, 108426, 105003.0, 102400.0);
 
     std::cout << "\n--- Testing rounding of H-skinny_cube20" << std::endl;
     P = gen_skinny_cube<Hpolytope>(20);
     rounding_test(P, 0,
                   8.26497 * std::pow(10,7),
-                  9.37982 * std::pow(10,7),
+                  8.23341 * std::pow(10,7),
                   1.09218e+08,
                   104857600.0);
 }
