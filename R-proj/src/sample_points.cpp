6921226d3b8235b2596a1dd9306a7ae61e05635b
Tolis Chalkis <tolis.chal@gmail.com>
2020-07-27 12:24:07 +0300

    Fix sampling issue #72 (#101)
    * set default walk length equal to 1, fix sample_points bug from given starting point
    
    * improve Rd files

--- R-proj/src/sample_points.cpp
+++ R-proj/src/sample_points.cpp
@@ -107,8 +107,8 @@ void sample_from_polytope(Polytope &P, int type, RNGType &rng, PointList &randPo
 //' @param random_walk Optional. A list that declares the random walk and some related parameters as follows:
 //' \itemize{
 //' \item{\code{walk} }{ A string to declare the random walk: i) \code{'CDHR'} for Coordinate Directions Hit-and-Run, ii) \code{'RDHR'} for Random Directions Hit-and-Run, iii) \code{'BaW'} for Ball Walk, iv) \code{'BiW'} for Billiard walk, v) \code{'BCDHR'} boundary sampling by keeping the extreme points of CDHR or vi) \code{'BRDHR'} boundary sampling by keeping the extreme points of RDHR. The default walk is \code{'BiW'} for the uniform distribution or \code{'CDHR'} for the Gaussian distribution.}
-//' \item{\code{walk_length} }{ The number of the steps per generated point for the random walk. The default value is \eqn{5} for \code{'BiW'} and \eqn{\lfloor 10 + d/10\rfloor} otherwise.}
-//' \item{\code{nburns} }{ The number of points to burn before start sampling.}
+//' \item{\code{walk_length} }{ The number of the steps per generated point for the random walk. The default value is \eqn{1}.}
+//' \item{\code{nburns} }{ The number of points to burn before start sampling. The default value is \eqn{1}.}
 //' \item{\code{starting_point} }{ A \eqn{d}-dimensional numerical vector that declares a starting point in the interior of the polytope for the random walk. The default choice is the center of the ball as that one computed by the function \code{inner_ball()}.}
 //' \item{\code{BaW_rad} }{ The radius for the ball walk.}
 //' \item{\code{L} }{ The maximum length of the billiard trajectory.}
@@ -157,12 +157,12 @@ Rcpp::NumericMatrix sample_points(Rcpp::Nullable<Rcpp::Reference> P,
     typedef Eigen::Matrix<NT,Eigen::Dynamic,Eigen::Dynamic> MT;
 
     unsigned int type = Rcpp::as<Rcpp::Reference>(P).field("type"),dim = Rcpp::as<Rcpp::Reference>(P).field("dimension"),
-          walkL = 10 + dim / 10, numpoints, nburns = 0;
+          walkL = 1, numpoints, nburns = 0;
 
     RNGType rng(dim);
     if (seed.isNotNull()) {
-        unsigned seed2 = Rcpp::as<double>(seed);
-        rng.set_seed(seed2);
+        unsigned seed_rcpp = Rcpp::as<double>(seed);
+        rng.set_seed(seed_rcpp);
     }
 
     Hpolytope HP;
@@ -227,7 +227,6 @@ Rcpp::NumericMatrix sample_points(Rcpp::Nullable<Rcpp::Reference> P,
             } else {
                 walk = billiard;
             }
-            walkL = 2;
         }
     } else if (Rcpp::as<std::string>(Rcpp::as<Rcpp::List>(random_walk)["walk"]).compare(std::string("CDHR")) == 0) {
         walk = cdhr;
@@ -243,7 +242,6 @@ Rcpp::NumericMatrix sample_points(Rcpp::Nullable<Rcpp::Reference> P,
     } else if (Rcpp::as<std::string>(Rcpp::as<Rcpp::List>(random_walk)["walk"]).compare(std::string("BiW")) == 0) {
         if (gaussian) throw Rcpp::exception("Billiard walk can be used only for uniform sampling!");
         walk = billiard;
-        walkL = 2;
         if (Rcpp::as<Rcpp::List>(random_walk).containsElementNamed("L")) {
             L = Rcpp::as<NT>(Rcpp::as<Rcpp::List>(random_walk)["L"]);
             set_L = true;
@@ -252,7 +250,6 @@ Rcpp::NumericMatrix sample_points(Rcpp::Nullable<Rcpp::Reference> P,
     } else if (Rcpp::as<std::string>(Rcpp::as<Rcpp::List>(random_walk)["walk"]).compare(std::string("aBiW")) == 0) {
         if (gaussian) throw Rcpp::exception("Billiard walk can be used only for uniform sampling!");
         walk = accelarated_billiard;
-        walkL = 2;
         if (Rcpp::as<Rcpp::List>(random_walk).containsElementNamed("L")) {
             L = Rcpp::as<NT>(Rcpp::as<Rcpp::List>(random_walk)["L"]);
             set_L = true;
@@ -286,7 +283,6 @@ Rcpp::NumericMatrix sample_points(Rcpp::Nullable<Rcpp::Reference> P,
         }
     }
 
-
     if (Rcpp::as<Rcpp::List>(random_walk).containsElementNamed("nburns")) {
         nburns = Rcpp::as<int>(Rcpp::as<Rcpp::List>(random_walk)["nburns"]);
         if (nburns < 0) {
@@ -300,8 +296,8 @@ Rcpp::NumericMatrix sample_points(Rcpp::Nullable<Rcpp::Reference> P,
             HP.init(dim, Rcpp::as<MT>(Rcpp::as<Rcpp::Reference>(P).field("A")),
                     Rcpp::as<VT>(Rcpp::as<Rcpp::Reference>(P).field("b")));
 
+            InnerBall = HP.ComputeInnerBall();
             if (!set_starting_point || (!set_mode && gaussian)) {
-                InnerBall = HP.ComputeInnerBall();
                 if (!set_starting_point) StartingPoint = InnerBall.first;
                 if (!set_mode && gaussian) mode = InnerBall.first;
             }
@@ -313,6 +309,8 @@ Rcpp::NumericMatrix sample_points(Rcpp::Nullable<Rcpp::Reference> P,
                 StartingPoint = StartingPoint - mode;
                 HP.shift(mode.getCoefficients());
             }
+            sample_from_polytope(HP, type, rng, randPoints, walkL, numpoints, gaussian, a, L,
+                                 StartingPoint, nburns, set_L, walk);
             break;
         }
         case 2: {
@@ -320,8 +318,8 @@ Rcpp::NumericMatrix sample_points(Rcpp::Nullable<Rcpp::Reference> P,
             VP.init(dim, Rcpp::as<MT>(Rcpp::as<Rcpp::Reference>(P).field("V")),
                     VT::Ones(Rcpp::as<MT>(Rcpp::as<Rcpp::Reference>(P).field("V")).rows()));
 
+            InnerBall = VP.ComputeInnerBall();
             if (!set_starting_point || (!set_mode && gaussian)) {
-                InnerBall = VP.ComputeInnerBall();
                 if (!set_starting_point) StartingPoint = InnerBall.first;
                 if (!set_mode && gaussian) mode = InnerBall.first;
             }
@@ -331,6 +329,8 @@ Rcpp::NumericMatrix sample_points(Rcpp::Nullable<Rcpp::Reference> P,
                 StartingPoint = StartingPoint - mode;
                 VP.shift(mode.getCoefficients());
             }
+            sample_from_polytope(VP, type, rng, randPoints, walkL, numpoints, gaussian, a, L,
+                                 StartingPoint, nburns, set_L, walk);
             break;
         }
         case 3: {
@@ -338,8 +338,8 @@ Rcpp::NumericMatrix sample_points(Rcpp::Nullable<Rcpp::Reference> P,
             ZP.init(dim, Rcpp::as<MT>(Rcpp::as<Rcpp::Reference>(P).field("G")),
                     VT::Ones(Rcpp::as<MT>(Rcpp::as<Rcpp::Reference>(P).field("G")).rows()));
 
+            InnerBall = ZP.ComputeInnerBall();
             if (!set_starting_point || (!set_mode && gaussian)) {
-                InnerBall = ZP.ComputeInnerBall();
                 if (!set_starting_point) StartingPoint = InnerBall.first;
                 if (!set_mode && gaussian) mode = InnerBall.first;
             }
@@ -349,6 +349,8 @@ Rcpp::NumericMatrix sample_points(Rcpp::Nullable<Rcpp::Reference> P,
                 StartingPoint = StartingPoint - mode;
                 ZP.shift(mode.getCoefficients());
             }
+            sample_from_polytope(ZP, type, rng, randPoints, walkL, numpoints, gaussian, a, L, 
+                                 StartingPoint, nburns, set_L, walk);
             break;
         }
         case 4: {
@@ -371,29 +373,8 @@ Rcpp::NumericMatrix sample_points(Rcpp::Nullable<Rcpp::Reference> P,
                 StartingPoint = StartingPoint - mode;
                 VPcVP.shift(mode.getCoefficients());
             }
-            break;
-        }
-    }
-
-    switch (type) {
-        case 1: {
-            sample_from_polytope(HP, type, rng, randPoints, walkL, numpoints, gaussian, a, L, StartingPoint, nburns,
-                   set_L, walk);
-            break;
-        }
-        case 2: {
-            sample_from_polytope(VP, type, rng, randPoints, walkL, numpoints, gaussian, a, L, StartingPoint, nburns,
-                                 set_L, walk);
-            break;
-        }
-        case 3: {
-            sample_from_polytope(ZP, type, rng, randPoints, walkL, numpoints, gaussian, a, L, StartingPoint, nburns,
-                                 set_L, walk);
-            break;
-        }
-        case 4: {
-            sample_from_polytope(VPcVP, type, rng, randPoints, walkL, numpoints, gaussian, a, L, StartingPoint, nburns,
-                                 set_L, walk);
+            sample_from_polytope(VPcVP, type, rng, randPoints, walkL, numpoints, gaussian, a, L,
+                                 StartingPoint, nburns, set_L, walk);
             break;
         }
     }
@@ -402,10 +383,9 @@ Rcpp::NumericMatrix sample_points(Rcpp::Nullable<Rcpp::Reference> P,
     MT RetMat(dim, numpoints);
     unsigned int jj = 0;
 
-
     for (typename std::list<Point>::iterator rpit = randPoints.begin(); rpit!=randPoints.end(); rpit++, jj++) {
         if (gaussian) {
-            RetMat.col(jj) = rpit->getCoefficients() + mode.getCoefficients();
+            RetMat.col(jj) = (*rpit).getCoefficients() + mode.getCoefficients();
         } else {
             RetMat.col(jj) = (*rpit).getCoefficients();
         }
