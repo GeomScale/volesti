90b8a117d7db8b8b4765e3064f9e69c4af5e29a5
hariszaf <haris-zaf@hcmr.gr>
2020-08-05 16:58:21 +0300

    Update test_rounding.py

--- volestipy/tests/test_rounding.py
+++ volestipy/tests/test_rounding.py
@@ -5,48 +5,46 @@ import volestipy
 from volestipy import HPolytope
 
 if __name__ == "__main__":
-    dim = 8
+    dim = 9
     A = np.zeros((2*dim, dim), dtype=np.float)
     A[0:dim] = np.eye(dim)
     A[dim:] -=  np.eye(dim,dim, dtype=np.float)
     b = np.ones(2*dim, dtype=np.float)
 
 
-    p = HPolytope(A,b)
+    hp = HPolytope(A,b)
+
     print("The polytope:")
-    print("Dimensions: {}".format(p.dimensions))
+    print("Dimensions: {}".format(hp.dimensions))
     print("A matrix:")
-    print(p.A)
-    print(type(p.A))
+    print(hp.A)
+    print(type(hp.A))
     print("b vector:")
-    print(p.b)
-    print(type(p.b))
-   
-# Run test for the rounding() function and its different methods
-    print("\n\n ***The rounding step is about to start*** \n\n")
-    p.rounding(rounding_method = "max_ellipsoid")
+    print(hp.b)
+    print(type(hp.b))
+
+    # Run test for the rounding() function and its different methods
+    rounding_returns = ["new_A","new_b","T_matrix","shift","round_val"]
+
+    # Case 1
+    rounding_output_max_ellipsoid = hp.rounding(rounding_method = "max_ellipsoid")
     print("\n ***This is the output for the max_ellipsoid rounding method.***\n")
+    for i in range(len(rounding_output_max_ellipsoid)):
+        print("\n" + rounding_returns[i] + ": ")
+        print(rounding_output_max_ellipsoid[i])
+
+    # Case 2
+    rounding_output_svd = hp.rounding(rounding_method = "svd")
+    print("\n ***This is the output for the svd rounding method.***\n")
+    for i in range(len(rounding_output_svd)):
+        print("\n" + rounding_returns[i] + ": ")
+        print(rounding_output_svd[i])
 
 
-
-#    for i in rounding_output_max_ellipsoid:
-#        print(i)
-#        print("\n\n*************\n\n")
-#
-#    rounding_output_svd = p.rounding(rounding_method = "svd")
-#    print("\n\nthis is the output for the svd rounding")
-#    for i in rounding_output_svd:
-#        print(i)
-#        print("\n\n*************\n\n")
-#
-#
-#    rounding_output_min_ellipsoid = p.rounding(rounding_method = "min_ellipsoid")
-#    print("\n this is the output for the max_ellipsoid rounding method\n")
-#    for i in rounding_output_min_ellipsoid:
-#        print(i)
-#        print("\n\n*************\n\n")
-
-
-
-
+    # Case 3
+    rounding_output_min_ellipsoid = hp.rounding(rounding_method = "min_ellipsoid")
+    print("\n ***This is the output for the min_ellipsoid rounding method.***\n")
+    for i in range(len(rounding_output_min_ellipsoid)):
+        print("\n" + rounding_returns[i] + ": ")
+        print(rounding_output_min_ellipsoid[i])
 
