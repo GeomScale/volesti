105e618010279e7f6d3be39ab4168c88400401be
hariszaf <haris-zaf@hcmr.gr>
2020-08-07 14:30:56 +0300

    Update bindings.h
    new class for the preprocessing and the get_full_dimensional_polytope() function in the bindings.h

--- volestipy/volestipy/include/bindings.h
+++ volestipy/volestipy/include/bindings.h
@@ -27,8 +27,7 @@
 #include "preprocess/min_sampling_covering_ellipsoid_rounding.hpp"
 #include "preprocess/svd_rounding.hpp"
 #include "preprocess/max_inscribed_ellipsoid_rounding.hpp"
-#include "preprocess/get_full_dimensional_polytope.hpp"    // will be used after completing the rounding
-
+#include "preprocess/get_full_dimensional_polytope.hpp"
 
 typedef double NT;
 typedef Cartesian<NT>    Kernel;
@@ -37,51 +36,62 @@ typedef HPolytope<Point> Hpolytope;
 typedef typename Hpolytope::MT    MT;
 typedef typename Hpolytope::VT    VT;
 typedef BoostRandomNumberGenerator<boost::mt19937, double>    RNGType;
-typedef struct rounded{
-   MT new_A;
-   VT new_b;
-}rounded_HPolytope;
 
+
+// This is the HPolytopeCPP class; the main volesti class that is running the compute_volume(), rounding() and sampling() methods
 class HPolytopeCPP{
+
    public:
-
-
-      typedef double NT;
-      typedef Cartesian<NT>    Kernel;
-      typedef typename Kernel::Point    Point;
-      typedef HPolytope<Point> Hpolytope;
-      typedef typename Hpolytope::MT    MT;
-      typedef typename Hpolytope::VT    VT;
-      typedef BoostRandomNumberGenerator<boost::mt19937, double>    RNGType;
-
+      
+      std::pair<Point,NT> CheBall;
+      
       // regarding the rounding step
       typedef std::tuple<MT, VT, NT>    round_result;
 
 
-
-
-// The class and its main specs
-
+      // The class and its main specs
       HPolytopeCPP();
       HPolytopeCPP(double *A, double *b, int n_hyperplanes, int n_variables);
 
-      Hpolytope HP;
-      std::pair<Point,NT> CheBall;
+      Hpolytope HP;      
+      // Here we use the "~" destructor; this way we avoid a memory leak.
       ~HPolytopeCPP();
 
-// the compute_volume() function
+      // the compute_volume() function
       double compute_volume(char* vol_method, char* walk_method, int walk_len, double epsilon, int seed);
 
-// the generate_samples() function
+      // the generate_samples() function
       double generate_samples(int walk_len, int number_of_points, int number_of_points_to_burn, bool boundary, \
        bool cdhr, bool rdhr, bool gaussian, bool set_L, bool billiard, bool ball_walk, double a, double L,  double* samples);
 
-// the rounding() function
+      // the rounding() function
       void rounding(char* rounding_method, double* new_A, double* new_b, double* T_matrix, double* shift, double &round_value);
-
+      
 };
 
+// The preHPolytopeCPP class is responsible for the preprocess step of the polytope as well as for getting the full dimensional polytope 
+class preHPolytopeCPP{
 
+   public:
+      
+      std::pair<Point,NT> CheBall;
+      
+      // regarding getting full dimensional polytope
+      typedef std::pair<Hpolytope, std::pair<MT, VT> > get_full_dim_pol_result;
+
+      // The class and its main specs
+      preHPolytopeCPP();
+      preHPolytopeCPP(double *A, double *b, double *Aeq, double *beq, int n_rows_of_A, int n_cols_of_A, int n_row_of_Aeq, int n_cols_of_Aeq);
+
+      Hpolytope HP;      
+      // Here we use the "~" destructor; this way we avoid a memory leak.
+      ~preHPolytopeCPP();
+      
+      
+      // the get_full_dimensional_polytop() function
+      void get_full_dimensiolal_polytope();
+
+};
 
 
 #endif
