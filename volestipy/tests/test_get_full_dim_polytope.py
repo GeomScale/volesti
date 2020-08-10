e8b810b6911219292fa6ac0c8b3d4d22b8b162b4
hariszaf <haris-zaf@hcmr.gr>
2020-08-10 14:20:08 +0300

    Create test_get_full_dim_polytope.py
    test file for the get_full_dimensional_polytope function
    not ready yet

---
+++ volestipy/tests/test_get_full_dim_polytope.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3.6
+
+import numpy as np
+import volestipy
+from volestipy import HPolytope
+
+if __name__ == "__main__":
+    
+    m = 3
+    n = 5
+
+    A = np.zeros((2*n, n), dtype=np.float)
+    A[0:n] = np.eye(n)
+    A[n:] -=  np.eye(n,n, dtype=np.float)
+    print("This is the A matrix: \n")
+    print(A)
+
+    b = np.ones(2*n, dtype=np.float)
+    print("This is the vector b: \n")
+    print(b)
+
+    Aeq = np.zeros((m, n), dtype = np.float)
+    Aeq = np.random.normal(0, 1, n)
+
+    print("This is the Aeq matrix: \n")
+    print(Aeq)
+
+    beq = np.zeros(m)
+    print("This is the vector beq: \n")
+    print(beq)
