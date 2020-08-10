769f728b80978d8cf9f9afd6d04a9b70a1e4a9bb
hariszaf <haris-zaf@hcmr.gr>
2020-08-10 16:46:34 +0300

    Update test_get_full_dim_polytope.py
    final test for the get_full_dimensional_polytope() function

--- volestipy/tests/test_get_full_dim_polytope.py
+++ volestipy/tests/test_get_full_dim_polytope.py
@@ -3,6 +3,7 @@
 import numpy as np
 import volestipy
 from volestipy import HPolytope
+from volestipy import low_dim_HPolytope
 
 if __name__ == "__main__":
     
@@ -12,17 +13,28 @@ if __name__ == "__main__":
     A = np.zeros((2*n, n), dtype=np.float)
     A[0:n] = np.eye(n)
     A[n:] -=  np.eye(n,n, dtype=np.float)
-    print("This is the A matrix: \n")
+    print("\n This is the A matrix: \n")
     print(A)
 
     b = np.ones(2*n, dtype=np.float)
-    print("This is the vector b: \n")
+    print("\n This is the vector b: \n")
     print(b)
 
-    Aeq = np.random.normal(1, size=(m, n))
-    print("This is the Aeq matrix: \n")
+    Aeq = np.random.normal(0, 1, size=(m, n))
+    print("\n This is the Aeq matrix: \n")
     print(Aeq)
 
     beq = np.zeros(m)
-    print("This is the vector beq: \n")
+    print("\n This is the vector beq: \n")
     print(beq)
+
+    # run the low_dim_HPolytope() class and build an object 
+    low_hp = low_dim_HPolytope(A,b,Aeq,beq)
+    # run the full_dimensiolal_polytope() function
+    get_full_hp = low_hp.full_dimensiolal_polytope()
+
+    # print the output of the full_dimensiolal_polytope() function
+    print("\n this is the get_full_hp.A, meaning the A matrix of the full dimensional polytope:")
+    print(get_full_hp.A)
+    print("\n this is the get_full_hp.b, meaning the b vector of the full dimensional polytope:")
+    print(get_full_hp.b)
