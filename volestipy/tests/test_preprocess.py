4168a4699c7cb670e83589e03294c59b3f1edd69
hariszaf <haris-zaf@hcmr.gr>
2020-08-19 15:16:55 +0300

    Update test_preprocess.py
    build Aeq in a way to be more like to a stoichiometric matrix

--- volestipy/tests/test_preprocess.py
+++ volestipy/tests/test_preprocess.py
@@ -7,9 +7,9 @@ from gurobipy import GRB
 
 
 if __name__ == "__main__":
-
-   m = 4
-   n = 10
+    
+   m = 3
+   n = 6
 
    A = np.zeros((2*n, n), dtype=np.float)
    A[0:n] = np.eye(n)
@@ -21,14 +21,14 @@ if __name__ == "__main__":
    print("\n This is the vector b: \n")
    print(b)
 
-   Aeq = np.random.normal(0, 1, size=(m, n))
+#   Aeq = np.random.randint(-4, 4, size=(m, n))
+   Aeq = np.random.choice(np.arange(-3, 3), p=[0.05, 0.05, 0.3, 0.5, 0.1, 0.0], size=(m,n))
    print("\n This is the Aeq matrix: \n")
    print(Aeq)
 
    beq = np.zeros(m)
    print("\n This is the vector beq: \n")
    print(beq)
-
 
    res = pre_process(A, b, Aeq, beq)
    print("new A is:")
@@ -39,5 +39,6 @@ if __name__ == "__main__":
    print(res[2])
    print("new beq is:")
    print(res[3])
+
 
 
