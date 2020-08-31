aec0c40c521ca5306853155e3bb9282db427228b
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2020-08-31 08:15:07 +0000

    Remove fixed threshold for -0 bug fix

--- include/convex_bodies/hpolytope.h
+++ include/convex_bodies/hpolytope.h
@@ -131,10 +131,12 @@ public:
         for (unsigned int i = 1; i < Pin.size(); i++) {
             b(i - 1) = Pin[i][0];
             for (unsigned int j = 1; j < _d + 1; j++) {
-                if(Pin[i][j]>1e-9 || Pin[i][j]<-(1e-9) )
+                if(Pin[i][j] != 0){
                     A(i - 1, j - 1) = -Pin[i][j];
-                else
-                    A(i - 1, j - 1) = Pin[i][j];
+                }
+                else{
+                    A(i - 1, j - 1) = 0;
+                }
             }
         }
     }
