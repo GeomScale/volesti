7bd04460dece22639706486d85df1ceea0048161
hariszaf <haris-zaf@hcmr.gr>
2020-09-02 22:52:32 +0300

    one test file for reading both json and mat bigg files

--- volestipy/tests/test_read_bigg_files.py
+++ volestipy/tests/test_read_bigg_files.py
@@ -7,11 +7,32 @@ import volestipy
 
 current_directory = os.getcwd()
 input_file_json = current_directory +  '/bigg_files/e_coli_core.json'
-
+input_file_mat = current_directory +  '/bigg_files/e_coli_core.mat'
 
 
 met_net = volestipy.read_json_file(input_file_json)
 
+print("A")
+print(met_net[0])
+print("------------------------")
+print("b")
+print(met_net[1])
+print("-------------------------")
+print("Aeq")
+print(met_net[2])
+print("-------------------------")
+print("beq")
+print(met_net[3])
+print("-------------------------")
+print("Metabolites are the following " + str(len(met_net[4])) + ":")
+print(met_net[4])
+print("-------------------------")
+print("Reactions are the following " + str(len(met_net[5])) + ":")
+print(met_net[5])
+
+# -----------------------------------------------------------------
+
+met_net = volestipy.read_mat_file(input_file_mat)
 
 print("A")
 print(met_net[0])
