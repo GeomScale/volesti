d7278763fb6802e4d9981728d9c8e2c95b56c2ea
hariszaf <haris-zaf@hcmr.gr>
2020-10-09 16:05:10 +0300

    test the svd rounding method

---
+++ volestipy/tests/test_svd_rounding.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3.6
+
+import numpy as np
+import volestipy
+from volestipy import HPolytope
+
+if __name__ == "__main__":
+    dim = 5
+    A = np.zeros((2*dim, dim), dtype=np.float)
+    A[0:dim] = np.eye(dim)
+    A[dim:] -=  np.eye(dim,dim, dtype=np.float)
+    b = np.ones(2*dim, dtype=np.float)
+
+
+    hp = HPolytope(A,b)
+
+    print("The polytope:")
+    print("Dimensions: {}".format(hp.dimensions))
+    print("A matrix:")
+    print(hp.A)
+    print(type(hp.A))
+    print("b vector:")
+    print(hp.b)
+    print(type(hp.b))
+
+    # Run test for the rounding() function and its different methods
+    rounding_returns = ["new_A","new_b","T_matrix","shift","round_val"]
+    print("*** This is a test for th rounding() function of the volestipy library *** \n")
+
+    # Case 1
+    rounding_output_max_ellipsoid = hp.rounding_svd()
+    print("\n >> This is the output for the svd rounding method <<")
+    for i in range(len(rounding_output_max_ellipsoid)):
+        print("\n" + rounding_returns[i] + ": ")
+        print(rounding_output_max_ellipsoid[i])
+
