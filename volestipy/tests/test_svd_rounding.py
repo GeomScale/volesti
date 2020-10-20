1a88bb394ed2eb9f0911f2d8e38badb1902fb5b5
hariszaf <haris-zaf@hcmr.gr>
2020-10-20 16:35:59 +0300

    remove unecessary lines

--- volestipy/tests/test_svd_rounding.py
+++ volestipy/tests/test_svd_rounding.py
@@ -5,32 +5,27 @@ import volestipy
 from volestipy import HPolytope
 
 if __name__ == "__main__":
-    dim = 5
-    A = np.zeros((2*dim, dim), dtype=np.float)
-    A[0:dim] = np.eye(dim)
-    A[dim:] -=  np.eye(dim,dim, dtype=np.float)
-    b = np.ones(2*dim, dtype=np.float)
+   dim = 5
+   A = np.zeros((2*dim, dim), dtype=np.float)
+   A[0:dim] = np.eye(dim)
+   A[dim:] -=  np.eye(dim,dim, dtype=np.float)
+   b = np.ones(2*dim, dtype=np.float)
 
+   hp = HPolytope(A,b)
 
-    hp = HPolytope(A,b)
+   print("The polytope:")
+   print("Dimensions: {}".format(hp.dimensions))
+   print("A matrix:")
+   print(hp.A)
+   print("b vector:")
+   print(hp.b)
 
-#    print("The polytope:")
-#    print("Dimensions: {}".format(hp.dimensions))
-#    print("A matrix:")
-#    print(hp.A)
-#    print(type(hp.A))
-#    print("b vector:")
-#    print(hp.b)
-#    print(type(hp.b))
+   rounding_returns = ["new_A","new_b","T_matrix","shift","round_val"]
 
-    # Run test for the rounding() function and its different methods
-    rounding_returns = ["new_A","new_b","T_matrix","shift","round_val"]
-#    print("*** This is a test for th rounding() function of the volestipy library *** \n")
+   svd_rounding = hp.rounding_svd()
 
-    # Case 1
-    rounding = hp.rounding_svd()
-#    print("\n >> This is the output for the svd rounding method <<")
-#    print(rounding_output_svd[0])
+   print("\n\n ** This is the rounding svd output\n")
+   for i in range(len(rounding_returns)):
+      print("\n" + rounding_returns[i] + ":")
+      print(svd_rounding[0])
 
-
-    print(rounding[0])
