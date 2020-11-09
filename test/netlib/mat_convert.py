8f2c86d06e0b1da2012b6392f19a5238b1f095bf
Marios Papachristou <papachristoumarios@gmail.com>
2020-11-09 18:15:12 -0500

    Fix netlib polytope loader

--- test/netlib/mat_convert.py
+++ test/netlib/mat_convert.py
@@ -7,18 +7,32 @@ filename = sys.argv[-1]
 
 mat = scipy.io.loadmat(filename)
 
-A = - mat['problem']['Aeq'][0, 0].toarray()
-b = mat['problem']['beq'][0, 0]
+Aeq = mat['problem']['Aeq'][0, 0].toarray()
+beq = mat['problem']['beq'][0, 0]
+lb = mat['problem']['lb'][0, 0]
+ub = mat['problem']['ub'][0, 0]
 
-C = np.hstack((b, A))
+# Equality constraints Ax = b
+# are replaced by Ax <= b and - Ax <= - b
+# Lower bounds are defined by -Ix <= -lb
+# .ine files expect a format (b_hat, -A_hat)
+C = np.vstack (
+    (
+        np.hstack((beq, -Aeq)),
+        np.hstack((-beq, Aeq)),
+        np.hstack((-lb, np.eye(lb.shape[0])))
+    )
+)
 
-m, d = A.shape
+m, d = C.shape
 
-print('filename')
-print('begin')
-print('{} {} real'.format(m, d + 1))
-np.savetxt(sys.stdout, C, fmt="%.4f")
+outfile = os.path.splitext(filename)[0] + '.ine'
 
-print('end')
-print('input_incidence')
-
+with open(outfile, 'w+') as f:
+    f.write('filename\n')
+    f.write('begin\n')
+    f.write('{} {} real\n'.format(m, d))
+    np.savetxt(f, C, fmt="%.4f")
+    f.write('\n')
+    f.write('end\n')
+    f.write('input_incidence\n')
