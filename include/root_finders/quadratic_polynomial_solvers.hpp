434de2fd08a3094741ae566480aedbdafc21d544
TolisChal <tolis.chal@gmail.com>
2021-03-12 18:27:29 +0200

    resolve the reviews on the root computation

--- include/root_finders/quadratic_polynomial_solvers.hpp
+++ include/root_finders/quadratic_polynomial_solvers.hpp
@@ -2,7 +2,6 @@
 // Copyright (c) 2021 Vissarion Fisikopoulos
 // Copyright (c) 2021 Apostolos Chalkis
 
-
 // Licensed under GNU LGPL.3, see LICENCE file
 
 
@@ -11,29 +10,31 @@
 
 // The function compute the roots of a quadratic polynomial equation
 template <typename NT>
-void solve_quadratic_polynomial(NT a, NT b, NT c, NT &x1, NT &x2, bool &real) {
-
-    real = true;
-
+bool solve_quadratic_polynomial(NT const& a, NT const& b, NT const& c, NT &x1, NT &x2) 
+{
     if (a == NT(0)) {
         x1 = -c / b;
         x2 = x1;
-        return;
+        return true;
     }
 
     NT Delta = b * b - 4.0 * a * c;
-    if (Delta < NT(0)) {
-        real = false;
-        return;
+    if (Delta < NT(0)) 
+    {
+        return false;
     }
 
-    if (b >= NT(0)){
+    if (b >= NT(0))
+    {
         x1 = (- b - std::sqrt(Delta)) / (2.0 * a);
         x2 = (2.0 * c) / (- b - std::sqrt(Delta));
-    } else {
+    } 
+    else 
+    {
         x1 = (2.0 * c) / (- b + std::sqrt(Delta));
         x2 = (- b + std::sqrt(Delta)) / (2.0 * a);
     }
+    return true;
 }
 
 
