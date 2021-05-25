5d116f353105109f941386550cd07eac197d76ad
Suraj Choubey <srjchoubey2@gmail.com>
2021-05-26 01:28:01 +0530

    Removed LPSolve libraries

---
+++ test/LPSolve.cmake
@@ -0,0 +1,32 @@
+function(GetLPSolve)
+    find_path(LP_SOLVE_DIR NAMES LPsolve_src PATHS ../../external)
+
+    if (NOT LP_SOLVE_DIR)    
+        include(FetchContent)
+        FetchContent_Declare(
+            lpsolve 
+            URL https://cran.r-project.org/src/contrib/lpSolve_5.6.15.tar.gz
+            URL_HASH MD5=3b8d780f703e0da2e4863939add164bc
+        )
+        
+        FetchContent_GetProperties(lpsolve)
+            
+        if(NOT lpsolve_POPULATED)
+            message(STATUS "lp_solve library not found locally, downloading it.")
+            FetchContent_Populate(lpsolve)
+        endif()
+        
+        set(LP_SOLVE_DIR "${lpsolve_SOURCE_DIR}/src")
+        set(LP_SOLVE_R_DIR "${lpsolve_SOURCE_DIR}/R")
+        message(STATUS "Using downloaded lp_solve at: ${LP_SOLVE_DIR}")
+
+    else()
+
+        message(STATUS "lp_solve library found: ${LP_SOLVE_DIR}")
+    
+    endif()
+
+    include_directories(${LP_SOLVE_DIR})
+    include_directories(${LP_SOLVE_R_DIR})
+        
+endfunction()
