e26e424b5f2970ff4d914c54086a14da07587e18
Suraj Choubey <srjchoubey2@gmail.com>
2021-07-07 12:52:00 +0530

    added tests birkhoff, prod-simplex, simplex, cross

--- include/integration/simple_MC_integration.hpp
+++ include/integration/simple_MC_integration.hpp
@@ -7,6 +7,8 @@
 
 // Licensed under GNU LGPL.3, see LICENCE file
 
+// Monte Carlo Integration algorithm used here : https://en.wikipedia.org/wiki/Monte_Carlo_integration#Overview
+
 #ifndef SIMPLE_MC_INTEGRATION_HPP
 #define SIMPLE_MC_INTEGRATION_HPP
 
@@ -35,23 +37,30 @@ typedef BoostRandomNumberGenerator<RNGType, NT> RandomNumberGenerator;
 typedef typename HPolytope<Point>::MT MT;
 typedef typename HPolytope<Point>::VT VT; 
 
-typedef const unsigned int Uint;  // positive constant value for no of samples & dimensions
-enum volType { CB , CG , SOB }; // Volume type for polytope
-typedef typename std::vector<NT> Limit; // Standard way for user to use limits E.g. Limits LL{0.5, 1.5, 2.5} ; Limits UL{1.2, 1.8 , 2.8 } ;
-const Point pt(0); // To initialize points in function params
-Limit lt{0}; // To initialize non-initialized limits
+typedef const unsigned int Uint;  // Positive constant value for no of samples & dimensions
+enum volumetype {CB ,CG ,SOB}; // Volume type for polytope
+typedef typename std::vector<NT> Limit; // Standard way for user to use limits 
+// E.g. Limits LL{0.5, 1.5, 2.5} ; Limits UL{1.2, 1.8 , 2.8 } ;
 
-// To check if two n-dimensional points ensure valid limits in integration 
-bool valid_limits(Point LL, Point UL){
-    if( UL.dimension() == LL.dimension() ) {
-        for (int i = 0 ; i<LL.dimension() ; i++){
-            if( LL[i] > UL[i]) {
+const Limit lt{0}; // To initialize non-initialized limits
+const Point pt{0}; // To initialize non-initialized points
+
+// To check if two n-dimensional points ensure valid limits in integration
+template
+<
+    typename Point = Point,
+    typename NT = NT
+>
+bool valid_limits(Point LL, Point UL) {
+    if (UL.dimension() == LL.dimension()) {
+        for (int i = 0; i<LL.dimension(); i++) {
+            if (LL[i] > UL[i]) {
                 std::cerr << "Invalid integration limits\n";
                 return false;
             }
         }
         return true;
-    }else{
+    } else {
         std::cerr << "Invalid integration limits\n";
         return false;
     }
@@ -60,201 +69,215 @@ bool valid_limits(Point LL, Point UL){
 // Initialize Limit Point
 template
 <
-    typename Point,
-    typename NT
+    typename Point = Point,
+    typename NT = NT
 >
-Point init_limit(Limit L, Uint dim){
+Point init_limit(Limit L, Uint dim) {
     Point pt(dim);
-    for(int i=0 ; i<dim ; i++){
+    for (int i=0; i<dim; i++) {
         pt.set_coord(i, L[i]);
     }
     return pt;
 }
 
 //Initialize to [-1,1]^n
-void initiate_unit_limits(Point& LL, Point& UL, int dim){
-
+template
+<
+    typename Point = Point,
+    typename NT = NT
+>
+void initiate_unit_limits(Point& LL, Point& UL, int dim) {
     LL.set_dimension(dim);
     UL.set_dimension(dim);
     LL.set_to_origin();
     UL.set_to_origin();
 
-    for(int i=0 ; i<dim ; i++){
-        LL.set_coord(i,-1);
-        UL.set_coord(i,1);
+    for (int i=0 ; i<dim; i++) {
+        LL.set_coord(i, -1);
+        UL.set_coord(i, 1);
     }
 }
-
-// Hyper-Rectangle volume calculator in n-dimensions
-template
-<
-    typename Point=Point,
-    typename NT=NT
->
-NT hyper_rect_volume(Point LL, Point UL){
-
-    NT product=1;
-    if(valid_limits(LL, UL)){
-        for(int i=0; i<LL.dimension() ; ++i) {
-            product = product * (UL[i] - LL[i]);
-        }
-        return product;
-    }
-    else return 0;
-
-}
-
-// To sample a point between two n-dimensional points using inbuilt random sampling
-// Please use `srand(time(0));` while invoking this function
-Point sample_point_bw_limits(Point LL, Point UL) {
-    
-    Point sample_point(LL.dimension());
-    for(int i=0; i<LL.dimension() ; ++i) {
-        sample_point.set_coord(i , LL[i] + (NT)(rand()) / ((NT)(RAND_MAX/(UL[i] - LL[i]))) );
-    }
-    return sample_point;
-}
-
-// Simple MC Integration over Hyper-Rectangles
-template
-<
-    typename WalkType=BallWalk,
-    typename RNG=RandomNumberGenerator,
-    typename Functor
->
-NT simple_mc_integrate(Functor Fx, Uint dim, Uint N=10000, Limit LowLimit=lt, Limit UpLimit=lt, int walk_length=10, NT e=0.1) {
-
-    // Setting up integration limits
-    Point LL, UL;
-    if( LowLimit.size()==1 && UpLimit.size()==1 && LowLimit[0]==0 && UpLimit[0]==0 ) { 
-        initiate_unit_limits(LL, UL, dim); 
-    } else if( LowLimit.size() == UpLimit.size() && LowLimit.size()==dim ) { 
-        LL = init_limit <Point, NT> (LowLimit, dim); 
-        UL = init_limit <Point, NT> (UpLimit, dim); 
-    } else { 
-        std::cerr << "Invalid limits entered"; 
-        return 0; 
-    }
- 
-    NT sum = 0;
-    if(valid_limits(LL, UL)) {
-        // Creating an MT & VT for HPolytope(Hyper-Rectangle) for integration limits using LL & UL
-        MT mt(dim*2, dim); 
-        mt.setZero();
-        VT vt(dim*2); 
-        vt.setZero();
-
-        for(int i=0 ; i<dim ; i++) {
-            mt(i,i) = 1;
-            vt(i) = UL[i];
-            mt(dim+i,i) = -1;
-            vt(dim+i) = LL[i]*(-1);
-        }
-
-        // Initialization of H-Polytope
-        HPOLYTOPE P(dim, mt, vt);
-        // P.print();
-    
-        // Setting up params for random walks
-        std::pair <Point, NT> inner_ball = P.ComputeInnerBall();
-        RNG rng(1);
-        Point x0 = inner_ball.first;
-        typename WalkType::template Walk <HPOLYTOPE, RNG> walk(P, x0, rng);
-        
-        for (int i = 0; i <=N; i++ ) {
-            walk.apply(P, x0, walk_length, rng);
-            sum += Fx(x0);
-        }    
-
-        NT volume = hyper_rect_volume(LL,UL);
-        // std::cout << volume << std::endl;
-
-        NT integration_value = volume * sum / N ;
-        return integration_value;
-    } else {
-        std::cerr << "Invalid integration limits\n";
-        return 0;
-    }
-}
-
 
 // Simple MC Integration Over Polytopes
 template 
 <
-    typename WalkType=BallWalk,
-    typename Polytope=HPOLYTOPE,
-    typename RNG=RandomNumberGenerator,
+    typename WalkType = BallWalk,
+    typename Polytope = HPOLYTOPE,
+    typename RNG = RandomNumberGenerator,
+    typename NT = NT,
     typename Functor
 >
-NT simple_mc_polytope_integrate(Functor Fx, Uint dim, Polytope &P, Uint N=10000, volType vT=SOB, int walk_length=1, NT e=0.1, Point Origin=pt){
+NT simple_mc_polytope_integrate(Functor Fx, 
+                                Polytope &P, 
+                                Uint N = 10000, 
+                                volumetype voltype = SOB, 
+                                int walk_length = 1, 
+                                NT e = 0.1, 
+                                Point Origin = pt) 
+{
 
+    Uint dim = P.dimension();
     // P.print();
 
-    // Check if origin is shifted
-    if(Origin.dimension() == 0) {
+    // Check if ShiftPoint is shifted with accurate dimensions
+    if (Origin.dimension() == 0 && dim > 0) {
         Origin.set_dimension(dim); 
         Origin.set_to_origin();
-    }
-    
-    // Checking if Origin dimensions, Polytope dimensions and dim match
-    if(Origin.dimension() != dim && P.dimension() != dim){
-        std::cerr << "Polytope sample shift point's dimension and its own dimension should match to entered dimension" << std::endl;
-        return 0;
+    } else if (Origin.dimension() != dim && dim > 0) {
+        std::cerr << "Polytope Dimension != Shiftpoint Dimension" << std::endl;
+        return -1;
     }
 
+    // std::cout << "Origin.dimension() = " << Origin.dimension() << std::endl;
+    // std::cout << "P.dimension() = " << P.dimension() << std::endl;
+
     // Volume calculation for HPolytope
-    NT volume=0;
+    NT volume;
     
-    switch(vT){
+    switch (voltype) {
     case CB:     
-        volume = volume_cooling_balls<BallWalk, RNG, Polytope>(P, e, walk_length).second; 
+        volume = volume_cooling_balls <BallWalk, RNG, Polytope> (P, e, walk_length).second; 
         break;
     case CG: 
-        volume = volume_cooling_gaussians<GaussianBallWalk, RNG, Polytope>(P, e, walk_length);
+        volume = volume_cooling_gaussians <GaussianBallWalk, RNG, Polytope> (P, e, walk_length);
         break;
     case SOB: 
-        volume = volume_sequence_of_balls<BallWalk, RNG, Polytope>(P, e, walk_length);
+        volume = volume_sequence_of_balls <BallWalk, RNG, Polytope> (P, e, walk_length);
         break;
     default:
         std::cerr << "Error in volume type: CB / SOB / CG" << std::endl;
-        volume = 0;
-        break;
+        return -1;
     }
 
-    // Volume of the Polytope
-    // std::cout << "Volume of the Polytope = " << volume << std::endl;
+    // std::cout << "Volume of the convex body = " << volume << std::endl;
 
     // For implementing Uniform Walks
     RNG rng(1);
     std::pair <Point, NT> inner_ball = P.ComputeInnerBall();
     Point x0 = inner_ball.first;
-    typename WalkType::template Walk<Polytope,RNG> walk(P, x0, rng);
+    typename WalkType::template Walk<Polytope, RNG> walk(P, x0, rng);
 
     // For storing sampled points
     Points points; 
     NT sum = 0;
 
     // Applying and walking through Uniform Walks + Storing Points in Vector<Point>
-    for(int i = 0; i < N; i++ ){
+    for (int i = 0; i < N; i++) {
         walk.apply(P, x0, walk_length, rng);
         sum += Fx(x0 + Origin);
-
-        // points.push_back(x0+Origin);
-        // (x0+Origin).print();
-    }
-
-    // To print sampled points
-    // for(auto i_th_point : points){
-    //     i_th_point.print();
-    // }
-
-    /*
-    Core idea of Monte Carlo Integration algorithm used here : https://en.wikipedia.org/wiki/Monte_Carlo_integration#Overview
-    */
+        
+        // (x0 + Origin).print();
+    } 
 
     // Integration Value
     NT integration_value = volume * sum / N ;
     return integration_value;
 }
 
+// Simple MC Integration over Hyper-Rectangles
+template
+<
+    typename WalkType = BallWalk,
+    typename RNG = RandomNumberGenerator,
+    typename NT = NT,
+    typename Functor
+>
+NT simple_mc_integrate (Functor Fx, 
+                        Uint dim, 
+                        Uint N = 10000, 
+                        volumetype voltype = SOB, 
+                        Limit LowLimit = lt, 
+                        Limit UpLimit = lt, 
+                        int walk_length = 10, 
+                        NT e = 0.1) 
+{
+
+    // Setting up integration limits
+    Point LL, UL;
+    if (LowLimit.size() == 1 && UpLimit.size() == 1 && LowLimit[0] == 0 && UpLimit[0] == 0) { 
+        initiate_unit_limits(LL, UL, dim); 
+    } else if (LowLimit.size() == UpLimit.size() && LowLimit.size() == dim) { 
+        LL = init_limit <Point, NT> (LowLimit, dim); 
+        UL = init_limit <Point, NT> (UpLimit, dim); 
+    } else { 
+        std::cerr << "Invalid limits entered"; 
+        return -1; 
+    }
+ 
+    NT sum = 0;
+
+    if (valid_limits(LL, UL)) {
+
+        // Creating an MT & VT for HPolytope(Hyper-Rectangle) for integration limits using LL & UL
+        MT mt(dim*2, dim); 
+        mt.setZero();
+        VT vt(dim*2); 
+        vt.setZero();
+
+        for (int i=0 ; i<dim; i++) {
+            mt(i,i) = 1;
+            vt(i) = UL[i];
+            mt(dim+i,i) = -1;
+            vt(dim+i) = -1 * LL[i];
+        }
+
+        // Initialization of H-Polytope
+        HPOLYTOPE P(dim, mt, vt);
+        // P.print();
+
+        NT integration_value = simple_mc_polytope_integrate <WalkType, HPOLYTOPE> (Fx, P, N, voltype, walk_length, e);
+        return integration_value;
+
+    } else {
+        std::cerr << "Invalid integration limits" << std::endl;
+        return -1;
+    }
+}
+
 #endif
+
+// Hyper-Rectangle volume calculator in n-dimensions
+// template
+// <
+//     typename Point = Point,
+//     typename NT = NT
+// >
+// NT hyper_rect_volume (Point LL, Point UL) {
+
+//     NT product = 1;
+//     if( valid_limits(LL, UL) ) {
+//         for(int i=0; i<LL.dimension() ; ++i) {
+//             product = product * (UL[i] - LL[i]);
+//         }
+//         return product;
+//     }
+//     else return -1;
+// }
+
+// To sample a point between two n-dimensional points using inbuilt random sampling
+// Please use `srand(time(0));` while invoking this function
+// Point sample_point_bw_limits (Point LL, Point UL) {
+    
+//     Point sample_point(LL.dimension());
+//     for (int i=0; i<LL.dimension() ; ++i) {
+//         sample_point.set_coord(i, LL[i] + (NT)(rand()) / ((NT)(RAND_MAX/(UL[i] - LL[i]))));
+//     }
+//     return sample_point;
+// }
+
+// Setting up params for random walks
+// std::pair <Point, NT> inner_ball = P.ComputeInnerBall();
+// RNG rng(1);
+// Point x0 = inner_ball.first;
+// typename WalkType::template Walk <HPOLYTOPE, RNG> walk(P, x0, rng);
+
+// for (int i = 0; i<=N; i++) {
+//     walk.apply(P, x0, walk_length, rng);
+//     sum += Fx(x0);
+// }    
+
+// NT volume = hyper_rect_volume(LL,UL);
+// std::cout << volume << std::endl;
+
+// NT integration_value = volume * sum / N ;
+// return integration_value;
--- test/simple_mc_integration.cpp
+++ test/simple_mc_integration.cpp
@@ -12,8 +12,8 @@
 #include "Eigen/Eigen"
 #include <vector>
 #include "cartesian_geom/cartesian_kernel.h"
-#include "hpolytope.h"
-#include "known_polytope_generators.h"
+#include "convex_bodies/hpolytope.h"
+#include "generators/known_polytope_generators.h"
 #include "ode_solvers/oracle_functors.hpp"
 #include "random_walks/random_walks.hpp"
 #include <iostream>
@@ -34,22 +34,19 @@ NT exp_normsq(Point X) {
 }
 
 NT simple_polynomial_1D(Point X) {
-	return (X[0]-1) * (X[0]-2) * (X[0]-3);
+	return (X[0] - 1) * (X[0] - 2) * (X[0] - 3);
 }
 
 NT logx_natural_1D(Point X) {
 	return log(X[0]);
 }
 
-NT rooted_squaresum_2D(Point X) {
-	return sqrt(X[0]*X[0] + X[1]*X[1]);
+NT rooted_squaresum(Point X) {
+	return sqrt(X.squared_length());
 }
 
-template 
-<
-	typename NT
->
-void test_values(NT computed, NT expected, NT exact) {
+template <typename NT>
+void test_values (NT computed, NT expected, NT exact) {
 	std::cout << "Computed integration value = " << computed << std::endl;
 	std::cout << "Expected integration value = " << expected << std::endl;
 	std::cout << "Exact integration value = " << exact << std::endl;
@@ -58,91 +55,226 @@ void test_values(NT computed, NT expected, NT exact) {
     CHECK(((std::abs((computed - expected)/expected) < 0.00001) || (std::abs((computed - exact)/exact) < 0.2)));
 }
 
-void call_test_simple_mc_over_hyperrectangle(){
+template <typename NT>
+void call_test_simple_mc_integration_over_rectangles(){
 	
 	NT integration_value;
-	std::cout << "\nTESTS FOR SIMPLE MC INTEGRATION OVER DEFINED INTEGRATION LIMITS USING UNIFORM RANDOM WALKS\n";
+	std::cout << "\nTESTS FOR SIMPLE MC INTEGRATION OVER RECTANGLES USING UNIFORM RANDOM WALKS\n";
 
-	integration_value = simple_mc_integrate <BallWalk> (exp_normsq, 10, 100000);
+	integration_value = simple_mc_integrate <BallWalk> (exp_normsq, 10, 100000, SOB);
 	test_values(integration_value, 54.8, 55.25);
 
-	integration_value = simple_mc_integrate <BilliardWalk> (exp_normsq, 8, 100000);
+	integration_value = simple_mc_integrate <BilliardWalk> (exp_normsq, 8, 100000, SOB);
 	test_values(integration_value, 24.8, 24.76);
 	
-	integration_value = simple_mc_integrate <BilliardWalk> (exp_normsq, 5, 100000);
+	integration_value = simple_mc_integrate <BilliardWalk> (exp_normsq, 5, 100000, SOB);
 	test_values(integration_value, 7.49, 7.46);
 
 	Limit LL{-1};
 	Limit UL{6};
-	integration_value = simple_mc_integrate(simple_polynomial_1D, 1, 100000, LL, UL);
+	integration_value = simple_mc_integrate <BilliardWalk> (simple_polynomial_1D, 1, 100000, CB, LL, UL);
 	test_values(integration_value, 39.7, 40.25);
 
 	Limit LL1{0.5};
 	Limit UL1{10};
-	integration_value = simple_mc_integrate(logx_natural_1D, 1, 100000, LL1, UL1);
+	integration_value = simple_mc_integrate <BilliardWalk> (logx_natural_1D, 1, 100000, CB, LL1, UL1);
 	test_values(integration_value, 13.65, 13.872);
 
 	Limit LL2{-1, -1};
 	Limit UL2{1, 1};
-	integration_value = simple_mc_integrate(rooted_squaresum_2D, 2, 100000, LL2, UL2);
+	integration_value = simple_mc_integrate <BilliardWalk> (rooted_squaresum, 2, 100000, SOB, LL2, UL2);
 	test_values(integration_value, 2.99, 3.0607);
 
 }
 
-void call_test_simple_mc_over_polytope() {
+template <typename NT>
+void call_test_simple_mc_integration_over_cubes() {
 
-	std::cout << "\nTESTS FOR SIMPLE MC INTEGRATION OVER CONVEX-BODIES USING UNIFORM RANDOM WALKS\n";
+	std::cout << "\nTESTS FOR SIMPLE MC INTEGRATION OVER CUBES USING UNIFORM RANDOM WALKS\n";
 
 	NT integration_value;
-	// H-Polytope Integration Test:1 for 2D Polytope around the origin
-	HPOLYTOPE HP = generate_cube <HPOLYTOPE> (2, false);
-	integration_value = simple_mc_polytope_integrate<BilliardWalk, HPOLYTOPE>(exp_normsq, 2, HP, 100000, SOB, 10, 0.01);
+	HPOLYTOPE HP;
+
+	HP = generate_cube <HPOLYTOPE> (2, false);
+	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, HP, 100000, SOB, 10, 0.01);
 	test_values(integration_value, 2.20, 2.230);
 
-	// H-Polytope Integration Test:2 for 2D Polytope shifted to (1,1) from origin
-	std::vector<NT> origin{1,1};
-	Point newOrigin(2,origin);
-	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, 2, HP, 100000, SOB, 1, 0.01, newOrigin);
+	// For 2D Polytope shifted to (1,1) from origin
+	std::vector<NT> Origin{1, 1};
+	Point newOrigin(2, Origin);
+	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, HP, 100000, SOB, 1, 0.01, newOrigin);
 	test_values(integration_value, 0.78, 0.777);
 
-	// H-Polytope Integration Test:3 for 10D Polytope around origin
-	HPOLYTOPE HP1 = generate_cube <HPOLYTOPE> (10, false);
-	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, 10, HP1, 100000, SOB);
+	HP = generate_cube <HPOLYTOPE> (10, false);
+	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, HP, 100000, SOB);
 	test_values(integration_value, 54.7, 55.25);
 
-	// H-Polytope Integration Test:4 for 15D Polytope around origin
-	HPOLYTOPE HP2 = generate_cube <HPOLYTOPE> (15, false);
-	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, 15, HP2, 100000, SOB);
+	HP = generate_cube <HPOLYTOPE> (15, false);
+	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, HP, 100000, SOB);
 	test_values(integration_value, 405.9, 410.690);
 
-	// H-Polytope Integration Test:5 for 20D Polytope around origin
-	HPOLYTOPE HP3 = generate_cube <HPOLYTOPE> (20, false);
-	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, 20, HP3, 100000, SOB);
+	HP = generate_cube <HPOLYTOPE> (20, false);
+	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, HP, 100000, SOB);
 	test_values(integration_value, 3050.0, 3052.71);
 
-	// V-Polytope Integration Test:6
-	// VPOLYTOPE VP = generate_cross<VPOLYTOPE>(2, true);
-	// integration_value = simple_mc_polytope_integrate <BilliardWalk, VPOLYTOPE> (exp_normsq, 2, VP, 100000, CB);
-	// std::cout << "Integration value: " << integration_value << std::endl;
-	// test_values(integration_value, expected, exact);
-
-	// Polytope Integration Test:3 Reading a HPolytope from ine file for 20 Dimensions
+	// Reading a H-Polytope from *.ine file for 20 Dimensions
 	// std::string fileName("cube10.ine");
 	// std::ifstream inp;
-	// std::vector<std::vector<NT> > Pin;
+	// std::vector<std::vector<NT>> Pin;
 	// inp.open(fileName, std::ifstream::in);
 	// read_pointset(inp,Pin);
 	// HPOLYTOPE HP4(Pin);
-	// integration_value = simple_mc_polytope_integrate<BilliardWalk,HPOLYTOPE>(exp_normsq, 2, HP4, 100000, SOB);
+	// integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, 2, HP4, 100000, SOB);
 	// std::cout << "Integration value: " << integration_value << std::endl;
 	// test_values(integration_value, expected, exact);
 	// inp.close();
-}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
-
-TEST_CASE("hyperrectangles") {
-    call_test_simple_mc_over_hyperrectangle();
 }
 
-TEST_CASE("polytopes") {
-    call_test_simple_mc_over_polytope();
+template <typename NT>
+void call_test_simple_mc_integration_over_simplices() {
+
+	std::cout << "\nTESTS FOR SIMPLE MC INTEGRATION OVER SIMPLICES USING UNIFORM RANDOM WALKS\n";
+
+	NT integration_value;
+	HPOLYTOPE HP;
+
+	HP = generate_simplex <HPOLYTOPE> (1, false);
+	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, HP, 100000, CB, 10, 0.01);
+	std::cout << "Integration value = " << integration_value << std::endl;
+	// test_values(integration_value, 2.20, 2.230);
+	
+	HP = generate_simplex <HPOLYTOPE> (2, false);
+	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, HP, 100000, SOB, 10, 0.01);
+	std::cout << "Integration value = " << integration_value << std::endl;
+	// test_values(integration_value, 2.20, 2.230);
+
+	HP = generate_simplex <HPOLYTOPE> (3, false);
+	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, HP, 100000, SOB);
+	std::cout << "Integration value = " << integration_value << std::endl;
+	// test_values(integration_value, 54.7, 55.25);
+
+	HP = generate_simplex <HPOLYTOPE> (5, false);
+	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, HP, 100000, SOB);
+	std::cout << "Integration value = " << integration_value << std::endl;
+	// test_values(integration_value, 405.9, 410.690);
+
+	HP = generate_simplex <HPOLYTOPE> (7, false);
+	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, HP, 100000, SOB);
+	std::cout << "Integration value = " << integration_value << std::endl;
+	// test_values(integration_value, 3050.0, 3052.71);
+
+}
+
+template <typename NT>
+void call_test_simple_mc_integration_over_product_simplices() {
+
+	std::cout << "\nTESTS FOR SIMPLE MC INTEGRATION OVER PRODUCT SIMPLICES USING UNIFORM RANDOM WALKS\n";
+
+	NT integration_value;
+	HPOLYTOPE HP;
+
+	HP = generate_prod_simplex <HPOLYTOPE> (1, false);
+	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, HP, 100000, CB, 10, 0.01);
+	std::cout << "Integration value = " << integration_value << std::endl;
+	// test_values(integration_value, 2.20, 2.230);
+	
+	HP = generate_prod_simplex <HPOLYTOPE> (2, false);
+	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, HP, 100000, SOB, 10, 0.01);
+	std::cout << "Integration value = " << integration_value << std::endl;
+	// test_values(integration_value, 2.20, 2.230);
+
+	HP = generate_prod_simplex <HPOLYTOPE> (3, false);
+	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, HP, 100000, SOB);
+	std::cout << "Integration value = " << integration_value << std::endl;
+	// test_values(integration_value, 54.7, 55.25);
+
+	HP = generate_prod_simplex <HPOLYTOPE> (5, false);
+	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, HP, 100000, SOB);
+	std::cout << "Integration value = " << integration_value << std::endl;
+	// test_values(integration_value, 405.9, 410.690);
+
+	HP = generate_prod_simplex <HPOLYTOPE> (7, false);
+	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq,  HP, 100000, SOB);
+	std::cout << "Integration value = " << integration_value << std::endl;
+	// test_values(integration_value, 3050.0, 3052.71);
+
+}
+
+template <typename NT>
+void call_test_simple_mc_integration_over_cross_polytopes() {
+
+	std::cout << "\nTESTS FOR SIMPLE MC INTEGRATION OVER CROSS POLYTOPES USING UNIFORM RANDOM WALKS\n";
+
+	NT integration_value;
+	HPOLYTOPE HP;
+
+	HP = generate_cross <HPOLYTOPE> (1, false);
+	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, HP, 100000, CB, 10, 0.01);
+	std::cout << "Integration value = " << integration_value << std::endl;
+	// test_values(integration_value, 2.20, 2.230);
+	
+	HP = generate_cross <HPOLYTOPE> (2, false);
+	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, HP, 100000, SOB, 10, 0.01);
+	std::cout << "Integration value = " << integration_value << std::endl;
+	// test_values(integration_value, 2.20, 2.230);
+
+	HP = generate_cross <HPOLYTOPE> (3, false);
+	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, HP, 100000, SOB);
+	std::cout << "Integration value = " << integration_value << std::endl;
+	// test_values(integration_value, 54.7, 55.25);
+
+	HP = generate_cross <HPOLYTOPE> (5, false);
+	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, HP, 100000, SOB);
+	std::cout << "Integration value = " << integration_value << std::endl;
+	// test_values(integration_value, 405.9, 410.690);
+
+	HP = generate_cross <HPOLYTOPE> (7, false);
+	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, HP, 100000, SOB);
+	std::cout << "Integration value = " << integration_value << std::endl;
+	// test_values(integration_value, 3050.0, 3052.71);
+
+}
+
+template <typename NT>
+void call_test_simple_mc_integration_over_birkhoff_polytopes() {
+
+	std::cout << "\nTESTS FOR SIMPLE MC INTEGRATION OVER BIRKHOFF POLYTOPES USING UNIFORM RANDOM WALKS\n";
+
+	NT integration_value;
+	HPOLYTOPE HP;
+
+	HP = generate_birkhoff <HPOLYTOPE> (2);
+	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, HP, 100000, CB, 10, 0.01);
+	std::cout << "Integration value = " << integration_value << std::endl;
+	// test_values(integration_value, 2.20, 2.230);
+	
+	HP = generate_birkhoff <HPOLYTOPE> (4);
+	integration_value = simple_mc_polytope_integrate <BilliardWalk, HPOLYTOPE> (exp_normsq, HP, 100000, SOB);
+	std::cout << "Integration value = " << integration_value << std::endl;
+	// test_values(integration_value, 3050.0, 3052.71);
+
+}
+
+TEST_CASE("rectangles") {
+    call_test_simple_mc_integration_over_rectangles<double>();
+}
+
+TEST_CASE("cubes") {
+    call_test_simple_mc_integration_over_cubes<double>();
+}
+
+TEST_CASE("simplices") {
+    call_test_simple_mc_integration_over_simplices<double>();
+}
+
+TEST_CASE("prod_simplices") {
+	call_test_simple_mc_integration_over_product_simplices<double>();
+}
+
+TEST_CASE("cross") {
+	call_test_simple_mc_integration_over_cross_polytopes<double>();
+}
+
+TEST_CASE("birkhoff") {
+	call_test_simple_mc_integration_over_birkhoff_polytopes<double>();
 }
