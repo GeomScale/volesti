2eac7b72ea582ed60e88d2b221e658560eadfe81
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2021-07-24 06:06:19 +0530

    add function to sample a direction from multivariate gaussian

--- include/sampling/ellipsoid.hpp
+++ include/sampling/ellipsoid.hpp
@@ -35,5 +35,32 @@ struct GetPointInDellipsoid
 };
 
 
+// Note: can also be used for sampling points on the surface of ellipsoid using normalize = false
+template <typename Point>
+struct GetGaussianDirection
+{
+    typedef typename Point::FT NT;
+    typedef typename Eigen::Matrix<NT, Eigen::Dynamic, 1> VT;
+
+    template <typename MT, typename RandomNumberGenerator>
+    inline static Point apply(unsigned int const& dim,
+                              MT const& L, // cholesky matrix L of the covariance matrix, LL' = Sigma
+                              RandomNumberGenerator &rng,
+                              bool normalize=true)
+    {
+        // Generate a point inside a sphere of radius 1.0
+        Point p = GetDirection<Point>::apply(dim ,rng);
+
+        // Multiply with cholesky matrix
+        VT gaussian_vec = L * p.getCoefficients();
+        if (normalize) {
+            gaussian_vec.normalize();
+        }
+
+        // convert to point
+        return Point(gaussian_vec);
+    }
+};
+
 
 #endif // ELLIPSOID_HPP
