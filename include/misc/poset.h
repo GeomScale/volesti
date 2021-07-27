62145c823834c24079a21ebd8d0d8f43227c6e84
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2021-07-27 18:47:20 +0530

    Order polytope class (#165)
    * Initial commit
    
    * Add functions for line intersection with rays
    
    * Add more member functions in order polytope class
    
    * Add copyright
    
    * fix some bugs in order polytope and poset class
    
    * Add example for order-polytope and poset class methods
    
    * Modify gitignore
    
    * fix minor bugs in order polytope and poset class
    
    * Add test for basic methods of order polytope
    
    * Add test for vec_mult and line_intersect methods of order polytope class
    
    * minor modification in order-polytope example
    
    * add test for reflection and fix line_intersect test
    
    * improve README for order polytope example
    
    * minor fix in compute_reflection with params
    
    * add boundary oracles for order polytope
    
    * minor bug fixes
    
    * more bug fixes
    
    * bug fix in membership oracle of order_polytope
    
    * trim trailing whitespaces
    
    * minor changes
    
    * formatting changes
    
    * add _ before row_norms member variable
    
    * remove runtime errors of dividing by 0
    
    * add todo comments for line_intersect functions which can be removed in future

---
+++ include/misc/poset.h
@@ -0,0 +1,94 @@
+// VolEsti (volume computation and sampling library)
+
+// Copyright (c) 2012-2020 Vissarion Fisikopoulos
+// Copyright (c) 2018-2020 Apostolos Chalkis
+// Copyright (c) 2020-2021 Vaibhav Thakkar
+
+// Contributed and/or modified by Vaibhav Thakkar, as part of Google Summer of Code 2021 program.
+
+// Licensed under GNU LGPL.3, see LICENCE file
+
+#ifndef POSET_H
+#define POSET_H
+
+#include <iostream>
+#include <vector>
+
+
+class Poset {
+public:
+    typedef std::pair<unsigned int, unsigned int> RT; // relation type
+    typedef std::vector<RT> RV;  // relations vector
+
+private:
+    unsigned int n;  // elements will be from 0 to n-1
+    RV order_relations;     // pairs of form a <= b
+
+public:
+    Poset(unsigned int _n, RV& _order_relations) :
+        n(_n), order_relations(verify(_order_relations, n))
+    {}
+
+
+    // verify if the relations are valid
+    static RV verify(const RV& relations, unsigned int n)
+    {
+        for (int i = 0; i < relations.size(); i++) {
+            if (relations[i].first < 0 || relations[i].first >= n)
+                throw "invalid elements in order relations";
+
+            if (relations[i].second < 0 || relations[i].second >= n)
+                throw "invalid elements in order relations";
+        }
+
+        // TODO: Check if corresponding DAG is actually acyclic
+
+        return relations;
+    }
+
+
+    void print()
+    {
+        std::cout << "Number of elements: " << num_elem() << std::endl;
+
+        unsigned int count = num_relations();
+        for(int i=0; i<count; ++i) {
+            std::cout <<"A" << order_relations[i].first << " <= A" << order_relations[i].second << std::endl;
+        }
+    }
+
+
+    unsigned int num_elem() const
+    {
+        return n;
+    }
+
+
+    unsigned int num_relations() const
+    {
+        return order_relations.size();
+    }
+
+
+    RT get_relation(unsigned int idx) const
+    {
+        return order_relations[idx];
+    }
+
+    template <typename NT>
+    bool is_in(const std::vector<NT>& pt_coeffs, NT tol=NT(0)) const
+    {
+        for (int i = 0; i < order_relations.size(); i++) {
+            unsigned int a = order_relations[i].first;
+            unsigned int b = order_relations[i].second;
+
+            if (! (pt_coeffs[a] - pt_coeffs[b] <= tol) ) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+#endif
