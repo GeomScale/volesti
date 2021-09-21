83c989da4113930ff1938bb295bcb84a13f1a67a
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2021-09-21 17:23:51 +0530

    Ellipsoid related changes and improvements (#177)
    * reformatting changes in ellipsoid sampling example
    
    * fixed and improvement in ellipsoid class
    
    * minor fix in header file inclusion
    
    * add functions for sampling from multivariate gaussian direction and on an ellipsoid
    
    * allow normalizing gaussian direction
    
    * add requested changes

--- examples/ellipsoid-sampling/ellipsoid2d-sampling.cpp
+++ examples/ellipsoid-sampling/ellipsoid2d-sampling.cpp
@@ -11,27 +11,22 @@ typedef Cartesian <NT> Kernel;
 typedef typename Kernel::Point Point;
 typedef Eigen::Matrix<NT, Eigen::Dynamic, Eigen::Dynamic> MT;
 typedef Eigen::Matrix<NT, Eigen::Dynamic, 1> VT;
-typedef BoostRandomNumberGenerator<boost::mt19937, NT, 3> RNGType;
+typedef BoostRandomNumberGenerator<boost::mt19937, NT> RNGType;
 
 int main(int argc, char const *argv[]) {
     unsigned int dim = 2;
-    MT L(2, 2);
-    L << 0.5, 0,
-         1.5, 1.0;
-    MT A = L * L.transpose();
+    MT A(2, 2);
+    A << 0.25, 0.75,
+         0.75, 3.25;
 
-    Ellipsoid<Point, MT> ell(A);    // origin centered ellipsoid
-
+    Ellipsoid<Point> ell(A);    // origin centered ellipsoid
     int num_points = 1000;
     Point p(dim);
     RNGType rng(dim);
-    VT eigenvalues_inv_sqrt = ell.eigenvalues_inv_sqrt();
-    MT EigenVectors = ell.eigenvectors();
 
     for (int i=0; i<num_points; ++i) {
-        p = GetPointInDellipsoid<Point>::apply<NT>(dim, eigenvalues_inv_sqrt, EigenVectors, rng);
+        p = GetPointInDellipsoid<Point>::apply(dim, ell, rng);
         p.print();
     }
     return 0;
 }
-
