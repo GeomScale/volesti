c5061ffce1882a7eda56e056ed9a27bbc76cf422
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2021-11-14 03:19:26 +0530

    lpsolve include changes

--- external/cmake-files/LPSolve.cmake
+++ external/cmake-files/LPSolve.cmake
@@ -1,31 +1,31 @@
 function(GetLPSolve)
-    find_path(LP_SOLVE_DIR NAMES src PATHS ${CMAKE_SOURCE_DIR}/../external/_deps/lpsolve-src)
+  find_path(LP_SOLVE_DIR NAMES lpsolve.h PATHS ${CMAKE_SOURCE_DIR}/../external/_deps/lpsolve-src)
 
-    if (NOT LP_SOLVE_DIR)
-        include(FetchContent)
-        set(FETCHCONTENT_BASE_DIR "${CMAKE_SOURCE_DIR}/../external/_deps")
-        FetchContent_Declare(
-            lpsolve
-            URL https://cran.r-project.org/src/contrib/lpSolve_5.6.15.tar.gz
-            URL_HASH MD5=3b8d780f703e0da2e4863939add164bc
-        )
+  if (NOT LP_SOLVE_DIR)
+      include(FetchContent)
+      set(FETCHCONTENT_BASE_DIR "${CMAKE_SOURCE_DIR}/../external/_deps")
+      FetchContent_Declare(
+          lpsolve
+          URL https://webwerks.dl.sourceforge.net/project/lpsolve/lpsolve/5.5.2.11/lp_solve_5.5.2.11_source.tar.gz
+          URL_HASH MD5=a829a8d9c60ff81dc72ff52363703886
+      )
 
-        FetchContent_GetProperties(lpsolve)
+      FetchContent_GetProperties(lpsolve)
 
-        if(NOT lpsolve_POPULATED)
-            message(STATUS "lp_solve library not found locally, downloading it.")
-            FetchContent_Populate(lpsolve)
-        endif()
+      if(NOT lpsolve_POPULATED)
+          message(STATUS "lp_solve library not found locally, downloading it.")
+          FetchContent_Populate(lpsolve)
+      endif()
 
-        set(LP_SOLVE_DIR "${lpsolve_SOURCE_DIR}")
-        message(STATUS "Using downloaded lp_solve at: ${LP_SOLVE_DIR}")
+      set(LP_SOLVE_DIR "${lpsolve_SOURCE_DIR}")
+      message(STATUS "Using downloaded lp_solve at: ${LP_SOLVE_DIR}")
 
-    else()
+  else()
 
-        message(STATUS "lp_solve library found: ${LP_SOLVE_DIR}")
+      message(STATUS "lp_solve library found: ${LP_SOLVE_DIR}")
 
-    endif()
+  endif()
 
-    include_directories(${LP_SOLVE_DIR}/src)
+  include_directories(${LP_SOLVE_DIR})
 
 endfunction()
