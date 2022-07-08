77a92392785d0bf756c790b4c6045840280a356b
abureyanahmed <abureyanahmed@email.arizona.edu>
2022-07-07 21:17:45 -0500

    No globals in the gaussian HMC
    Previously, we were using a global variable, which is a bad practice. Hence, we have removed global variables.
    
    Also, removed unnecessary definitions, headers, and printing.

--- include/random_walks/gaussian_hamiltonian_monte_carlo_exact_walk.hpp
+++ include/random_walks/gaussian_hamiltonian_monte_carlo_exact_walk.hpp
@@ -10,16 +10,9 @@
 
 #include "sampling/sphere.hpp"
 
-#include <iostream>
 
-typedef long double LNT;
-typedef double NT;
 
 // Exact HMC for sampling from the spherical Gaussian distribution
-
-
-//LNT _Len;
-NT _Len;
 
 struct GaussianHamiltonianMonteCarloExactWalk
 {
@@ -64,8 +57,8 @@ struct Walk
     template <typename GenericPolytope>
     Walk(GenericPolytope &P, Point const& p, NT const& a_i, RandomNumberGenerator &rng)
     {
-        _Len = compute_diameter<GenericPolytope>
-                ::template compute<NT>(P);
+        //_Len = compute_diameter<GenericPolytope>
+        //        ::template compute<NT>(P);
         _omega = std::sqrt(NT(2) * a_i);
         _rho = 100 * P.dimension(); // upper bound for the number of reflections (experimental)
         initialize(P, p, a_i, rng);
@@ -75,9 +68,9 @@ struct Walk
     Walk(GenericPolytope &P, Point const& p, NT const& a_i, RandomNumberGenerator &rng,
          parameters const& params)
     {
-        _Len = params.set_L ? params.m_L
-                          : compute_diameter<GenericPolytope>
-                            ::template compute<NT>(P);
+        //_Len = params.set_L ? params.m_L
+        //                  : compute_diameter<GenericPolytope>
+        //                    ::template compute<NT>(P);
         _omega = std::sqrt(NT(2) * a_i);
         _rho = 100 * P.dimension(); // upper bound for the number of reflections (experimental)
         initialize(P, p, a_i, rng);
@@ -93,26 +86,19 @@ struct Walk
                       unsigned int const& walk_length,
                       RandomNumberGenerator &rng)
     {
-	//std::cout << "Inside apply of gaussian hamiltonian" << std::endl;
-	//std::cout << "_rho: " << _rho << std::endl;
         unsigned int n = P.dimension();
         NT T;
-        //LNT T;
 
         for (auto j=0u; j<walk_length; ++j)
         {
-	    //std::cout << "j: " << j << ", _p: " << _p.squared_length() << std::endl;
             T = rng.sample_urdist() * _Len;
-	    //std::cout << "_Len: " << _Len << ", T: " << T << std::endl;
             _v = GetDirection<Point>::apply(n, rng, false);
             Point p0 = _p;
             int it = 0;
             while (it < _rho)
             {
-	        //std::cout << "it: " << it << ", _p: " << _p.squared_length() << std::endl;
                 auto pbpair = P.trigonometric_positive_intersect(_p, _v, _omega, _facet_prev);
                 if (T <= pbpair.first) {
-	            //std::cout << "T: " << T << ", pbpair.first: " << pbpair.first << std::endl;
                     update_position(_p, _v, T, _omega);
                     break;
                 }
@@ -195,14 +181,9 @@ struct Walk
     }
 
     inline void update_delta(NT L)
-    //inline void update_delta(LNT L)
     {
-	//std::cout << "Updating delta\n";
-        //std::cout << "L: " << L << std::endl;
         _Len = L;
-        //std::cout << "_Len: " << _Len << std::endl;
     }
-
 
 private :
 
@@ -280,8 +261,7 @@ private :
 
     int _facet_prev;
     unsigned int _rho;
-    //NT _Len;
-    //LNT _Len;
+    NT _Len;
     Point _p;
     Point _v;
     NT _omega;
