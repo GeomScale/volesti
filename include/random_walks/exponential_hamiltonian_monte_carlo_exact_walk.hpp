ef573b5bcae64bc4bd1c8bb3971ae3bc7cc14233
abureyanahmed <abureyanahmed@email.arizona.edu>
2022-07-19 15:01:38 -0500

    Calculating diameter more appropriately
    Previously the diameter was initially set up equal to zero. Now we are setting the diameter more appropriately from the properties of the polytope.

--- include/random_walks/exponential_hamiltonian_monte_carlo_exact_walk.hpp
+++ include/random_walks/exponential_hamiltonian_monte_carlo_exact_walk.hpp
@@ -5,29 +5,29 @@
 
 // Licensed under GNU LGPL.3, see LICENCE file
 
-#ifndef RANDOM_WALKS_EXPONENTIAL_EXACT_HMC_WALK_HPP
-#define RANDOM_WALKS_EXPONENTIAL_EXACT_HMC_WALK_HPP
-
-#define INSIDE_BODY_TOLLERANCE 1e-10
+#ifndef RANDOM_WALKS_GAUSSIAN_EXACT_HMC_WALK_HPP
+#define RANDOM_WALKS_GAUSSIAN_EXACT_HMC_WALK_HPP
 
 #include "sampling/sphere.hpp"
 
 
-// Exact HMC for sampling from the Exponential distribution restricted to a convex polytope
-struct ExponentialHamiltonianMonteCarloExactWalk
+// Exact HMC for sampling from the spherical Gaussian distribution
+
+struct GaussianHamiltonianMonteCarloExactWalk
 {
-    ExponentialHamiltonianMonteCarloExactWalk(double L)
+    GaussianHamiltonianMonteCarloExactWalk(double L, unsigned int _rho)
+            :   param(L, true, _rho, true)
+    {}
+    
+    GaussianHamiltonianMonteCarloExactWalk(double L)
             :   param(L, true, 0, false)
     {}
 
-    ExponentialHamiltonianMonteCarloExactWalk(double L, unsigned int rho)
-            :   param(L, true, rho, true)
-    {}
-
-    ExponentialHamiltonianMonteCarloExactWalk()
+    GaussianHamiltonianMonteCarloExactWalk()
             :   param(0, false, 0, false)
     {}
 
+    
     struct parameters
     {
         parameters(double L, bool set, unsigned int _rho, bool _set_rho)
@@ -41,6 +41,12 @@ struct ExponentialHamiltonianMonteCarloExactWalk
 
     parameters param;
 
+template <typename GenericPolytope>
+struct compute_diameter
+{
+    template <typename NT>
+    static NT compute(GenericPolytope &P) {return NT(2) * std::sqrt(NT(P.dimension())) * P.InnerBall().second;}
+};
 
 template
 <
@@ -51,85 +57,67 @@ struct Walk
 {
     typedef typename Polytope::PointType Point;
     typedef typename Point::FT NT;
-    typedef typename Polytope::MT MT;
     typedef typename Polytope::VT VT;
 
     template <typename GenericPolytope>
-    Walk(GenericPolytope &P, Point const& p, Point const& c, NT const& T, RandomNumberGenerator &rng)
+    Walk(GenericPolytope &P, Point const& p, NT const& a_i, RandomNumberGenerator &rng)
     {
         _Len = compute_diameter<GenericPolytope>
                 ::template compute<NT>(P);
-        _Ac = P.get_mat() * c.getCoefficients();
-        _Temp = T;
-        _c = c;
+        _omega = std::sqrt(NT(2) * a_i);
         _rho = 100 * P.dimension(); // upper bound for the number of reflections (experimental)
-        initialize(P, p, rng);
+        initialize(P, p, a_i, rng);
     }
 
     template <typename GenericPolytope>
-    Walk(GenericPolytope &P, Point const& p, Point const& c, NT const& T, RandomNumberGenerator &rng,
+    Walk(GenericPolytope &P, Point const& p, NT const& a_i, RandomNumberGenerator &rng,
          parameters const& params)
     {
         _Len = params.set_L ? params.m_L
                           : compute_diameter<GenericPolytope>
                             ::template compute<NT>(P);
-        _Ac = P.get_mat() * c.getCoefficients();
-        _Temp = T;
-        _c = c;
+        _omega = std::sqrt(NT(2) * a_i);
         _rho = 100 * P.dimension(); // upper bound for the number of reflections (experimental)
-        initialize(P, p, rng);
+        initialize(P, p, a_i, rng);
     }
 
     template
     <
         typename GenericPolytope
     >
-    inline bool apply(GenericPolytope const& P,
-                      Point& p,   // a point to start
+    inline void apply(GenericPolytope const& P,
+                      Point& p,
+                      NT const& a_i,
                       unsigned int const& walk_length,
                       RandomNumberGenerator &rng)
     {
         unsigned int n = P.dimension();
         NT T;
-        int failures = 0, it;
-        Point p0 = _p;
 
         for (auto j=0u; j<walk_length; ++j)
         {
-            do {
-                _p = p0;
-                failures++;
-                if (failures == 1000) { // if the number of failures exceeds 1000 then stop
-                    return false;
+            T = rng.sample_urdist() * _Len;
+            _v = GetDirection<Point>::apply(n, rng, false);
+            Point p0 = _p;
+            int it = 0;
+            while (it < _rho)
+            {
+                auto pbpair = P.trigonometric_positive_intersect(_p, _v, _omega, _facet_prev);
+                if (T <= pbpair.first) {
+                    update_position(_p, _v, T, _omega);
+                    break;
                 }
-                T = rng.sample_urdist() * _Len;
-                _v = GetDirection<Point>::apply(n, rng, false);
-                
-                it = 0;
-                while (it < _rho)
-                {
-                    auto pbpair = P.quadratic_positive_intersect(_p, _v, _Ac, _Temp, _lambdas,
-                                                             _Av, _lambda_prev, _facet_prev);
-                    if (T <= pbpair.first) {
-                        _p += ((T * T) / (-2.0*_Temp)) *_c + (T * _v);
-                        _lambda_prev = T;
-                        break;
-                    }
-                    _lambda_prev = pbpair.first;
-                    _p += ((_lambda_prev * _lambda_prev) / (-2.0*_Temp)) *_c + (_lambda_prev * _v);
-                    T -= _lambda_prev;
-                    _v += (-_lambda_prev/_Temp) * _c;
-                    P.compute_reflection(_v, _p, pbpair.second);
-                    it++;
-                }
-                
-            } while (P.is_in(_p, INSIDE_BODY_TOLLERANCE) == 0);
-            if (it == _rho) {
+                _lambda_prev = pbpair.first;
+                T -= _lambda_prev;
+                update_position(_p, _v, _lambda_prev, _omega);
+                P.compute_reflection(_v, _p, pbpair.second);
+                it++;
+            }
+            if (it == _rho){
                 _p = p0;
             }
         }
         p = _p;
-        return true;
     }
 
 
@@ -183,7 +171,7 @@ struct Walk
             {
                 Lmax = max_dist;
             }
-            if (2.0 * rad > Lmax) 
+            if (2.0*rad > Lmax) 
             {
                 Lmax = 2.0 * rad;
             }
@@ -197,12 +185,10 @@ struct Walk
         pointset.clear();
     }
 
-
     inline void update_delta(NT L)
     {
         _Len = L;
     }
-
 
 private :
 
@@ -212,55 +198,54 @@ private :
     >
     inline void initialize(GenericPolytope const& P,
                            Point const& p,
+                           NT const& a_i,
                            RandomNumberGenerator &rng)
     {
         unsigned int n = P.dimension();
-        NT T;
-        _lambdas.setZero(P.num_of_hyperplanes());
-        _Av.setZero(P.num_of_hyperplanes());
+        _facet_prev = -1;
+        _p = p;
         _v = GetDirection<Point>::apply(n, rng, false);
-        
-        do {
-            _p = p;
-            T = rng.sample_urdist() * _Len;
-            int it = 0;
 
-            std::pair<NT, int> pbpair
-                    = P.quadratic_positive_intersect(_p, _v, _Ac, _Temp, _lambdas, _Av, _facet_prev);
-            if (T <= pbpair.first || pbpair.second < 0) {
-                _p += ((T * T) / (-2.0*_Temp)) *_c + (T * _v);
-                _lambda_prev = T;
-                return;
+        NT T = rng.sample_urdist() * _Len;
+        int it = 0;
+
+        while (it <= _rho)
+        {
+            auto pbpair
+                    = P.trigonometric_positive_intersect(_p, _v, _omega, _facet_prev);
+            if (T <= pbpair.first) {
+                update_position(_p, _v, T, _omega);
+                break;
+            } else if (it == _rho) {
+                _lambda_prev = rng.sample_urdist() * pbpair.first;
+                update_position(_p, _v, _lambda_prev, _omega);
+                break;
             }
             _lambda_prev = pbpair.first;
-            _p += ((_lambda_prev * _lambda_prev) / (-2.0*_Temp)) *_c + (_lambda_prev * _v);
-            _v += (-_lambda_prev/_Temp) * _c;
+            update_position(_p, _v, _lambda_prev, _omega);
             T -= _lambda_prev;
             P.compute_reflection(_v, _p, pbpair.second);
-
-            while (it <= _rho)
-            {
-                std::pair<NT, int> pbpair
-                        = P.quadratic_positive_intersect(_p, _v, _Ac, _Temp, _lambdas, _Av, _lambda_prev, _facet_prev);
-                if (T <= pbpair.first || pbpair.second < 0) {
-                    _p += ((T * T) / (-2.0*_Temp)) *_c + (T * _v);
-                    _lambda_prev = T;
-                    break;
-                } else if (it == _rho) {
-                    _lambda_prev = rng.sample_urdist() * pbpair.first;
-                    _p += ((_lambda_prev * _lambda_prev) / (-2.0*_Temp)) *_c + (_lambda_prev * _v);
-                    break;
-                }
-                _lambda_prev = pbpair.first;
-                _p += ((_lambda_prev * _lambda_prev) / (-2.0*_Temp)) *_c + (_lambda_prev * _v);
-                _v += (-_lambda_prev/_Temp) * _c;
-                T -= _lambda_prev;
-                P.compute_reflection(_v, _p, pbpair.second);
-                it++;
-            }
-        } while (P.is_in(_p, INSIDE_BODY_TOLLERANCE) == 0);
+            it++;
+        }
     }
 
+    inline void update_position(Point &p, Point &v, NT const& T, NT const& omega)
+    {
+        NT C, Phi;
+        for (size_t i = 0; i < p.dimension(); i++)
+        {
+            C = std::sqrt(p[i] * p[i] + (v[i] * v[i]) / (omega * omega));
+            Phi = std::atan((-v[i]) / (p[i] * omega));
+            if (v[i] < 0.0 && Phi < 0.0) {
+                Phi += M_PI;
+            } else if (v[i] > 0.0 && Phi > 0.0) {
+                Phi -= M_PI;
+            }
+            p.set_coord(i, C * std::cos(omega * T + Phi));
+            v.set_coord(i, -C * omega * std::sin(omega * T + Phi));
+        }
+        
+    }
 
     inline double get_max_distance(std::vector<Point> &pointset, Point const& q, double &rad) 
     {
@@ -279,22 +264,17 @@ private :
         return dis;
     }
 
-
-    NT _Len;
-    VT _Ac;
-    Point _p;
-    Point _v;
-    Point _c;
-    NT _Temp;
-    NT _lambda_prev;
     int _facet_prev;
     unsigned int _rho;
-    typename Point::Coeff _lambdas;
-    typename Point::Coeff _Av;
+    NT _Len;
+    Point _p;
+    Point _v;
+    NT _omega;
+    NT _lambda_prev;
 };
 
 };
 
 
-#endif // RANDOM_WALKS_EXPONENTIAL_HMC_WALK_HPP
+#endif // RANDOM_WALKS_GAUSSIAN_HMC_WALK_HPP
 
