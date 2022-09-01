45e21458cb9ee853125ca339f2de9724240a0985
iakoviid <iakoviid@gmail.com>
2022-09-02 02:00:43 +0300

    options update

--- include/preprocess/crhmc/opts.h
+++ include/preprocess/crhmc/opts.h
@@ -18,22 +18,20 @@
 template <typename Type> class opts {
 public:
   /*Preprocess options*/
-  const int ipmMaxIter = 200;
+  const int ipmMaxIter = 200; //Maximum number of iterations for finding the analytic and lewis center
   const Type ipmDistanceTol = 1e-8;
   const Type ipmDualTol = 1e-12;
   int maxNZ = 30;
   Type max_coord = 1e7;
   bool EnableReordering = true;
+  const int min_convergence_steps=8;
 
   /*ODE options*/
   const Type implicitTol = 1e-5;
   const int maxODEStep = 30;
   Type initialStep = 0.2;
-
-  /*PackedCS Solver Options*/
   Type solver_accuracy_threashold=1e-2;
-  int simdLen=1;
-  
+
   /*Sampler options*/
   bool DynamicWeight = true; //Enable the use of dynamic weights for each variable when sampling
   bool DynamicStepSize = true;  // Enable adaptive step size that avoids low acceptance probability
