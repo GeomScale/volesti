f2bd94cecfe8bfa738b9f4006a9eace222b1d369
iakoviid <iakoviid@gmail.com>
2022-09-09 11:12:21 +0300

    updating benchmark

--- test/benchmarks_crhmc_sampling.cpp
+++ test/benchmarks_crhmc_sampling.cpp
@@ -378,7 +378,9 @@ int main() {
       << "---------------CRHMC polytope sampling benchmarking---------------"
       << std::endl
       << std::endl;
+  std::cout<< "---------------Using simdLen=1---------------"<<std::endl;
   call_test_benchmark_polytope<double,1>();
+  std::cout<< "---------------Using simdLen=4---------------"<<std::endl;
   call_test_benchmark_polytope<double,4>();
   return 0;
 }
