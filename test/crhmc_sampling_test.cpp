d3884858b695ee16b360da6a682951e57ae0b23e
iakoviid <iakoviid@gmail.com>
2022-10-28 17:52:55 +0300

    typo

--- test/crhmc_sampling_test.cpp
+++ test/crhmc_sampling_test.cpp
@@ -522,7 +522,7 @@ void test_polytope_sampling_sparse_problem(ConstraintProblem problem, int n_samp
   Hess h(params);
   RNG rng(1);
   std::list<Point> PointList;
-  execute_crhmc<ConstraintProblem, RNG, std::List<Point>, Func, Grad, Hess, CRHMCWalk, simdLen>(problem, rng, PointList, 1, n_samples, n_burns, &f, &g, &h, simdLen, true);
+  execute_crhmc<ConstraintProblem, RNG, std::list<Point>, Func, Grad, Hess, CRHMCWalk, simdLen>(problem, rng, PointList, 1, n_samples, n_burns, &f, &g, &h, simdLen, true);
   MT samples = MT(PointList[0].dimension(), PointList.size());
   int i=0;
   for (std::list<Point>::iterator it = PointList.begin(); it != PointList.end(); ++it){
