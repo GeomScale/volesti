367c9ad9416cd238cc404701f3d385bba77a27be
Sarthak Bhandari <bsarthak16@gmail.com>
2023-02-20 21:35:43 +0530

    Z polytopes Volume Added

--- examples/zpolytopes-volume/zpolytope.cpp
+++ examples/zpolytopes-volume/zpolytope.cpp
@@ -1,77 +1,62 @@
-#include <iostream>
-#include <random>
+// VolEsti ( volume computation and sampling library)
+
+// Copyright (c) 2012-2023 Vissarion Fisikopoulos
+// Copyright (c) 2023 Apostolos Chalkis
+
+// Conributed and/or modified by Sarthak Bhandari
+// Licensed under GNU LGPL.3, see LICENCE file
+
+
+#include "Eigen/Eigen"
 #include <vector>
+#include "cartesian_geom/cartesian_kernel.h"
+#include "known_polytope_generators.h"
+#include "random_walks/random_walks.hpp"
 
-using namespace std;
+#include "vpolytope.h"
+#include <iostream>
+#include <fstream>
+#include "misc.h"
+#include <limits>
 
-// Define a struct to represent a vertex of the z-polytope
-struct Vertex {
-    vector<double> coordinates;
-};
+#include "volume_cooling_gaussians.hpp"
+#include "volume_cooling_balls.hpp"
 
-// Define a struct to represent a z-polytope
-struct Polytope {
-    vector<Vertex> vertices;
-};
+typedef double NT;
+typedef Cartesian <NT> Kernel;
+typedef typename Kernel::Point Point;
+typedef BoostRandomNumberGenerator<boost::mt19937, NT, 3> RNGType;
+typedef VPolytope <Point> VPOLYTOPE;
 
-// Compute the volume of the z-polytope using Monte Carlo integration
-double computeVolume(const Polytope& polytope, int numSamples) {
-    // Create a random number generator
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_real_distribution<double> dist(0.0, 1.0);
+void calculateVolumes(const VPOLYTOPE &VP) {
+	// Setup parameters for calculating volume
+	int walk_len = 10 + VP.dimension()/10;
+	NT e=0.1;
 
-    // Initialize the count of points inside the z-polytope
-    int count = 0;
+	// Calculating volume of the passed polytope
+	NT volume1 = volume_cooling_balls<BallWalk, RNGType, VPOLYTOPE>(VP, e, walk_len).second;
+	NT volume2 = volume_cooling_gaussians<GaussianBallWalk, RNGType>(VP, e, walk_len);
 
-    // Generate the specified number of random samples
-    for (int i = 0; i < numSamples; i++) {
-        // Generate a random point in the unit hypercube
-        vector<double> point;
-        for (int j = 0; j < polytope.vertices[0].coordinates.size(); j++) {
-            point.push_back(dist(gen));
-        }
-
-        // Check if the point is inside the z-polytope
-        bool inside = true;
-        for (const Vertex& vertex : polytope.vertices) {
-            double sum = 0;
-            for (int j = 0; j < vertex.coordinates.size(); j++) {
-                sum += vertex.coordinates[j] * point[j];
-            }
-            if (sum > 1) {
-                inside = false;
-                break;
-            }
-        }
-
-        // Increment the count of points inside the z-polytope
-        if (inside) {
-            count++;
-        }
-    }
-
-    // Estimate the volume of the z-polytope
-    double volume = 1.0;
-    for (int i = 0; i < polytope.vertices[0].coordinates.size(); i++) {
-        volume *= 1.0 * count / numSamples;
-    }
-    return volume;
+	std::cout<<"\t Using Cooling Balls method: "<<volume1<<"\n";
+	std::cout<<"\t Using Cooling Gaussians method: "<<volume2<<"\n";
 }
 
-int main() {
-    // Define a z-polytope with 4 vertices
-    Polytope polytope;
-    polytope.vertices.push_back({{0.5, 0, 0}});
-    polytope.vertices.push_back({{0, 0.5, 0}});
-    polytope.vertices.push_back({{0, 0, 0.5}});
-    polytope.vertices.push_back({{0.5, 0.5, 0.5}});
+int main(int argc, char* argv[]) {
+	// Reading a VPolytope from ext file
+	std::string fileName("data/zpolytope_5d_10.ext");
+	std::cout<<"Reading input from file..."<<std::endl;
+	std::ifstream inp;
+	std::vector<std::vector<NT> > Pin;
+	inp.open(fileName, std::ifstream::in);
+	read_pointset(inp,Pin);
 
-    // Compute the volume of the z-polytope using 10,000 samples
-    double volume = computeVolume(polytope, 10000);
+	VPOLYTOPE VP(Pin);
+	std::cout<<"Polytope VP: \n";
+	VP.print();
+	std::cout<<"\n";
 
-    // Print the estimated volume
-    cout << "Volume: " << volume << endl;
+	std::cout<<"Volume of VP: \n";
+	calculateVolumes(VP);
 
-    return 0;
+	return 0;
 }
