f9aaa6480ce7d85f917cc560e2fe76c5282520bc
Iva Janković <ivaxjankovic@gmail.com>
2025-07-14 01:17:15 +0200

    Corrected & Unfunctional

--- include/random_walks/billiard_shake_and_bake_walk.hpp
+++ include/random_walks/billiard_shake_and_bake_walk.hpp
@@ -1,124 +1,164 @@
+// VolEsti (volume computation and sampling library)
+
+// Copyright (c) 2012-2025 Vissarion Fisikopoulos
+// Copyright (c) 2018-2025 Apostolos Chalkis
+// Copyright (c) 2025-2025 Iva Janković
+
+// Contributed and/or modified by Iva Janković, as part of Google Summer of Code 2025 program.
+
+// Licensed under GNU LGPL.3, see LICENCE file
+
 #ifndef RANDOM_WALKS_BILLIARD_SHAKE_AND_BAKE_WALK_HPP
 #define RANDOM_WALKS_BILLIARD_SHAKE_AND_BAKE_WALK_HPP
 
 #include <Eigen/Eigen>
 #include <cmath>
+#include <limits>
 #include <stdexcept>
+#include <type_traits>
+#include <iostream>
+
 #include "sampling/sphere.hpp"
 #include "convex_bodies/hpolytope.h"
 #include "convex_bodies/convex_body.h"
-#include "random_walks/compute_diameter.hpp"
 #include "random_walks/accelerated_billiard_walk_utils.hpp"
 
 struct BilliardShakeAndBakeWalk
 {
     BilliardShakeAndBakeWalk(double L) : param(L, true) {}
-    BilliardShakeAndBakeWalk() : param(0, false) {}
+    BilliardShakeAndBakeWalk()        : param(0, false) {}
 
-    struct parameters
-    {
+    struct parameters {
         parameters(double L, bool set) : m_L(L), set_L(set) {}
         double m_L;  bool set_L;
-    };
-
-    parameters param;
+    } param;
 
     template <typename Polytope, typename RandomNumberGenerator>
     struct Walk
     {
-        using Point    = typename Polytope::PointType;
-        using NT       = typename Point::FT;
-        using VT       = typename Polytope::VT;
-        using MT       = typename Polytope::MT;
-        using MTdense  = Eigen::Matrix<NT,Eigen::Dynamic,Eigen::Dynamic>;
+        using Point   = typename Polytope::PointType;
+        using NT      = typename Point::FT;
+        using VT      = typename Polytope::VT;
+        using MT      = typename Polytope::MT;
+        using DenseMT = Eigen::Matrix<NT,Eigen::Dynamic,Eigen::Dynamic>;
+        static constexpr bool SPARSE = std::is_same_v<MT, Eigen::SparseMatrix<NT, Eigen::RowMajor>>;
 
-        struct update_parameters { int facet_prev{-1}; } params_;
+        struct update_parameters {
+            int  facet_prev{-1};
+            NT   inner_vi_ak{NT(0)};
+            bool hit_ball{false};
+            NT   ball_inner_norm{NT(1)};
+        } params_;
+
         static constexpr NT kDefaultEps = NT(1e-10);
 
         template <typename GenericPolytope>
-        Walk(GenericPolytope &P, const Point &boundary_pt, int facet_idx,
-             RandomNumberGenerator &rng, NT eps = kDefaultEps)
-        : P_(P), epsilon_(eps)
+        Walk(GenericPolytope &P,
+             const Point &boundary_pt,
+             int          facet_idx,
+             RandomNumberGenerator &rng,
+             NT           eps = kDefaultEps)
+          : P_(P), epsilon_(eps)
         {
-            if(!P_.is_normalized()) P_.normalize();
-            _Len = compute_diameter<GenericPolytope>::template compute<NT>(P_);
-            initialize(boundary_pt, facet_idx);
+            if(!P.is_normalized()) {
+                P.normalize();
+            }
+            _Len = compute_diameter<GenericPolytope>::template compute<NT>(P);
+            if constexpr (SPARSE)
+                _AA = DenseMT(P.get_mat() * P.get_mat().transpose());
+            else
+                _AA.noalias() = P.get_mat() * P.get_mat().transpose();
+            initialize(boundary_pt, facet_idx, rng);
         }
 
         template <typename GenericPolytope>
-        Walk(GenericPolytope &P, const Point &boundary_pt, int facet_idx,
-             RandomNumberGenerator &rng, NT eps, parameters const& par)
-        : P_(P), epsilon_(eps)
+        Walk(GenericPolytope &P,
+             const Point &boundary_pt,
+             int          facet_idx,
+             RandomNumberGenerator &rng,
+             NT           eps,
+             NT           manual_L)
+          : P_(P), epsilon_(eps)
         {
-            if(!P_.is_normalized()) P_.normalize();
-            _Len = par.set_L ? par.m_L : compute_diameter<GenericPolytope>::template compute<NT>(P_);
-            initialize(boundary_pt, facet_idx);
+            if (!P_.is_normalized()) P_.normalize();
+            if constexpr (SPARSE)
+                _AA = DenseMT(P.get_mat() * P.get_mat().transpose());
+            else
+                _AA.noalias() = P.get_mat() * P.get_mat().transpose();
+            _Len = manual_L;
+            initialize(boundary_pt, facet_idx, rng);
         }
 
         NT get_epsilon() const noexcept { return epsilon_; }
 
-
         void apply(unsigned walk_len, RandomNumberGenerator& rng)
         {
-            for (unsigned step = 0; step < walk_len; ++step)
-            {
-                params_.facet_prev = -1;          
-                _lambda_prev       = NT(0);       
-                lambda_hit_        = NT(0);       
-
+            for (unsigned step = 0; step < walk_len; ++step) {
+                params_.facet_prev = -1;
+                _lambda_prev      = NT(0);
+                lambda_hit_       = NT(0);
 
                 Point v = get_direction(rng);
-                NT T = rng.sample_urdist() * _Len;
-                //THIS KEEP STAYING ZERO :(
-                Point p0 = p_;
-                int reflections = 0;
+                NT    T = -std::log(rng.sample_urdist()) * _Len;  
 
-                Av_ = P_.get_mat() * v.getCoefficients();
-                // I know I shouldnt compute this like this :)
-                Ar_ = P_.get_mat() * p_.getCoefficients();
+                Point p0 = p_;         
+                int   reflections = 0;
 
-                while (T > epsilon_ && reflections < 50*dim_)
-                {
-                    NT  lambda_min;
-                    int facet_new;
-                    int attempts = 0;
+                auto [first_lambda, first_facet] = P_.line_positive_intersect_skip(p_, v, Ar_, Av_, _lambda_prev,_AA, params_,params_.facet_prev);
 
-                    do {
-                        std::tie(lambda_min, facet_new) =
-                            P_.line_positive_intersect(p_, v, Ar_, Av_, lambda_hit_);
-                    } while ((lambda_min <= epsilon_ ||
-                            facet_new == params_.facet_prev)   
-                            && ++attempts < dim_);
-
-                    if (lambda_min <= epsilon_ || facet_new < 0) break;
-
-                    if (T <= lambda_min) {                     
-                        p_  += T * v;
-                        Ar_ += T * Av_;                        
-                        T    = NT(0);
-                    } else {                                   
-                        p_  += lambda_min * v;
-                        Ar_ += lambda_min * Av_;
-                        T   -= lambda_min;
-
-                        P_.compute_reflection(v, p_, facet_new); 
-                        Av_ = P_.get_mat() * v.getCoefficients();
-
-                        params_.facet_prev = facet_new;        
-                        lambda_hit_        = NT(0);            
-                        _lambda_prev       = lambda_min;       
-                        reflections++;
-                    }
+                if (T <= first_lambda) {
+                    p_ = p_ + T * v;
+                    continue;
                 }
 
-                if (reflections >= 50*dim_)  p_ = p0;         
-            }
+                _lambda_prev = first_lambda;
+                p_ += first_lambda * v;
+                T  -= first_lambda;
 
+                if constexpr (SPARSE) {
+                    P_.compute_reflection_abw_sparse(v, p_, params_);
+                } else {
+                    P_.compute_reflection(v, p_, params_);
+                }
+
+                params_.facet_prev = first_facet;
+                reflections++;
+
+                while (T > epsilon_ && reflections < 50 * dim_) 
+                {
+                    auto [lam, facet] = P_.line_positive_intersect_skip(p_, v, Ar_, Av_, _lambda_prev,_AA, params_,params_.facet_prev);
+
+                    if (!std::isfinite(lam) || lam <= epsilon_ || facet < 0)
+                        break;
+
+                    if (T <= lam) {
+                        p_ += T * v;
+                        _lambda_prev = T;
+                        break;
+                    }
+
+                    _lambda_prev = lam;
+                    p_ += lam * v;
+                    T  -= lam;
+
+                    if constexpr (SPARSE) {
+                        P_.compute_reflection_abw_sparse(v, p_, params_);
+                    } else {
+                        P_.compute_reflection(v, p_, params_);
+                    }
+                    params_.facet_prev = facet;
+                    reflections++;
+                }
+
+                if (reflections >= 50 * dim_) {
+                    p_ = p0;
+                }
+            }
         }
 
 
         const Point& getCurrentPoint() const noexcept { return p_; }
-        void update_delta(NT L){ _Len=L; }
+        void update_delta(NT L) { _Len = L; }
 
     private:
 
@@ -138,26 +178,23 @@ struct BilliardShakeAndBakeWalk
             return Point(v);
         }
 
-
         void initialize(const Point& boundary_pt,
-                        int   facet_idx,
-                        RandomNumberGenerator& rng)
-        {
+                        int facet_idx,
+                        RandomNumberGenerator& rng) {
             dim_ = P_.dimension();
-            m_ = P_.num_of_hyperplanes();
-            VT b=P_.get_vec();
-
+            m_   = P_.num_of_hyperplanes();
+            const VT &b = P_.get_vec();
             NT kFacetEps = epsilon_;
 
-            // Checking if facet index belongs to the boundary point 
+            // Checking if boundary point belongs to facet_idx
             p_ = boundary_pt;
-            VT ai = P_.get_facet_normal_vec(facet_idx);
+            VT ai = P_.get_row(facet_idx);
             NT dist = std::abs(ai.dot(p_.getCoefficients()) - b.coeff(facet_idx));
             if (dist > kFacetEps)
             {
                 facet_idx_ = -1;
                 for (int i = 0; i < m_; ++i) {
-                    VT ai = P_.get_facet_normal_vec(i);
+                    VT ai = P_.get_row(i);
                     NT dist = std::abs(ai.dot(p_.getCoefficients()) - b.coeff(i));
                     if (dist < kFacetEps) {
                         facet_idx_ = i;
@@ -171,33 +208,22 @@ struct BilliardShakeAndBakeWalk
             }
             facet_idx_ = facet_idx;
 
-            //Normal of active facet
-            A_row_k_   = P_.get_facet_normal_vec(facet_idx_);
-
-            //Calculating first Ar and initializing Av 
+            A_row_k_ = P_.get_row(facet_idx_);
             Ar_.setZero(m_);
             Av_.setZero(m_);
-            lambda_hit_ = NT(0);
-            
             Ar_.noalias() = P_.get_mat() * p_.getCoefficients();
-            lambda_hit_ = NT(0);
-
-            A_row_k_   = P_.get_facet_normal_vec(facet_idx_);
-
             params_.facet_prev  = facet_idx_;
-
+         
         }
 
         Polytope &P_;
         NT epsilon_{kDefaultEps};
-        int dim_{0}, m_{0};
-        NT    _lambda_prev = NT(0);  
-        Point p_;                
-        VT    Ar_, Av_;           
-        VT    A_row_k_;           
-        NT    lambda_hit_{0};
-        NT    _Len{1};
+        int dim_{0}, m_{0}, facet_idx_{-1};
+        NT _lambda_prev{NT(0)}, lambda_hit_{NT(0)}, _Len{NT(1)};
+        Point p_;
+        VT Ar_, Av_, A_row_k_;
+        DenseMT _AA;
     };
 };
 
-#endif /* RANDOM_WALKS_BILLIARD_SHAKE_AND_BAKE_WALK_HPP */
+#endif 
