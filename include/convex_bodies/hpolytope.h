3363f4b3a98d3284eb4e940260889a09a64a1fe0
Iva Janković <ivaxjankovic@gmail.com>
2025-09-15 11:22:42 +0100

    Check

--- include/convex_bodies/hpolytope.h
+++ include/convex_bodies/hpolytope.h
@@ -586,6 +586,7 @@ public:
     {
         NT inner_prev = params.inner_vi_ak;
         NT* Av_data = Av.data();
+        //numerical check
         distances_set.change_val(params.facet_prev, std::numeric_limits<NT>::infinity(), params.moved_dist);
 
         // Updating Av due to the change in direction caused by the previous reflection
