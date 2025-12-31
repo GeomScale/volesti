166b5f5b92f310a5c0c46c9714fb523a807d34b2
Akash Santra <santraakash999@gmail.com>
2025-12-31 14:59:35 +0000

    Improve robustness and documentation of CDHR uniform walk

--- include/random_walks/uniform_cdhr_walk.hpp
+++ include/random_walks/uniform_cdhr_walk.hpp
@@ -9,7 +9,7 @@
 
 #ifndef RANDOM_WALKS_UNIFORM_CDHR_WALK_HPP
 #define RANDOM_WALKS_UNIFORM_CDHR_WALK_HPP
-
+#include <cassert>
 #include "sampling/sphere.hpp"
 
 // coordinate directions hit-and-run walk with uniform target distribution
@@ -24,6 +24,12 @@ template
     typename Polytope,
     typename RandomNumberGenerator
 >
+
+// NOTE:
+// This implementation of CDHR (Coordinate Direction Hit-and-Run)
+// is stateful. The walk depends on the previously selected coordinate
+// direction and previous point (_p_prev) when computing line intersections.
+// This behavior should be preserved when restarting chains.
 struct Walk
 {
     typedef typename Polytope::PointType Point;
@@ -51,20 +57,32 @@ struct Walk
                       unsigned int const& walk_length,
                       RandomNumberGenerator &rng)
     {
-        for (auto j=0u; j<walk_length; ++j)
-        {
-            auto rand_coord_prev = _rand_coord;
-            _rand_coord = rng.sample_uidist();
-            NT kapa = rng.sample_urdist();
-            std::pair<NT, NT> bpair = P.line_intersect_coord(_p,
-                                                             _p_prev,
-                                                             _rand_coord,
-                                                             rand_coord_prev,
-                                                             _lamdas);
-            _p_prev = _p;
-            _p.set_coord(_rand_coord, _p[_rand_coord] + bpair.first + kapa
-                         * (bpair.second - bpair.first));
-        }
+        for (auto j = 0u; j < walk_length; ++j)
+    {
+        auto rand_coord_prev = _rand_coord;
+        _rand_coord = rng.sample_uidist();
+
+        // Ensure sampled coordinate is valid
+        assert(_rand_coord < _p.dimension());
+
+        NT kapa = rng.sample_urdist();
+
+        std::pair<NT, NT> bpair =
+            P.line_intersect_coord(_p,
+                                _p_prev,
+                                _rand_coord,
+                                rand_coord_prev,
+                                _lamdas);
+
+        // Intersection interval must be valid
+        assert(bpair.first <= bpair.second);
+
+        _p_prev = _p;
+        _p.set_coord(
+            _rand_coord,
+            _p[_rand_coord] + bpair.first + kapa * (bpair.second - bpair.first)
+        );
+    }
         p = _p;
     }
 
@@ -72,18 +90,34 @@ private :
 
     template <typename BallPolytope>
     inline void initialize(BallPolytope const& P,
-                           Point const& p,
-                           RandomNumberGenerator &rng)
+                        Point const& p,
+                        RandomNumberGenerator &rng)
     {
+        // CDHR requires a non-empty point dimension
+        assert(p.dimension() > 0);
+
         _lamdas.setZero(P.num_of_hyperplanes());
+
         _rand_coord = rng.sample_uidist();
+        // Ensure sampled coordinate is valid
+        assert(_rand_coord < p.dimension());
+
         NT kapa = rng.sample_urdist();
         _p = p;
-        std::pair<NT, NT> bpair = P.line_intersect_coord(_p, _rand_coord, _lamdas);
+
+        std::pair<NT, NT> bpair =
+            P.line_intersect_coord(_p, _rand_coord, _lamdas);
+
+        // Intersection interval must be valid
+        assert(bpair.first <= bpair.second);
+
         _p_prev = _p;
-        _p.set_coord(_rand_coord, _p[_rand_coord] + bpair.first + kapa
-                    * (bpair.second - bpair.first));
+        _p.set_coord(
+            _rand_coord,
+            _p[_rand_coord] + bpair.first + kapa * (bpair.second - bpair.first)
+        );
     }
+
 
     unsigned int _rand_coord;
     Point _p;
