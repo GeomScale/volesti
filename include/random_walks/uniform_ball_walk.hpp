b76d781eeeffd5ab4856f50b1640aa8d5896920f
Akash Santra <santraakash999@gmail.com>
2026-01-01 13:52:48 +0000

    Improve robustness of Ball Walk for low-dimensional polytopes

--- include/random_walks/uniform_ball_walk.hpp
+++ include/random_walks/uniform_ball_walk.hpp
@@ -7,8 +7,16 @@
 
 // Licensed under GNU LGPL.3, see LICENCE file
 
+// Ball Walk assumes:
+// - polytope dimension > 0
+// - existence of a non-zero inner ball
+// Violating these assumptions leads to undefined sampling behavior.
+
+
 #ifndef RANDOM_WALKS_UNIFORM_BALL_WALK_HPP
 #define RANDOM_WALKS_UNIFORM_BALL_WALK_HPP
+#include <stdexcept>
+#include <cmath>
 
 #include "generators/boost_random_number_generator.hpp"
 
@@ -56,15 +64,32 @@ struct BallWalk
         Walk(GenericPolytope& P, Point const& /*p*/,
              RandomNumberGenerator& /*rng*/, parameters const& params)
         {
-            _delta = params.set_delta ? params.m_L
-                                      : compute_delta(P);
+            _delta = params.set_delta ? NT(params.m_L) : compute_delta(P);
+            if (_delta <= NT(0))
+            {
+                throw std::runtime_error(
+                    "BallWalk requires a strictly positive step size");
+            }
         }
 
         template <typename GenericPolytope>
         static inline NT compute_delta(GenericPolytope& P)
         {
-            //return ((P.InnerBall()).second * NT(4)) / NT(P.dimension());
-            return (NT(4) * (P.InnerBall()).second) / std::sqrt(NT(P.dimension()));
+            const auto dim = P.dimension();
+            if (dim <= 0)
+            {
+                throw std::runtime_error(
+                    "BallWalk requires a polytope of positive dimension");
+            }
+
+            const NT radius = (P.InnerBall()).second;
+            if (radius <= NT(0))
+            {
+                throw std::runtime_error(
+                    "BallWalk requires a polytope with a non-zero inner ball");
+            }
+
+            return (NT(4) * radius) / std::sqrt(NT(dim));
         }
 
         template<typename BallPolytope>
@@ -85,11 +110,16 @@ struct BallWalk
 
         inline void update_delta(NT delta)
         {
+            if (delta <= NT(0))
+            {
+                throw std::runtime_error(
+                    "BallWalk step size must be positive");
+            }
             _delta = delta;
         }
 
     private:
-        double _delta;
+        NT _delta;
     };
 };
 
